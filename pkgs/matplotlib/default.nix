78647a7fa44405042338bd3133edfd2108021f8b
Jonathan Ringer <jonringer117@gmail.com>
2026-06-24 16:32:29 -0700

    python3Packages.matplotlib: init at 3.10.9

--- pkgs/matplotlib/default.nix
+++ pkgs/matplotlib/default.nix
@@ -13,9 +13,7 @@
   setuptools-scm,
 
   # native libraries
-  ffmpeg-headless,
   freetype,
-  qhull,
 
   # propagates
   contourpy,
@@ -28,38 +26,9 @@
   pyparsing,
   python-dateutil,
 
-  # GTK3
-  enableGtk3 ? false,
-  cairo,
-  gobject-introspection,
-  gtk3,
-  pycairo,
-  pygobject3,
-
-  # Tk
-  enableTk ? (!stdenv.hostPlatform.isDarwin && !isPyPy),
-  tkinter,
-
-  # Qt
-  enableQt ? false,
-  pyqt5,
-
   # Webagg
-  enableWebagg ? false,
   tornado,
-
-  # nbagg
-  enableNbagg ? false,
-  ipykernel,
-
-  # required for headless detection
-  libx11,
-  wayland,
 }:
-
-let
-  interactive = enableTk || enableGtk3 || enableQt;
-in
 
 buildPythonPackage rec {
   version = "3.10.9";
@@ -83,24 +52,12 @@ buildPythonPackage rec {
         --replace-fail "meson-python>=0.13.1,<0.17.0" meson-python
 
       patchShebangs tools
-    ''
-    + lib.optionalString (stdenv.hostPlatform.isLinux && interactive) ''
-      # fix paths to libraries in dlopen calls (headless detection)
-      substituteInPlace src/_c_internal_utils.cpp \
-        --replace-fail libX11.so.6 ${libx11}/lib/libX11.so.6 \
-        --replace-fail libwayland-client.so.0 ${wayland}/lib/libwayland-client.so.0
     '';
 
-  nativeBuildInputs = [ pkg-config ] ++ lib.optionals enableGtk3 [ gobject-introspection ];
+  nativeBuildInputs = [ pkg-config ];
 
   buildInputs = [
-    ffmpeg-headless
     freetype
-    qhull
-  ]
-  ++ lib.optionals enableGtk3 [
-    cairo
-    gtk3
   ];
 
   hardeningDisable = lib.optionals stdenv.hostPlatform.isDarwin [ "strictoverflow" ];
@@ -123,19 +80,15 @@ buildPythonPackage rec {
     pillow
     pyparsing
     python-dateutil
-  ]
-  ++ lib.optionals enableGtk3 [
-    pycairo
-    pygobject3
-  ]
-  ++ lib.optionals enableQt [ pyqt5 ]
-  ++ lib.optionals enableWebagg [ tornado ]
-  ++ lib.optionals enableNbagg [ ipykernel ]
-  ++ lib.optionals enableTk [ tkinter ];
+  ];
+
+  optional-dependencies = {
+    webagg = [ tornado ];
+  };
 
   mesonFlags = lib.mapAttrsToList lib.mesonBool {
     system-freetype = true;
-    system-qhull = true;
+    system-qhull = false;
     b_lto = false;
   };
 
