f3bccf526c2cbc38f4d6af565417889fb6e0edaa
Jonathan Ringer <jonringer117@gmail.com>
2026-07-14 16:07:01 -0700

    default.nix: clean up module logic

--- default.nix
+++ default.nix
@@ -2,32 +2,19 @@ let
   pins = import ./pins.nix;
 in
 
-# Continuation passing style of import
-# Values we care to modify are modified, while all other
-# arguments are "passed through" to the next scope
 {
-  overlays ? [ ],
-  config ? { },
-  ...
+  modules ? [ ]
 }@args:
 
 let
   filteredAttrs = builtins.removeAttrs args [
-    "overlays"
-    "config"
+    "modules"
   ];
 in
 
 import pins.core (
   {
-    modules = [ ./pkgs-module.nix ];
-    # overlays = [
-    #   toplevelOverlay
-    # ] ++ overlays;
-
-    # config = config // {
-    #   overlays.python = [ pythonOverlay ] ++ (config.overlays.python or [ ]);
-    # };
+    modules = modules ++ [ ./pkgs-module.nix ];
   }
   // filteredAttrs
 )
