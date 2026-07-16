7dd9175a6d82369d0498b26df1058efbe35b3d89
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 16:08:17 -0700

    python3Packages.whoosh: init at 2.7.4

---
+++ pkgs/whoosh/default.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  fetchPypi,
+  buildPythonPackage,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "whoosh";
+  version = "2.7.4";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "Whoosh";
+    inherit version;
+    hash = "sha256-fKVjPb+p4OD6QA0xUaigxL7FO9Ls7cCmdwWxdWXDGoM=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "whoosh" ];
+
+  meta = {
+    description = "Fast, pure-Python full text indexing, search, and spell checking library";
+    homepage = "https://github.com/mchaput/whoosh";
+    license = lib.licenses.bsd2;
+  };
+}
