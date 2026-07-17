fa836022d421334fd4c63a2dbf32f0c446dbcbdd
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 08:19:02 -0700

    python3Packages.daemonize: init at 2.5.0

---
+++ pkgs/daemonize/default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+}:
+
+buildPythonPackage rec {
+  pname = "daemonize";
+  version = "2.5.0";
+  format = "setuptools";
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-3QJuT/jSLLAW7SEwvHOLfUsdpZfvk8B00q255N6gi8M=";
+  };
+
+  pythonImportsCheck = [ "daemonize" ];
+
+  meta = {
+    description = "Library to enable your code run as a daemon process on Unix-like systems";
+    homepage = "https://github.com/thesharp/daemonize";
+    license = lib.licenses.mit;
+  };
+}
