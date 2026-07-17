15a233e516257ca5ecb0c0b706a025f4a5309c96
Jonathan Ringer <jonringer117@gmail.com>
2026-07-16 18:48:11 -0700

    python3Packages.priority: init at 2.0.0

---
+++ pkgs/priority/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  fetchPypi,
+  buildPythonPackage,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "priority";
+  version = "2.0.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-yWXVTxuNDQsZR52zkkx8Ns9nLb8q7JLUP72vRJK6GMA=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "priority" ];
+
+  meta = {
+    description = "Pure-Python implementation of the HTTP/2 priority tree";
+    homepage = "https://github.com/python-hyper/priority/";
+    license = lib.licenses.mit;
+  };
+}
