7db2e29c478307c8d638fbf121776438a1834947
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 01:48:44 -0700

    python3Packages.ansicolor: init at 0.3.3

---
+++ pkgs/ansicolor/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "ansicolor";
+  version = "0.3.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "numerodix";
+    repo = "ansicolor";
+    tag = version;
+    hash = "sha256-ndChpcHjsGWmlw0uvPF0RvRvi99b3cnajHRXudmQXBw=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "ansicolor" ];
+
+  meta = {
+    description = "Library to produce ansi color output and colored highlighting and diffing";
+    homepage = "https://github.com/numerodix/ansicolor/";
+    license = lib.licenses.asl20;
+  };
+}
