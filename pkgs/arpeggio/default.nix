320912e85845a55672ea46533b9cc46d8498f915
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 01:52:50 -0700

    python3Packages.arpeggio: init at 2.0.3

---
+++ pkgs/arpeggio/default.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "arpeggio";
+  version = "2.0.3";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "Arpeggio";
+    inherit version;
+    hash = "sha256-noWtNc/GyThnaBfHrpoQAKfHKjTHHbDGhxNsRg0SuF4=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "arpeggio" ];
+
+  meta = {
+    description = "Recursive descent parser with memoization based on PEG grammars (aka Packrat parser)";
+    homepage = "https://github.com/textX/Arpeggio";
+    license = lib.licenses.mit;
+  };
+}
