8cd8e5b4838a4890fe73513d88d48d872f9a3fba
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:26:38 -0700

    python3Packages.eradicate: init at 3.0.1

--- top-level.nix
+++ top-level.nix
@@ -3,5 +3,6 @@ final: prev: {
   ansi2html = with final.python3Packages; toPythonApplication ansi2html;
   ghp-import = with final.python3Packages; toPythonApplication ghp-import;
   gpxinfo = with final.python3Packages; toPythonApplication gpxpy;
+  eradicate = with final.python3Packages; toPythonApplication eradicate;
   huey = with final.python3Packages; toPythonApplication huey;
 }
