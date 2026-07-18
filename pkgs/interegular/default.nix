bab2197e5d2b0f29d98ce928837a01357d1244f1
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 05:53:38 -0700

    python3Packages.interegular: init at 0.3.3

---
+++ pkgs/interegular/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "interegular";
+  version = "0.3.3";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-2baXshs0iEcROZug8DdpFLgYmc5nADJIbQ0Eg0SnZgA=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "interegular" ];
+
+  meta = {
+    description = "Library to check a subset of python regexes for intersections";
+    homepage = "https://github.com/MegaIng/interegular";
+    license = lib.licenses.mit;
+  };
+}
