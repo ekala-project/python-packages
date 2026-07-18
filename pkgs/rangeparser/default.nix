a19d433c7dfb84de931a5c572e9ddb03a51d74cc
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 06:42:32 -0700

    python3Packages.rangeparser: init at 0.1.3

---
+++ pkgs/rangeparser/default.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  fetchPypi,
+  buildPythonPackage,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "rangeparser";
+  version = "0.1.3";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "RangeParser";
+    inherit version;
+    hash = "sha256-gjA7Iytg802Lv7/rLfhGE0yjz4e6FfOXbEoWNPjhCOY=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "rangeparser" ];
+
+  meta = {
+    description = "Parses ranges";
+    homepage = "https://pypi.org/project/RangeParser/";
+    license = lib.licenses.bsd2;
+  };
+}
