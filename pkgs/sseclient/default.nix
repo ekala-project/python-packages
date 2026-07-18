5a53099362add563b1a011d3ec8f34dce5dde083
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 07:10:11 -0700

    python3Packages.sseclient: init at 0.0.27

---
+++ pkgs/sseclient/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  fetchPypi,
+  buildPythonPackage,
+  setuptools,
+  requests,
+  six,
+}:
+
+buildPythonPackage rec {
+  pname = "sseclient";
+  version = "0.0.27";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-sv5TTcszsdP6rRPWDFp8cY4o+FmH8qA07PXsJ5kYwRw=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    requests
+    six
+  ];
+
+  pythonImportsCheck = [ "sseclient" ];
+
+  meta = {
+    description = "Client library for reading Server Sent Event streams";
+    homepage = "https://github.com/btubbs/sseclient";
+    license = lib.licenses.mit;
+  };
+}
