938c7926959e21f185f08697e101adf909ba12a0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:32:04 -0700

    python3Packages.three-merge: init at 0.1.1

---
+++ pkgs/three-merge/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  diff-match-patch,
+}:
+
+buildPythonPackage rec {
+  pname = "three-merge";
+  version = "0.1.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-YPav4URZVWDWOuMmJTUbzvO5RzO1Trl4AKn+sPPZ2XA=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ diff-match-patch ];
+
+  pythonImportsCheck = [ "three_merge" ];
+
+  meta = {
+    description = "Simple library for merging two strings with respect to a base one";
+    homepage = "https://github.com/spyder-ide/three-merge";
+    license = lib.licenses.mit;
+  };
+}
