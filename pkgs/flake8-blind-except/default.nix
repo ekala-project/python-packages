6d55bdce38537624fce49cc236670e4e2fc6a583
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 06:35:20 -0700

    python3Packages.flake8-blind-except: init at 0.2.1

---
+++ pkgs/flake8-blind-except/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  pycodestyle,
+}:
+
+buildPythonPackage rec {
+  pname = "flake8-blind-except";
+  version = "0.2.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-8lpXWp3LPus8dgv5wi22C4taIxICJO0fqppD913X3RY=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ pycodestyle ];
+
+  pythonImportsCheck = [ "flake8_blind_except" ];
+
+  meta = {
+    description = "Flake8 extension that checks for blind except: statements";
+    homepage = "https://github.com/elijahandrews/flake8-blind-except";
+    license = lib.licenses.mit;
+  };
+}
