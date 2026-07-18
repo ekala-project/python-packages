8ea7802b46ebeccafa459468e359cdbf36dd3cc5
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:14:11 -0700

    python3Packages.dirtyjson: init at 1.0.8

---
+++ pkgs/dirtyjson/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "dirtyjson";
+  version = "1.0.8";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-kMpKGPP/MM6EnRANz0oAOVPHnTojSO8Fbx2cIiMaJf0=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "dirtyjson" ];
+
+  meta = {
+    description = "JSON decoder for Python that can extract data from the muck";
+    homepage = "https://github.com/codecobblers/dirtyjson";
+    license = with lib.licenses; [
+      afl21
+      mit
+    ];
+  };
+}
