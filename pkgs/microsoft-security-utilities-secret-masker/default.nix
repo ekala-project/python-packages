00ae4f7a5300d659f168c5ecfe507c242c1ffbcb
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 07:06:36 -0700

    python3Packages.microsoft-security-utilities-secret-masker: init at 1.0.0b4

---
+++ pkgs/microsoft-security-utilities-secret-masker/default.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "microsoft-security-utilities-secret-masker";
+  version = "1.0.0b4";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "microsoft_security_utilities_secret_masker";
+    inherit version;
+    hash = "sha256-owvTYawYyLUvaEQHa8JkZTNZSeqcegBNlfUZbsb97z4=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "microsoft_security_utilities_secret_masker" ];
+
+  meta = {
+    description = "Tool for detecting and masking secrets";
+    homepage = "https://pypi.org/project/microsoft-security-utilities-secret-masker/";
+    license = lib.licenses.mit;
+  };
+}
