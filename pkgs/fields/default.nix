d405433227ea10d55ed186a00a9ecf83a650a17b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 05:57:34 -0700

    python3Packages.fields: init at 5.0.0

---
+++ pkgs/fields/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "fields";
+  version = "5.0.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-MdSqA9jUTjXfE8Qx3jUTaZfwR6kkpZfYT3vCCeG+Vyc=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "fields" ];
+
+  meta = {
+    description = "Container class boilerplate killer";
+    homepage = "https://github.com/ionelmc/python-fields";
+    license = lib.licenses.bsd2;
+  };
+}
