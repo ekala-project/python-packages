1f52d91d6f1e502c7acb0060855bdac1940d2040
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 16:11:34 -0700

    python3Packages.rst2ansi: init at 0.1.5-unstable-2025-02-12

---
+++ pkgs/rst2ansi/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  docutils,
+}:
+
+buildPythonPackage {
+  pname = "rst2ansi";
+  version = "0.1.5-unstable-2025-02-12";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Snaipe";
+    repo = "python-rst2ansi";
+    rev = "3728e16f8b8b1dc338e5df90ba2c4a93ee054b3f";
+    hash = "sha256-V7tl/YJcPvEgBfH334t6CU7OXKQqBqRo/zZPiOlyCmE=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ docutils ];
+
+  pythonImportsCheck = [ "rst2ansi" ];
+
+  meta = {
+    description = "Rst converter to ansi-decorated console output";
+    mainProgram = "rst2ansi";
+    homepage = "https://github.com/Snaipe/python-rst-to-ansi";
+    license = lib.licenses.mit;
+  };
+}
