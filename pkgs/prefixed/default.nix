a9ea87d8a3e7c8767187196525079af926e02470
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:11:05 -0700

    python3Packages.prefixed: init at 0.9.0

---
+++ pkgs/prefixed/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "prefixed";
+  version = "0.9.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-FkQD+p68gygLvEcF9LJDoog34WQxC05lw4zKseuv7rM=";
+  };
+
+  build-system = [ setuptools ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "prefixed" ];
+
+  meta = {
+    description = "Prefixed alternative numeric library";
+    homepage = "https://github.com/Rockhopper-Technologies/prefixed";
+    changelog = "https://github.com/Rockhopper-Technologies/prefixed/releases/tag/${version}";
+    license = with lib.licenses; [ mpl20 ];
+  };
+}
