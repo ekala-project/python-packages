a31ea8befd240746f8b958b270b29819b2c89481
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:43:15 -0700

    python3Packages.yattag: init at 1.16.1

---
+++ pkgs/yattag/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "yattag";
+  version = "1.16.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-uqjyVOfqXT4GGCga0v9WEODlNgs2COaVwpv7OynQUfQ=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "yattag" ];
+
+  meta = {
+    description = "Library to generate HTML or XML";
+    homepage = "https://www.yattag.org/";
+    license = lib.licenses.lgpl21Only;
+  };
+}
