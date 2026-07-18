2927cb321e57e07eb37121fd46ee3b0e044156ca
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 16:37:08 -0700

    python3Packages.jwt: init at 1.4.0

---
+++ pkgs/jwt/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  setuptools,
+
+  # dependencies
+  cryptography,
+}:
+
+buildPythonPackage rec {
+  pname = "jwt";
+  version = "1.4.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "GehirnInc";
+    repo = "python-jwt";
+    tag = "v${version}";
+    hash = "sha256-Cv64SmhkETm8mx1Kj5u0WZpCPjPNvC+KS6/XaMzxCho=";
+  };
+
+  postPatch = ''
+    substituteInPlace setup.cfg --replace "--flake8" ""
+  '';
+
+  build-system = [ setuptools ];
+
+  dependencies = [ cryptography ];
+
+  pythonImportsCheck = [ "jwt" ];
+
+  meta = {
+    description = "JSON Web Token library for Python 3";
+    homepage = "https://github.com/GehirnInc/python-jwt";
+    license = lib.licenses.asl20;
+  };
+}
