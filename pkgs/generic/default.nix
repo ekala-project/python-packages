1bc5678daf737fdf168288b8b5defe1e32cbd0ca
Jonathan Ringer <jonringer117@gmail.com>
2026-07-18 06:38:55 -0700

    python3Packages.generic: init at 1.1.7

---
+++ pkgs/generic/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  poetry-core,
+  exceptiongroup,
+}:
+
+buildPythonPackage rec {
+  pname = "generic";
+  version = "1.1.7";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-0U2uZihULrCEQ0WI01B1bIjy8wx+I0itX8+gH723zu0=";
+  };
+
+  build-system = [ poetry-core ];
+
+  dependencies = [ exceptiongroup ];
+
+  pythonImportsCheck = [ "generic" ];
+
+  meta = {
+    description = "Generic programming (Multiple dispatch) library for Python";
+    homepage = "https://github.com/gaphor/generic";
+    license = lib.licenses.bsd3;
+  };
+}
