d03a42eac153fdf09d0b9e3f0c457474e750941a
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:13:01 -0700

    python3Packages.iowait: init at 0.2

---
+++ pkgs/iowait/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "iowait";
+  version = "0.2";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-qxvC64TCLM9h8XoAJPn7bfeBs58YUnZKZqd2nVrfspk=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "iowait" ];
+
+  meta = {
+    description = "Platform-independent module for I/O completion events";
+    homepage = "https://launchpad.net/python-iowait";
+    license = lib.licenses.lgpl3Only;
+  };
+}
