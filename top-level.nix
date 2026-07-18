63a6434e5a4146f9f9b8977a1768a9f84ecc8e4d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:18:59 -0700

    python3Packages.ghp-import: init at 2.1.0

--- top-level.nix
+++ top-level.nix
@@ -1,6 +1,7 @@
 # These will be added to the pkgs scope
 final: prev: {
   ansi2html = with final.python3Packages; toPythonApplication ansi2html;
+  ghp-import = with final.python3Packages; toPythonApplication ghp-import;
   gpxinfo = with final.python3Packages; toPythonApplication gpxpy;
   huey = with final.python3Packages; toPythonApplication huey;
 }
