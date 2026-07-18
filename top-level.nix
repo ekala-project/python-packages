fb94bb5a926b2ab7c6f4d021c50b9beac523072e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-17 21:13:41 -0700

    python3Packages.huey: init at 3.2.1

--- top-level.nix
+++ top-level.nix
@@ -1,4 +1,5 @@
 # These will be added to the pkgs scope
 final: prev: {
   gpxinfo = with final.python3Packages; toPythonApplication gpxpy;
+  huey = with final.python3Packages; toPythonApplication huey;
 }
