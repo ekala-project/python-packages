a9a5cdf50b5a62aa015c52e53f411c2b68e457a9
Jonathan Ringer <jonringer117@gmail.com>
2026-07-23 23:35:00 -0700

    django-filter: 25.2 -> 26.1

--- pkgs/django-filter/default.nix
+++ pkgs/django-filter/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "django-filter";
-  version = "25.2";
+  version = "26.1";
   pyproject = true;
 
   src = fetchPypi {
     pname = "django_filter";
     inherit version;
-    hash = "sha256-dg6YSpMfRGjQlvVUF4fvuJmMYSF7cwBhY78vlSP+jyM=";
+    hash = "sha256-ZuoEAxsGjHfIbhrCbO16P48Tznl/V5V1Fwfj3u/FgFQ=";
   };
 
   build-system = [ flit-core ];
