5e24d4891147f3d57794ebeca4fd6382d61b7459
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 14:11:35 -0700

    python3Packages.azure-mgmt-storage: init at 24.0.1

---
+++ python/pkgs/azure-mgmt-storage/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  azure-mgmt-common,
+  azure-mgmt-core,
+  isodate,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "azure-mgmt-storage";
+  version = "24.0.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "azure_mgmt_storage";
+    inherit version;
+    hash = "sha256-p/cGgWOzCOXx9xhUGsgBT3/5IZ2gRjv3NG7inZw0mTg=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    azure-mgmt-common
+    azure-mgmt-core
+    isodate
+  ];
+
+  pythonNamespaces = [ "azure.mgmt" ];
+
+  pythonImportsCheck = [ "azure.mgmt.storage" ];
+
+  # Module has no tests
+  doCheck = false;
+
+  meta = {
+    description = "This is the Microsoft Azure Storage Management Client Library";
+    homepage = "https://github.com/Azure/azure-sdk-for-python";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
