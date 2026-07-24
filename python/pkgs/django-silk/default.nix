0bb8ace98644b5887728ff7b3dbc31ccb21fd166
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 13:50:08 -0700

    python3Packages.django-silk: init at 5.5.0

---
+++ python/pkgs/django-silk/default.nix
@@ -0,0 +1,67 @@
+{
+  lib,
+  autopep8,
+  buildPythonPackage,
+  django,
+  fetchFromGitHub,
+  gprof2dot,
+  jinja2,
+  mock,
+  pillow,
+  pygments,
+  python,
+  python-dateutil,
+  pytz,
+  requests,
+  setuptools-scm,
+  simplejson,
+  sqlparse,
+}:
+
+buildPythonPackage rec {
+  pname = "django-silk";
+  version = "5.5.0";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "jazzband";
+    repo = "django-silk";
+    tag = version;
+    hash = "sha256-F9IHxp2dx0hzu9iOaborJjLyzZjs86rccczhc5GPbtY=";
+  };
+
+  # "test_time_taken" tests aren't suitable for reproducible execution, but Django's
+  # test runner doesn't have an easy way to ignore tests - so instead prevent it from picking
+  # them up as tests
+  postPatch = ''
+    substituteInPlace project/tests/test_silky_profiler.py \
+      --replace "def test_time_taken" "def _test_time_taken"
+    substituteInPlace setup.py \
+      --replace 'use_scm_version=True' 'version="${version}"'
+  '';
+
+  nativeBuildInputs = [ setuptools-scm ];
+
+  buildInputs = [ mock ];
+
+  propagatedBuildInputs = [
+    autopep8
+    django
+    gprof2dot
+    jinja2
+    pillow
+    pygments
+    python-dateutil
+    pytz
+    requests
+    simplejson
+    sqlparse
+  ];
+  pythonImportsCheck = [ "silk" ];
+  meta = {
+    description = "Silky smooth profiling for the Django Framework";
+    homepage = "https://github.com/jazzband/django-silk";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
