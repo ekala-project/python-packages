6fca5ef372de6bfaed3b22052fe0d3dbb316a300
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:54 -0700

    python3Packages.pydub: init at 0.25.1

---
+++ python/pkgs/pydub/default.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  audioop-lts,
+  buildPythonPackage,
+  fetchFromGitHub,
+  fetchpatch,
+  ffmpeg,
+  setuptools,
+  replaceVars,
+}:
+
+buildPythonPackage rec {
+  pname = "pydub";
+  version = "0.25.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jiaaro";
+    repo = "pydub";
+    tag = "v${version}";
+    hash = "sha256-FTEMT47wPXK5i4ZGjTVAhI/NjJio3F2dbBZzYzClU3c=";
+  };
+
+  patches = [
+    # Fix test assertions, https://github.com/jiaaro/pydub/pull/769
+    (fetchpatch {
+      name = "fix-assertions.patch";
+      url = "https://github.com/jiaaro/pydub/commit/66c1bf7813ae8621a71484fdcdf609734c0d8efd.patch";
+      hash = "sha256-3OIzvTgGK3r4/s5y7izHvouB4uJEmjO6cgKvegtTf7A=";
+    })
+    # Fix paths to ffmpeg, ffplay and ffprobe
+    (replaceVars ./ffmpeg-fix-path.patch {
+      ffmpeg = lib.getExe ffmpeg;
+      ffplay = lib.getExe' ffmpeg "ffplay";
+      ffprobe = lib.getExe' ffmpeg "ffprobe";
+    })
+  ];
+
+  nativeBuildInputs = [ setuptools ];
+
+  dependencies = [ audioop-lts ];
+  pythonImportsCheck = [
+    "pydub"
+    "pydub.audio_segment"
+    "pydub.playback"
+  ];
+  meta = {
+    description = "Manipulate audio with a simple and easy high level interface";
+    homepage = "http://pydub.com";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
