1f8ec84a9f22dab58388925aedc7fbeb8dfd43cb
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:58 -0700

    python3Packages.webauthn: init at 2.7.0

---
+++ python/pkgs/webauthn/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  asn1crypto,
+  cbor2,
+  cryptography,
+  pyopenssl,
+}:
+
+buildPythonPackage rec {
+  pname = "webauthn";
+  version = "2.7.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "duo-labs";
+    repo = "py_webauthn";
+    tag = "v${version}";
+    hash = "sha256-aZDptKJPFU6Oo4vKkIWkqkJ5ogDe5x3v7PAQRixWFe4=";
+  };
+
+  build-system = [ setuptools ];
+
+  propagatedBuildInputs = [
+    asn1crypto
+    cbor2
+    cryptography
+    pyopenssl
+  ];
+  pythonImportsCheck = [ "webauthn" ];
+
+  meta = {
+    description = "Implementation of the WebAuthn API";
+    homepage = "https://github.com/duo-labs/py_webauthn";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
