64edd0a46c6a92bd2bf8c179fa093a24b274991e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:50 -0700

    python3Packages.cytoolz: init at 1.1.0

---
+++ python/pkgs/cytoolz/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  cython,
+  setuptools,
+  toolz,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "cytoolz";
+  version = "1.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pytoolz";
+    repo = "cytoolz";
+    tag = finalAttrs.version;
+    hash = "sha256-beOEhm7+Nq7oA7iDcdORz03D1InHmypqsYUDUXEUPC0=";
+  };
+
+  postPatch = ''
+    sed -i "/setuptools-git-versioning >=/d" pyproject.toml
+    substituteInPlace pyproject.toml \
+      --replace-fail "dynamic = [\"version\"]" "version = \"${finalAttrs.version}\""
+  '';
+
+  build-system = [
+    cython
+    setuptools
+  ];
+
+  dependencies = [ toolz ];
+
+  # tests are located in cytoolz/tests, but we need to prevent import from the cytoolz source
+  meta = {
+    homepage = "https://github.com/pytoolz/cytoolz/";
+    description = "Cython implementation of Toolz: High performance functional utilities";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+})
