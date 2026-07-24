48408b19e0e4087af07055e359d1223fed46138d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 13:47:59 -0700

    python3Packages.azure-mgmt-eventgrid: init at 10.4.0

---
+++ python/pkgs/azure-mgmt-eventgrid/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  azure-common,
+  azure-mgmt-core,
+  buildPythonPackage,
+  fetchPypi,
+  isodate,
+  msrest,
+  setuptools,
+  typing-extensions,
+}:
+
+buildPythonPackage rec {
+  pname = "azure-mgmt-eventgrid";
+  version = "10.4.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "azure_mgmt_eventgrid";
+    inherit version;
+    hash = "sha256-MD5eJ89LteyDO6Tlqe9wtbxBDhkEEuxHzeWdguQT+34=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    azure-common
+    azure-mgmt-core
+    isodate
+    msrest
+    typing-extensions
+  ];
+
+  # has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "azure.mgmt.eventgrid" ];
+
+  meta = {
+    description = "This is the Microsoft Azure EventGrid Management Client Library";
+    homepage = "https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/eventgrid/azure-mgmt-eventgrid";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
