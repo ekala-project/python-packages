7f4ec69d57ed985702c2601206e524fd1a426083
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:37:47 -0700

    python3Packages.dash-bootstrap-components: init at 2.0.4

---
+++ python/pkgs/dash-bootstrap-components/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  dash,
+  hatchling,
+}:
+
+buildPythonPackage rec {
+  pname = "dash-bootstrap-components";
+  version = "2.0.4";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit version;
+    pname = "dash_bootstrap_components";
+    hash = "sha256-wyBsCSN3S7xqbdqngiuNmqUyaw08HnzXlcyXUCX+JIQ=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [ dash ];
+
+  # Tests a additional requirements
+  doCheck = false;
+
+  pythonImportsCheck = [ "dash_bootstrap_components" ];
+
+  meta = {
+    description = "Bootstrap components for Plotly Dash";
+    homepage = "https://github.com/facultyai/dash-bootstrap-components";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
