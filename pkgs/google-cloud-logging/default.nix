5ffe762054a3d3516d55b0eb56f0f34a08d9bbef
Jonathan Ringer <jonringer117@gmail.com>
2026-07-23 23:42:21 -0700

    google-cloud-logging: 3.13.0 -> 3.16.1

--- pkgs/google-cloud-logging/default.nix
+++ pkgs/google-cloud-logging/default.nix
@@ -15,13 +15,13 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-logging";
-  version = "3.13.0";
+  version = "3.16.1";
   pyproject = true;
 
   src = fetchPypi {
     pname = "google_cloud_logging";
     inherit version;
-    hash = "sha256-Oq4Fc7GhpPWezfRXH054gbWCO9Ep/kaVYcHEmn+opME=";
+    hash = "sha256-YDAkrYJX2BlPU8nfryf8LDIRHWJ7xMV0VWWEqBUPys4=";
   };
 
   build-system = [ setuptools ];
