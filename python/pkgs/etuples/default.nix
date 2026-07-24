8325e88ef986630269cfde658a27031ae542739a
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:37:51 -0700

    python3Packages.etuples: init at 0.3.10

---
+++ python/pkgs/etuples/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  cons,
+  multipledispatch,
+  py,
+  pytest-html,
+  setuptools,
+  setuptools-scm,
+}:
+
+buildPythonPackage rec {
+  pname = "etuples";
+  version = "0.3.10";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pythological";
+    repo = "etuples";
+    tag = "v${version}";
+    hash = "sha256-h5MLj1z3qZiUXcNIDtUIbV5zeyTzxerbSezFD5Q27n0=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    cons
+    multipledispatch
+  ];
+  pythonImportsCheck = [ "etuples" ];
+
+  meta = {
+    description = "Python S-expression emulation using tuple-like objects";
+    homepage = "https://github.com/pythological/etuples";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
