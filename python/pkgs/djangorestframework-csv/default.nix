4bbfa60ff3543747e461224003c90316a8c5b245
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:37:50 -0700

    python3Packages.djangorestframework-csv: init at 3.0.2

---
+++ python/pkgs/djangorestframework-csv/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  django,
+  djangorestframework,
+  pytest-django,
+  python,
+}:
+
+buildPythonPackage rec {
+  pname = "djangorestframework-csv";
+  version = "3.0.2";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "mjumbewu";
+    repo = "django-rest-framework-csv";
+    tag = version;
+    hash = "sha256-XtMkSucB7+foRpTaRfGF1Co0n3ONNGyzex6MXR4xM5c=";
+  };
+
+  dependencies = [
+    django
+    djangorestframework
+  ];
+  checkPhase = ''
+    runHook preCheck
+    ${python.interpreter} manage.py test
+    runHook postCheck
+  '';
+
+  pythonImportsCheck = [ "rest_framework_csv" ];
+
+  meta = {
+    description = "CSV Tools for Django REST Framework";
+    homepage = "https://github.com/mjumbewu/django-rest-framework-csv";
+    license = lib.licenses.bsd2;
+    maintainers = [ lib.maintainers.onny ];
+  };
+}
