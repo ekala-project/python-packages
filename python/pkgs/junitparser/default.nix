829aab866bfcde4b186e5ccf5f0a7d2dc41cc4a6
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:52 -0700

    python3Packages.junitparser: init at 5.0.0

---
+++ python/pkgs/junitparser/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  glibcLocales,
+  lxml,
+}:
+
+buildPythonPackage rec {
+  pname = "junitparser";
+  version = "5.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "weiwei";
+    repo = "junitparser";
+    tag = version;
+    hash = "sha256-I/bQQPT6b6PTZ9bIlWCQmN/gUWnVIO42xtJh/g7L79A=";
+  };
+
+  build-system = [ setuptools ];
+  meta = {
+    description = "Manipulates JUnit/xUnit Result XML files";
+    mainProgram = "junitparser";
+    license = lib.licenses.asl20;
+    homepage = "https://github.com/weiwei/junitparser";
+    maintainers = [ ];
+  };
+}
