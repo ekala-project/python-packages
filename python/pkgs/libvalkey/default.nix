209c82b1985663bbb9387d1be3a807781d615615
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:57 -0700

    python3Packages.libvalkey: init at 4.0.1

---
+++ python/pkgs/libvalkey/default.nix
@@ -0,0 +1,30 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  lib,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "libvalkey";
+  version = "4.0.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "valkey-io";
+    repo = "libvalkey-py";
+    tag = "v${finalAttrs.version}";
+    fetchSubmodules = true;
+    hash = "sha256-tOq4SC9xA1rXfclqIzseedu7lyQ+7ZcVy/4ELTAorJ4=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "libvalkey" ];
+  meta = {
+    description = "Python wrapper for libvalkey";
+    homepage = "https://github.com/valkey-io/libvalkey-py";
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.dotlambda ];
+  };
+})
