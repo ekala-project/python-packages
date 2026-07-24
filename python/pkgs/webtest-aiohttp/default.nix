0b03fe2eea37cf3ffb1aaf722f41a247fde4e3dd
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 14:02:25 -0700

    python3Packages.webtest-aiohttp: init at 2.0.0

---
+++ python/pkgs/webtest-aiohttp/default.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  aiohttp,
+  buildPythonPackage,
+  fetchFromGitHub,
+  fetchpatch,
+  setuptools,
+  webtest,
+}:
+
+buildPythonPackage rec {
+  pname = "webtest-aiohttp";
+  version = "2.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "sloria";
+    repo = "webtest-aiohttp";
+    tag = version;
+    hash = "sha256-UuAz/k/Tnumupv3ybFR7PkYHwG3kH7M5oobZykEP+ao=";
+  };
+
+  patches = [
+    (fetchpatch {
+      name = "python311-compat.patch";
+      url = "https://github.com/sloria/webtest-aiohttp/commit/64e5ab1867ea9ef87901bb2a1a6142566bffc90b.patch";
+      hash = "sha256-OKJGajqJLFMkcbGmGfU9G5hCpJaj24Gs363sI0z7YZw=";
+    })
+  ];
+
+  postPatch = ''
+    substituteInPlace test_webtest_aiohttp.py \
+      --replace-fail '(app, loop)' '(app, event_loop)' \
+      --replace-fail 'WebTestApp(app, loop=loop)' 'WebTestApp(app, loop=event_loop)'
+  '';
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    aiohttp
+    webtest
+  ];
+  pythonImportsCheck = [ "webtest_aiohttp" ];
+
+  meta = {
+    description = "Provides integration of WebTest with aiohttp.web applications";
+    homepage = "https://github.com/sloria/webtest-aiohttp";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
