c156f11677f7406a5194ed547ebee3253f970cce
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 13:35:32 -0700

    python3Packages.aiohttp-basicauth: init at 1.1.0

---
+++ python/pkgs/aiohttp-basicauth/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  aiohttp,
+  pytest-asyncio,
+}:
+
+buildPythonPackage rec {
+  pname = "aiohttp-basicauth";
+  version = "1.1.0";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "romis2012";
+    repo = "aiohttp-basicauth";
+    tag = "v${version}";
+    hash = "sha256-DjwrMlkVVceA5kDzm0c/on0VMOxyMMA3Hu4Y2Tiu0lI=";
+  };
+
+  propagatedBuildInputs = [ aiohttp ];
+pythonImportsCheck = [ "aiohttp_basicauth" ];
+
+  meta = {
+    description = "HTTP basic authentication middleware for aiohttp 3.0";
+    homepage = "https://github.com/romis2012/aiohttp-basicauth";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
