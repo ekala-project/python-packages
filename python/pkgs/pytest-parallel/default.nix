04ef5020484aaf795444468100560e7e1ffdfb08
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:32:03 -0700

    python3Packages.pytest-parallel: init at 0.1.1

---
+++ python/pkgs/pytest-parallel/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  # build inputs
+  tblib,
+  pytest,
+  py,
+}:
+let
+  pname = "pytest-parallel";
+  version = "0.1.1";
+in
+buildPythonPackage {
+  inherit pname version;
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "kevlened";
+    repo = "pytest-parallel";
+    tag = version;
+    hash = "sha256-ddpoWBTf7Zor569p6uOMjHSTx3Qa551f4mSwyTLDdBU=";
+  };
+
+  propagatedBuildInputs = [
+    tblib
+    pytest
+    py
+  ];
+
+  meta = {
+    description = "Pytest plugin for parallel and concurrent testing";
+    homepage = "https://github.com/kevlened/pytest-parallelt";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
