b720ec04b1c1e0ec1c8bbf57371c31f4ec798176
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:57 -0700

    python3Packages.typepy: init at 1.3.4

---
+++ python/pkgs/typepy/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools-scm,
+  mbstrdecoder,
+  python-dateutil,
+  pytz,
+  packaging,
+  tcolorpy,
+}:
+
+buildPythonPackage rec {
+  pname = "typepy";
+  version = "1.3.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "thombashi";
+    repo = "typepy";
+    tag = "v${version}";
+    hash = "sha256-lgwXoEtv2nBRKiWQH5bDrAIfikKN3cOqcHLEdnSAMpc=";
+  };
+
+  build-system = [ setuptools-scm ];
+
+  dependencies = [ mbstrdecoder ];
+
+  optional-dependencies = {
+    datetime = [
+      python-dateutil
+      pytz
+      packaging
+    ];
+  };
+  meta = {
+    description = "Library for variable type checker/validator/converter at a run time";
+    homepage = "https://github.com/thombashi/typepy";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
