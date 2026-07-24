98a13cc417ff0b193a31b77384040a80f6758c36
Jonathan Ringer <jonringer117@gmail.com>
2026-07-23 23:51:18 -0700

    uc-micro-py: 1.0.3 -> 2.0.0

--- pkgs/uc-micro-py/default.nix
+++ pkgs/uc-micro-py/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "uc-micro-py";
-  version = "1.0.3";
+  version = "2.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tsutsu3";
     repo = "uc.micro-py";
     tag = "v${version}";
-    hash = "sha256-Z7XHWeV5I/y19EKg4lzcl9pwRexWnSQ7d8CpQu5xdnI=";
+    hash = "sha256-o4rUK16skeB7+9gvOltYC6u1bIPvw44bVSMoB+BHvRk=";
   };
 
   nativeBuildInputs = [ setuptools ];
