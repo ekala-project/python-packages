e09725ba01e19358a1806cd8a95f43b452205013
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:48 -0700

    python3Packages.aws-sam-translator: init at 1.110.0

---
+++ python/pkgs/aws-sam-translator/default.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  boto3,
+  buildPythonPackage,
+  fetchFromGitHub,
+  jsonschema,
+  parameterized,
+  pydantic,
+  pytest-env,
+  pytest-rerunfailures,
+  pytest-xdist,
+  pyyaml,
+  requests,
+  setuptools,
+  typing-extensions,
+}:
+
+buildPythonPackage rec {
+  pname = "aws-sam-translator";
+  version = "1.110.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "aws";
+    repo = "serverless-application-model";
+    tag = "v${version}";
+    hash = "sha256-Zn+6cDyDZSsV9V+zAA8BOPs4aKl0j3dF92/azGYG+OI=";
+  };
+
+  postPatch = ''
+    # don't try to use --cov or fail on new warnings
+    rm pytest.ini
+  '';
+
+  pythonRelaxDeps = [ "pydantic" ];
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    boto3
+    jsonschema
+    pydantic
+    typing-extensions
+  ];
+  disabledTestMarks = [
+    "slow"
+  ];
+  pythonImportsCheck = [ "samtranslator" ];
+
+  meta = {
+    description = "Python library to transform SAM templates into AWS CloudFormation templates";
+    homepage = "https://github.com/aws/serverless-application-model";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
