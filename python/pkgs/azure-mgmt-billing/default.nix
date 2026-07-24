bacd24dc6d53ef4f04ab937c6b6027d33baac20d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:49 -0700

    python3Packages.azure-mgmt-billing: init at 7.0.0

---
+++ python/pkgs/azure-mgmt-billing/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  azure-common,
+  azure-mgmt-core,
+  buildPythonPackage,
+  fetchPypi,
+  isodate,
+  setuptools,
+  typing-extensions,
+}:
+
+buildPythonPackage rec {
+  pname = "azure-mgmt-billing";
+  version = "7.0.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "azure_mgmt_billing";
+    inherit version;
+    hash = "sha256-jgplxlEQtTpCk35b7WrgDvydYgaXLZa/1KdOgMhcLXs=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    azure-common
+    azure-mgmt-core
+    isodate
+    typing-extensions
+  ];
+
+  pythonNamespaces = [ "azure.mgmt" ];
+
+  # Module has no tests
+  doCheck = false;
+
+  meta = {
+    description = "This is the Microsoft Azure Billing Client Library";
+    homepage = "https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/billing/azure-mgmt-billing";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
