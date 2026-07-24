286f4e8a8d413166e3e804db0edad4885b6d97f8
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:57 -0700

    python3Packages.lingva: init at 5.0.6

---
+++ python/pkgs/lingva/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  buildPythonPackage,
+  chameleon,
+  click,
+  fetchFromGitHub,
+  polib,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "lingva";
+  version = "5.0.6";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "vacanza";
+    repo = "lingva";
+    tag = "v${version}";
+    hash = "sha256-eGXUBSEO5n5WUENhJ+p5eKTdenBsONUWw1mDax7QcSA=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    click
+    polib
+  ];
+
+  optional-dependencies = {
+    chameleon = [ chameleon ];
+  };
+  pythonImportsCheck = [ "lingva" ];
+
+  meta = {
+    description = "Module with tools to extract translatable texts from your code";
+    homepage = "https://github.com/vacanza/lingva";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
