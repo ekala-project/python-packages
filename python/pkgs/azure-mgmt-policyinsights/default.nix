89fade14088330d0c91514363fc2668380f695ed
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:49 -0700

    python3Packages.azure-mgmt-policyinsights: init at 1.0.1

---
+++ python/pkgs/azure-mgmt-policyinsights/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  azure-common,
+  azure-mgmt-core,
+  buildPythonPackage,
+  fetchPypi,
+  isodate,
+  setuptools,
+  typing-extensions,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "azure-mgmt-policyinsights";
+  version = "1.0.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "azure_mgmt_policyinsights";
+    inherit (finalAttrs) version;
+    hash = "sha256-rsmIKwVcRrWUxDjJf1Cj4YczEooRUwRpzgl6fFmaDl0=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    azure-common
+    azure-mgmt-core
+    isodate
+    typing-extensions
+  ];
+
+  # has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "azure.mgmt.policyinsights" ];
+
+  meta = {
+    description = "Microsoft Azure Policy Insights Client Library";
+    homepage = "https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/policyinsights/azure-mgmt-policyinsights";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
