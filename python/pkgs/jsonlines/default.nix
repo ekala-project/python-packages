6e17682c2e64d273f5f72be9cdfa3d41f11d1efb
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:52 -0700

    python3Packages.jsonlines: init at 4.0.0

---
+++ python/pkgs/jsonlines/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  attrs,
+  fetchFromGitHub,
+  buildPythonPackage,
+}:
+
+buildPythonPackage rec {
+  pname = "jsonlines";
+  version = "4.0.0";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "wbolster";
+    repo = "jsonlines";
+    rev = version;
+    hash = "sha256-KNEJdAxEgd0NGPnk9J51C3yUN2e6Cvvevth0iKOMlhE=";
+  };
+
+  propagatedBuildInputs = [ attrs ];
+  pythonImportsCheck = [ "jsonlines" ];
+
+  meta = {
+    description = "Python library to simplify working with jsonlines and ndjson data";
+    homepage = "https://github.com/wbolster/jsonlines";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
