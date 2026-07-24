eec7bad1068e08242ac01afdede626f294dc505c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:31:59 -0700

    python3Packages.postgrest: init at 2.29.0

---
+++ python/pkgs/postgrest/default.nix
@@ -0,0 +1,52 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  lib,
+  uv-build,
+  httpx,
+  pydantic,
+  yarl,
+  strenum,
+  deprecation,
+  pytest-asyncio,
+  pytest-cov-stub,
+  unasync,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "postgrest";
+  version = "2.29.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "supabase";
+    repo = "supabase-py";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-LaSlAYFvx/HHdfmc9J+KScVQ9JFGS98Yfihzn8F7t3g=";
+  };
+
+  sourceRoot = "${finalAttrs.src.name}/src/postgrest";
+
+  build-system = [ uv-build ];
+
+  dependencies = [
+    httpx
+    deprecation
+    pydantic
+    strenum
+    yarl
+  ]
+  ++ httpx.optional-dependencies.http2;
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail 'uv_build>=0.8.3,<0.9.0' 'uv_build>=0.8.3'
+  '';
+  pythonImportsCheck = [ "postgrest" ];
+  meta = {
+    description = "Client library for Supabase Functions";
+    homepage = "https://github.com/supabase/supabase-py";
+    maintainers = [ ];
+    license = lib.licenses.mit;
+  };
+})
