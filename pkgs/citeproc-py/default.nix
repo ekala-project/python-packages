75c6b4313d1b20dca1dc31a3a96683d38ae72cf6
Jonathan Ringer <jonringer117@gmail.com>
2026-07-23 23:33:52 -0700

    citeproc-py: 0.9.0 -> 0.10.2

--- pkgs/citeproc-py/default.nix
+++ pkgs/citeproc-py/default.nix
@@ -9,13 +9,13 @@
 
 buildPythonPackage rec {
   pname = "citeproc-py";
-  version = "0.9.0";
+  version = "0.10.2";
   pyproject = true;
 
   src = fetchPypi {
     pname = "citeproc_py";
     inherit version;
-    hash = "sha256-WHgdilY+h8p8zrQ9CL6soQ3N+fPPd93zsXiUBx7cJ8g=";
+    hash = "sha256-GVZv5W1vt5GBizIgS1HEIkPCpLYPnm0xLpNxv+rH04E=";
   };
 
   build-system = [
