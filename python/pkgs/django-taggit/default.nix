b0cb310c0dfc7002f5cceabd9463a75fdaef095a
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:50 -0700

    python3Packages.django-taggit: init at 6.1.0

---
+++ python/pkgs/django-taggit/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  django,
+  djangorestframework,
+  fetchFromGitHub,
+  python,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "django-taggit";
+  version = "6.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jazzband";
+    repo = "django-taggit";
+    tag = version;
+    hash = "sha256-QLJhO517VONuf+8rrpZ6SXMP/WWymOIKfd4eyviwCsU=";
+  };
+
+  build-system = [ setuptools ];
+
+  buildInputs = [ django ];
+  pythonImportsCheck = [ "taggit" ];
+
+  checkPhase = ''
+    # prove we're running tests against installed package, not build dir
+    rm -r taggit
+    # Replace directory of locale
+    substituteInPlace tests/test_utils.py \
+      --replace-fail 'os.path.dirname(__file__), ".."' "\"$out/lib/python${lib.versions.majorMinor python.version}/site-packages/\""
+    ${python.interpreter} -m django test --settings=tests.settings
+  '';
+
+  meta = {
+    description = "Simple tagging for django";
+    homepage = "https://github.com/jazzband/django-taggit";
+    license = lib.licenses.bsd3;
+  };
+}
