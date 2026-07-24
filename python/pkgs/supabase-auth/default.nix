8ba11f65e0662644d2eaa810fe3d93b24e5a398c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:57 -0700

    python3Packages.supabase-auth: init at 2.29.0

---
+++ python/pkgs/supabase-auth/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  uv-build,
+  httpx,
+  pydantic,
+  pyjwt,
+  faker,
+  respx,
+  pytest-mock,
+  pytest-asyncio,
+}:
+buildPythonPackage (finalAttrs: {
+  pname = "supabase-auth";
+  version = "2.29.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "supabase";
+    repo = "supabase-py";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-LaSlAYFvx/HHdfmc9J+KScVQ9JFGS98Yfihzn8F7t3g=";
+  };
+
+  sourceRoot = "${finalAttrs.src.name}/src/auth";
+
+  build-system = [ uv-build ];
+
+  dependencies = [
+    httpx
+    pydantic
+    pyjwt
+  ]
+  ++ httpx.optional-dependencies.http2
+  ++ pyjwt.optional-dependencies.crypto;
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail 'uv_build>=0.8.3,<0.9.0' 'uv_build>=0.8.3'
+  '';
+  pythonImportsCheck = [ "supabase_auth" ];
+
+  meta = {
+    description = "Client library for Supabase Auth";
+    homepage = "https://github.com/supabase/supabase-py/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
