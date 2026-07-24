b8eb55a39238ed5a8c31428366d87003eaef3431
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:32:08 -0700

    python3Packages.swagger-spec-validator: init at 3.0.4

---
+++ python/pkgs/swagger-spec-validator/default.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  jsonschema,
+  pyyaml,
+  typing-extensions,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "swagger-spec-validator";
+  version = "3.0.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Yelp";
+    repo = "swagger_spec_validator";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-8T0973g8JZKLCTpYqyScr/JAiFdBexEReUJoMQh4vO4=";
+  };
+
+  postPatch = ''
+    # https://github.com/Yelp/swagger_spec_validator/pull/176
+    substituteInPlace swagger_spec_validator/common.py tests/common_test.py \
+      --replace-fail "import importlib_resources" "import importlib.resources as importlib_resources"
+  '';
+
+  build-system = [ setuptools ];
+
+  pythonRemoveDeps = [
+    "importlib-resources"
+  ];
+
+  dependencies = [
+    pyyaml
+    jsonschema
+    typing-extensions
+  ];
+  pythonImportsCheck = [ "swagger_spec_validator" ];
+
+  meta = {
+    homepage = "https://github.com/Yelp/swagger_spec_validator";
+    license = lib.licenses.asl20;
+    description = "Validation of Swagger specifications";
+    maintainers = [ ];
+  };
+})
