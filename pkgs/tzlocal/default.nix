86d80c385a14155f414d6b1ea0162633cbb1a00c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-23 23:51:18 -0700

    tzlocal: 5.3.1 -> 5.4.4

--- pkgs/tzlocal/default.nix
+++ pkgs/tzlocal/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "tzlocal";
-  version = "5.3.1";
+  version = "5.4.4";
 
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-zO/8ft7O/qH1lVQdvW6ZDLHqPRm/AbKAnzYqA915If0=";
+    hash = "sha256-jbuGYIOGiKe2uk/tMdGN7fhCr7TUfKBQ1tiRwsFfO+Q=";
   };
 
   nativeBuildInputs = [ setuptools ];
