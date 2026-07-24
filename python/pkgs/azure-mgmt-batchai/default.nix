809c309cb4fb9a81661b866156faaa9aaa576590
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:49 -0700

    python3Packages.azure-mgmt-batchai: init at 7.0.0

---
+++ python/pkgs/azure-mgmt-batchai/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  azure-common,
+  azure-mgmt-core,
+  isodate,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "azure-mgmt-batchai";
+  version = "7.0.0";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) version;
+    pname = "azure_mgmt_batchai";
+    hash = "sha256-XfAE/QyST8ZVlJR6nP9Pdgh97hfIhFM6G7sLINsn06M=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    isodate
+    azure-common
+    azure-mgmt-core
+  ];
+
+  pythonNamespaces = [ "azure.mgmt" ];
+
+  # has no tests
+  doCheck = false;
+
+  meta = {
+    description = "This is the Microsoft Azure Batch AI Management Client Library";
+    homepage = "https://github.com/Azure/azure-sdk-for-python";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
