9583b2af04bc397a13d2d5dfb2bea57ca04cb0b7
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 13:56:43 -0700

    python3Packages.opentelemetry-instrumentation-aiohttp-client: init at unknown

---
+++ python/pkgs/opentelemetry-instrumentation-aiohttp-client/default.nix
@@ -0,0 +1,39 @@
+{
+  buildPythonPackage,
+  hatchling,
+  opentelemetry-api,
+  opentelemetry-instrumentation,
+  opentelemetry-semantic-conventions,
+  opentelemetry-util-http,
+  wrapt,
+  aiohttp,
+}:
+
+buildPythonPackage {
+  inherit (opentelemetry-instrumentation) version src;
+  pname = "opentelemetry-instrumentation-aiohttp-client";
+  pyproject = true;
+
+  sourceRoot = "${opentelemetry-instrumentation.src.name}/instrumentation/opentelemetry-instrumentation-aiohttp-client";
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    aiohttp
+    opentelemetry-api
+    opentelemetry-instrumentation
+    opentelemetry-semantic-conventions
+    opentelemetry-util-http
+    wrapt
+  ];
+
+  # missing https://github.com/ezequielramos/http-server-mock
+  # which looks unmaintained
+  doCheck = false;
+  pythonImportsCheck = [ "opentelemetry.instrumentation.aiohttp_client" ];
+
+  meta = opentelemetry-instrumentation.meta // {
+    homepage = "https://github.com/open-telemetry/opentelemetry-python-contrib/blob/main/instrumentation/opentelemetry-instrumentation-aiohttp-client";
+    description = "OpenTelemetry Instrumentation for aiohttp-client";
+  };
+}
