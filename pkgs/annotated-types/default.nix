5f3cb09f11c17c6fbb0e6e85cf675a47e333aa33
Jonathan Ringer <jonringer117@gmail.com>
2026-07-23 23:32:10 -0700

    annotated-types: 0.7.0 -> 0.8.0

--- pkgs/annotated-types/default.nix
+++ pkgs/annotated-types/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "annotated-types";
-  version = "0.7.0";
+  version = "0.8.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "annotated-types";
     repo = "annotated-types";
     tag = "v${version}";
-    hash = "sha256-I1SPUKq2WIwEX5JmS3HrJvrpNrKDu30RWkBRDFE+k9A=";
+    hash = "sha256-3ztALuBF4zJ7Fc1UCWqWRMm/a8Y0I/al+M2B669D314=";
   };
 
   nativeBuildInputs = [ hatchling ];
