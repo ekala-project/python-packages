9ee4ce691794f1ab5452c37aa4352885c6421c2c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:56 -0700

    python3Packages.simsimd: init at 6.5.16

---
+++ python/pkgs/simsimd/default.nix
@@ -0,0 +1,40 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  lib,
+  numpy,
+  pytest-repeat,
+  setuptools,
+  tabulate,
+}:
+
+buildPythonPackage rec {
+  pname = "simsimd";
+  version = "6.5.16";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ashvardanian";
+    repo = "SimSIMD";
+    tag = "v${version}";
+    hash = "sha256-J4lxmsIgzBhG2MSu2LPDY/5IKTNWEG0fDX1EI4NgLB0=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  pythonImportsCheck = [
+    "simsimd"
+  ];
+  meta = {
+    description = "Portable mixed-precision BLAS-like vector math library for x86 and ARM";
+    homepage = "https://github.com/ashvardanian/SimSIMD";
+    license = with lib.licenses; [
+      asl20
+      # or
+      bsd3
+    ];
+    maintainers = [ ];
+  };
+}
