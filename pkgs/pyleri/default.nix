fdbcdff322fe646cda61c6a2f95c5bac99548fc9
Jonathan Ringer <jonringer117@gmail.com>
2026-07-23 23:48:04 -0700

    pyleri: 1.5.0 -> 1.5.1

--- pkgs/pyleri/default.nix
+++ pkgs/pyleri/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "pyleri";
-  version = "1.5.0";
+  version = "1.5.1";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-BxWkM+W5fj0v2PdLTleHHjZes6HHoJ+3DS94cA/SXkw=";
+    hash = "sha256-mY1bDMRTlA4XU/waz4ORJWh4bm3G/kXzwlpvTqcqOCY=";
   };
 
   build-system = [ setuptools ];
