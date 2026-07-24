c7f41d4f325cd3a013194ebedcca07f2828e0f92
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:55 -0700

    python3Packages.python-backoff: init at 2.3.1

---
+++ python/pkgs/python-backoff/default.nix
@@ -0,0 +1,32 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  lib,
+  pytest-asyncio,
+  requests,
+  responses,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "python-backoff";
+  version = "2.3.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "python-backoff";
+    repo = "backoff";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Os20Gz+uWaEdUPPF9/tT7LNxbmN0W/tuzVZa3H+ZG2A=";
+  };
+
+  build-system = [ hatchling ];
+
+  pythonImportsCheck = [ "backoff" ];
+  meta = {
+    description = "Python library providing function decorators for configurable backoff and retry";
+    homepage = "https://github.com/python-backoff/backoff";
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.dotlambda ];
+  };
+})
