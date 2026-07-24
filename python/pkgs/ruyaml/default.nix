f441fb074be33121170bf38efa6681fb6eda0edf
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:38:09 -0700

    python3Packages.ruyaml: init at 0.91.0

---
+++ python/pkgs/ruyaml/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildPythonPackage,
+  distro,
+  fetchFromGitHub,
+  setuptools-scm,
+}:
+
+buildPythonPackage rec {
+  pname = "ruyaml";
+  version = "0.91.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pycontribs";
+    repo = "ruyaml";
+    tag = "v${version}";
+    hash = "sha256-A37L/voBrn2aZ7xT8+bWdZJxbWRjnxbstQtSyUeN1sA=";
+  };
+
+  postPatch = ''
+    # https://github.com/pycontribs/ruyaml/pull/107
+    substituteInPlace pyproject.toml \
+      --replace '"pip >= 19.3.1",' "" \
+      --replace '"setuptools_scm_git_archive >= 1.1",' ""
+  '';
+
+  nativeBuildInputs = [ setuptools-scm ];
+
+  propagatedBuildInputs = [ distro ];
+  pythonImportsCheck = [ "ruyaml" ];
+  meta = {
+    description = "YAML 1.2 loader/dumper package for Python";
+    homepage = "https://ruyaml.readthedocs.io/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
