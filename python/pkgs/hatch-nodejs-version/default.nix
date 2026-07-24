dcfad4df1327b17f3f79a5a352789d145ab8670b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:54 -0700

    python3Packages.hatch-nodejs-version: init at 0.4.0

---
+++ python/pkgs/hatch-nodejs-version/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+}:
+
+buildPythonPackage rec {
+  pname = "hatch-nodejs-version";
+  version = "0.4.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "agoose77";
+    repo = "hatch-nodejs-version";
+    tag = "v${version}";
+    hash = "sha256-Oe07HFzhhnAGTWM51xSgRmpJgIZg0oMIxkmMxKRPMwI=";
+  };
+
+  propagatedBuildInputs = [ hatchling ];
+  pythonImportsCheck = [ "hatch_nodejs_version" ];
+
+  meta = {
+    description = "Plugins for dealing with NodeJS versions";
+    homepage = "https://github.com/agoose77/hatch-nodejs-version";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
