ddf376bedc2edf104ce6386e1ef51b621c74f44d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:55 -0700

    python3Packages.hypothesis-jsonschema: init at 0.23.1

---
+++ python/pkgs/hypothesis-jsonschema/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  hypothesis,
+  jsonschema,
+  pytest-cov-stub,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "hypothesis-jsonschema";
+  version = "0.23.1";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  # no git tags
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-9KwDICQ0KkFJoQJTmE9aVza4Kz/ir7CIjzg0oxFT8hU=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
+    hypothesis
+    jsonschema
+  ];
+
+  doCheck = false; # sdist does not include everything to run the tests
+  pythonImportsCheck = [
+    "hypothesis_jsonschema"
+  ];
+
+  meta = {
+    description = "Generate test data from JSON schemata with Hypothesis";
+    homepage = "https://github.com/Zac-HD/hypothesis-jsonschema";
+    license = lib.licenses.mpl20;
+  };
+})
