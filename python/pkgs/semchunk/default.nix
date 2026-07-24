c503a1f5e8382cc6ffa11f83ef4a962510ea483d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:56 -0700

    python3Packages.semchunk: init at 4.1.0

---
+++ python/pkgs/semchunk/default.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  tqdm,
+}:
+
+buildPythonPackage rec {
+  pname = "semchunk";
+  version = "4.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "isaacus-dev";
+    repo = "semchunk";
+    tag = "v${version}";
+    hash = "sha256-jQQNb5E/EarsN9OwlF6l8huX06kM2EChfUYW+MM5uxA=";
+  };
+
+  build-system = [
+    hatchling
+  ];
+
+  dependencies = [
+    tqdm
+  ];
+
+  pythonImportsCheck = [
+    "semchunk"
+  ];
+
+  meta = {
+    description = "Fast, lightweight and easy-to-use Python library for splitting text into semantically meaningful chunks";
+    homepage = "https://github.com/isaacus-dev/semchunk";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
