c3d9ba896ff452c2944c26f5fc485ff7037f8ea4
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:50 -0700

    python3Packages.cron-descriptor: init at 2.1.0

---
+++ python/pkgs/cron-descriptor/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildPythonPackage,
+  setuptools,
+  typing-extensions,
+}:
+
+buildPythonPackage rec {
+  pname = "cron-descriptor";
+  version = "2.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Salamek";
+    repo = "cron-descriptor";
+    tag = version;
+    hash = "sha256-EdOcAuheCV1I/dQu4FpQ3DYx4TfPy5TyuSSmivQGy3w=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ typing-extensions ];
+  pythonImportsCheck = [ "cron_descriptor" ];
+
+  meta = {
+    description = "Library that converts cron expressions into human readable strings";
+    homepage = "https://github.com/Salamek/cron-descriptor";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
