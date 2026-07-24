eae86e9141ec61bf323e2f216198118675b9ce8a
Jonathan Ringer <jonringer117@gmail.com>
2026-07-23 23:49:37 -0700

    sgp4: 2.25 -> 2.27

--- pkgs/sgp4/default.nix
+++ pkgs/sgp4/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "sgp4";
-  version = "2.25";
+  version = "2.27";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-4Z7cbcwl1p+4/eCiZ7jwxE1+kVx7y+rPXTqLWVuvBnQ=";
+    hash = "sha256-BtNyR8aYVzm3B7izm2uD4G6K94OyUx4xllUBupg5hfk=";
   };
 
   build-system = [ setuptools ];
