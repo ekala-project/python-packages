e65c5c7c72ffb89f3428d2221444b941d1fd68f5
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:54 -0700

    python3Packages.pydata-google-auth: init at 1.9.1

---
+++ python/pkgs/pydata-google-auth/default.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  google-auth-oauthlib,
+  google-auth,
+  setuptools,
+  versioneer,
+}:
+
+buildPythonPackage rec {
+  pname = "pydata-google-auth";
+  version = "1.9.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    repo = "pydata-google-auth";
+    owner = "pydata";
+    tag = version;
+    hash = "sha256-NxEpwCkjNWao2bnKOsDgw93N+cVqwM12VfoEu8CyWUU=";
+  };
+
+  postPatch = ''
+    # Remove vendorized versioneer.py
+    rm versioneer.py
+  '';
+
+  build-system = [
+    setuptools
+    versioneer
+  ];
+
+  dependencies = [
+    google-auth
+    google-auth-oauthlib
+  ];
+
+  # tests require network access
+  doCheck = false;
+
+  pythonImportsCheck = [ "pydata_google_auth" ];
+
+  meta = {
+    description = "Helpers for authenticating to Google APIs";
+    homepage = "https://github.com/pydata/pydata-google-auth";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
