36a9eba234d58c05db727dfd7da20017f6748275
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:51 -0700

    python3Packages.giturlparse: init at 0.15.0

---
+++ python/pkgs/giturlparse/default.nix
@@ -0,0 +1,31 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  lib,
+  setuptools,
+  unittestCheckHook,
+}:
+buildPythonPackage rec {
+  pname = "giturlparse";
+  version = "0.15.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "nephila";
+    repo = "giturlparse";
+    tag = version;
+    hash = "sha256-EGhmWudQjzqw8xK/pIj5nZqosBX2lnYEgNRNQ/ePEmo=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+  pythonImportsCheck = [ "giturlparse" ];
+
+  meta = {
+    description = "Parse & rewrite git urls (supports GitHub, Bitbucket, Assembla ...)";
+    homepage = "https://github.com/nephila/giturlparse";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
