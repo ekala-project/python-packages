be190935ec8533935e44e85176593f0eff7a441f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:44:15 -0700

    python3Packages.tracerite: init at 2.3.1

---
+++ python/pkgs/tracerite/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  hatch-vcs,
+  html5tagger,
+  beautifulsoup4,
+  numpy,
+}:
+
+buildPythonPackage rec {
+  pname = "tracerite";
+  version = "2.3.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "sanic-org";
+    repo = "tracerite";
+    tag = "v${version}";
+    hash = "sha256-UXIQc5rXVaZuZj5xu2X9H38vKWAM+AoKrKfudovUhwA=";
+  };
+
+  build-system = [
+    hatchling
+    hatch-vcs
+  ];
+
+  dependencies = [
+    html5tagger
+  ];
+  pythonImportsCheck = [ "tracerite" ];
+  meta = {
+    description = "Tracebacks for Humans in Jupyter notebooks";
+    homepage = "https://github.com/sanic-org/tracerite";
+    # See https://github.com/sanic-org/tracerite/issues/13
+    license = with lib.licenses; [
+      mit
+      publicDomain
+      unlicense
+    ];
+    maintainers = [ ];
+  };
+}
