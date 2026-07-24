92b431bd5bbbdab34f06021a0628d0cfd4a821dd
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 13:56:35 -0700

    python3Packages.openai-agents: init at 0.18.1

---
+++ python/pkgs/openai-agents/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  hatchling,
+  griffelib,
+  mcp,
+  openai,
+  pydantic,
+  requests,
+  types-requests,
+  typing-extensions,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "openai-agents";
+  version = "0.18.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) version;
+    pname = "openai_agents";
+    hash = "sha256-aJrYjI9kQ1QT3ecHykX7QtVSF/9u9jqrPWOMM/eOBL8=";
+  };
+
+  build-system = [
+    hatchling
+  ];
+
+  dependencies = [
+    griffelib
+    mcp
+    openai
+    pydantic
+    requests
+    types-requests
+    typing-extensions
+  ];
+
+  pythonImportsCheck = [
+    "agents"
+  ];
+
+  meta = {
+    homepage = "https://github.com/openai/openai-agents-python";
+    description = "Lightweight, powerful framework for multi-agent workflows";
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.bryanhonof ];
+  };
+})
