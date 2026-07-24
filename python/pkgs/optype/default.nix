cd31ea9d992b08146fea593ae01078760fdb6a0f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:53 -0700

    python3Packages.optype: init at 0.15.0

---
+++ python/pkgs/optype/default.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  uv-build,
+  typing-extensions,
+  numpy,
+  numpy-typing-compat,
+  beartype,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "optype";
+  version = "0.15.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jorenham";
+    repo = "optype";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-tzbS+CeWGxMXK1LFN/LslI6kfbVQPjqYlDB7fX0ogfU=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "uv_build>=0.9.16,<0.10.0" uv_build
+  '';
+
+  build-system = [
+    uv-build
+  ];
+
+  dependencies = [
+    typing-extensions
+  ];
+
+  optional-dependencies = {
+    numpy = [
+      numpy
+      numpy-typing-compat
+    ];
+  };
+
+  pythonImportsCheck = [
+    "optype"
+  ];
+  meta = {
+    description = "Opinionated typing package for precise type hints in Python";
+    homepage = "https://github.com/jorenham/optype";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+})
