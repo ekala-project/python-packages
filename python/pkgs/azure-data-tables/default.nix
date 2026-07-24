612be44516050bb6cf0b3ee0f8b15eb724714f52
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:48 -0700

    python3Packages.azure-data-tables: init at 12.7.0

---
+++ python/pkgs/azure-data-tables/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  azure-core,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  isodate,
+  typing-extensions,
+  yarl,
+}:
+
+buildPythonPackage rec {
+  pname = "azure-data-tables";
+  version = "12.7.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "azure_data_tables";
+    inherit version;
+    hash = "sha256-sU/JSjIjooNf9WiOF9jhB7J8fNfEEUE48qyBNzcjcF0=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    azure-core
+    isodate
+    typing-extensions
+    yarl
+  ];
+
+  # Module has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "azure.data.tables" ];
+
+  meta = {
+    description = "NoSQL data storage service that can be accessed from anywhere";
+    homepage = "https://github.com/Azure/azure-sdk-for-python";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
