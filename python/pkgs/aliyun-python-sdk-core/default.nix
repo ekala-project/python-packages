0dc79b9c19de394ebccb4a82ed3d3f86f59c2195
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:48 -0700

    python3Packages.aliyun-python-sdk-core: init at 2.16.0

---
+++ python/pkgs/aliyun-python-sdk-core/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  cryptography,
+  fetchPypi,
+  jmespath,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "aliyun-python-sdk-core";
+  version = "2.16.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-ZRyq1ZfrOdT61s+FEz3/6Sg31TvfYtudjzfatlCLuPk=";
+  };
+
+  pythonRelaxDeps = true;
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    cryptography
+    jmespath
+  ];
+
+  # All components are stored in a mono repo
+  doCheck = false;
+
+  pythonImportsCheck = [ "aliyunsdkcore" ];
+
+  meta = {
+    description = "Core module of Aliyun Python SDK";
+    homepage = "https://github.com/aliyun/aliyun-openapi-python-sdk";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
