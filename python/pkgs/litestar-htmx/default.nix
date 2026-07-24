bf9a3c1512e64d84495c36ceb9a2e04734a1248f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:31:55 -0700

    python3Packages.litestar-htmx: init at 0.5.0

---
+++ python/pkgs/litestar-htmx/default.nix
@@ -0,0 +1,31 @@
+{
+  buildPythonPackage,
+  lib,
+  fetchPypi,
+  hatchling,
+}:
+
+buildPythonPackage rec {
+  pname = "litestar-htmx";
+  version = "0.5.0";
+
+  src = fetchPypi {
+    pname = "litestar_htmx";
+    inherit version;
+    hash = "sha256-4C0aOpIXLIdINfo+Z0nWWun8Ym0N9GcZSQoWKT4hRvs=";
+  };
+
+  pyproject = true;
+
+  build-system = [
+    hatchling
+  ];
+
+  meta = {
+    homepage = "https://docs.litestar.dev/latest/usage/htmx.html";
+    maintainers = [ ];
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.unix;
+    description = "HTMX Integration for Litesstar";
+  };
+}
