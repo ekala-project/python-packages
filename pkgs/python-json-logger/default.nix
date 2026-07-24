457d28b7bf4faa7c1eb34ff2d22f5472d82bb5fb
Jonathan Ringer <jonringer117@gmail.com>
2026-07-23 23:48:38 -0700

    python-json-logger: 4.0.0 -> 4.1.0

--- pkgs/python-json-logger/default.nix
+++ pkgs/python-json-logger/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "python-json-logger";
-  version = "4.0.0";
+  version = "4.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "nhairs";
     repo = "python-json-logger";
     tag = "v${version}";
-    hash = "sha256-YFtM+YWXDCaipqhQntdGeGbTmWO5XMGfpWhgQwicxFQ=";
+    hash = "sha256-hJ29rDP04apLf1zaP1vQrYF5xm4GtKZlvno97b7g+hg=";
   };
 
   build-system = [ setuptools ];
