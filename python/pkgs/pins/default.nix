8c18549b0a93691c9c8491be0e785b963c270d0c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 13:57:33 -0700

    python3Packages.pins: init at 0.9.1

---
+++ python/pkgs/pins/default.nix
@@ -0,0 +1,73 @@
+{
+  lib,
+  adlfs,
+  appdirs,
+  buildPythonPackage,
+  databackend,
+  fetchFromGitHub,
+  fsspec,
+  gcsfs,
+  humanize,
+  importlib-metadata,
+  importlib-resources,
+  jinja2,
+  joblib,
+  pandas,
+  pyyaml,
+  requests,
+  s3fs,
+  setuptools-scm,
+  setuptools,
+  typing-extensions,
+  xxhash,
+}:
+
+buildPythonPackage rec {
+  pname = "pins";
+  version = "0.9.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "rstudio";
+    repo = "pins-python";
+    tag = "v${version}";
+    hash = "sha256-fDbgas4RG4cJRqrISWmrMUQUycQindlqF9/jA5R1TF8=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  pythonRelaxDeps = [
+    "fsspec"
+  ];
+
+  dependencies = [
+    appdirs
+    databackend
+    fsspec
+    humanize
+    importlib-metadata
+    importlib-resources
+    jinja2
+    joblib
+    pandas
+    pyyaml
+    requests
+    typing-extensions
+    xxhash
+  ];
+
+  optional-dependencies = {
+    aws = [ s3fs ];
+    azure = [ adlfs ];
+    gcs = [ gcsfs ];
+  };
+  meta = {
+    description = "Module to publishes data, models and other Python objects";
+    homepage = "https://github.com/rstudio/pins-python";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
