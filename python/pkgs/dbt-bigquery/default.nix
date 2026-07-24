72ce4203cf4236a7f171883348a37b028499134c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:50 -0700

    python3Packages.dbt-bigquery: init at 1.9.1

---
+++ python/pkgs/dbt-bigquery/default.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  setuptools,
+
+  # dependencies
+  agate,
+  dbt-adapters,
+  dbt-common,
+  dbt-core,
+  google-cloud-bigquery,
+  google-cloud-dataproc,
+  google-cloud-storage,
+
+  # tests
+}:
+
+buildPythonPackage rec {
+  pname = "dbt-bigquery";
+  version = "1.9.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "dbt-labs";
+    repo = "dbt-bigquery";
+    tag = "v${version}";
+    hash = "sha256-YZA8lcUGoq5jMNS1GlbBd036X2F3khsZWr5Pv65zpPI=";
+  };
+
+  pythonRelaxDeps = [
+    "agate"
+    "google-cloud-storage"
+  ];
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
+    agate
+    dbt-adapters
+    dbt-common
+    dbt-core
+    google-cloud-bigquery
+    google-cloud-dataproc
+    google-cloud-storage
+  ];
+  pythonImportsCheck = [ "dbt.adapters.bigquery" ];
+
+  meta = {
+    description = "Plugin enabling dbt to operate on a BigQuery database";
+    homepage = "https://github.com/dbt-labs/dbt-bigquery";
+    license = lib.licenses.asl20;
+  };
+}
