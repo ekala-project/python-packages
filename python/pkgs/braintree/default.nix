4c7cb428dbea3dfabc56bc42b31150593bdd33e9
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:49 -0700

    python3Packages.braintree: init at 4.45.0

---
+++ python/pkgs/braintree/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  requests,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "braintree";
+  version = "4.45.0";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "braintree";
+    repo = "braintree_python";
+    tag = finalAttrs.version;
+    hash = "sha256-cD0TKqf2c/wdFwDc78rEPm4ucZkYS1nk2Uo0oTfcJvE=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ requests ];
+  pythonImportsCheck = [ "braintree" ];
+
+  # Most integration tests require a running Braintree gateway.
+  meta = {
+    description = "Python library for integration with Braintree";
+    homepage = "https://github.com/braintree/braintree_python";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
