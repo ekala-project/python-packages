836c130dd48780a46ca592c7a0817313d8789bda
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:59 -0700

    python3Packages.opentelemetry-instrumentation-httpx: init at unknown

---
+++ python/pkgs/opentelemetry-instrumentation-httpx/default.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  buildPythonPackage,
+  hatchling,
+  httpx,
+  opentelemetry-api,
+  opentelemetry-instrumentation,
+  opentelemetry-util-http,
+  opentelemetry-test-utils,
+  opentelemetry-semantic-conventions,
+  pythonOlder,
+  respx,
+}:
+
+buildPythonPackage {
+  inherit (opentelemetry-instrumentation) version src;
+  pname = "opentelemetry-instrumentation-httpx";
+  pyproject = true;
+
+  sourceRoot = "${opentelemetry-instrumentation.src.name}/instrumentation/opentelemetry-instrumentation-httpx";
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    httpx
+    opentelemetry-api
+    opentelemetry-instrumentation
+    opentelemetry-semantic-conventions
+    opentelemetry-util-http
+  ];
+  doCheck = pythonOlder "3.14";
+
+  pythonImportsCheck = [ "opentelemetry.instrumentation.httpx" ];
+
+  meta = opentelemetry-instrumentation.meta // {
+    homepage = "https://github.com/open-telemetry/opentelemetry-python-contrib/blob/main/instrumentation/opentelemetry-instrumentation-httpx";
+    description = "Allows tracing HTTP requests made by the httpx library";
+  };
+}
