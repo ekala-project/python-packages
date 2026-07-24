daa7fc873747fb8f49d05347ea824bca3ab1dd24
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:52 -0700

    python3Packages.kgb: init at 7.2

---
+++ python/pkgs/kgb/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "kgb";
+  version = "7.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "beanbaginc";
+    repo = "kgb";
+    tag = "release-${version}";
+    hash = "sha256-hNJXoUIyrCB9PCWLCmN81F6pBRwZApDR6JWA0adyklw=";
+  };
+
+  postPatch = ''
+    substituteInPlace setup.cfg \
+      --replace-fail "tag_build = .dev" ""
+  '';
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "kgb" ];
+  meta = {
+    description = "Python function spy support for unit tests";
+    homepage = "https://github.com/beanbaginc/kgb";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
