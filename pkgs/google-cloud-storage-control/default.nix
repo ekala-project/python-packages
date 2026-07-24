989cda10524cfd2b6cbfd08055bb9c6e0d0bd09c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 00:29:27 -0700

    python3Packages.google-cloud-storage-control: init at 1.12.0

---
+++ pkgs/google-cloud-storage-control/default.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  google-api-core,
+  google-auth,
+  grpc-google-iam-v1,
+  grpcio,
+  proto-plus,
+  protobuf,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "google-cloud-storage-control";
+  version = "1.12.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "googleapis";
+    repo = "google-cloud-python";
+    tag = "google-cloud-storage-control-v${finalAttrs.version}";
+    hash = "sha256-M/7uDWWz4YCfxa4gyM9BaAo10iyTMvtR2MhNpdFYnis=";
+  };
+
+  sourceRoot = "${finalAttrs.src.name}/packages/google-cloud-storage-control";
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    google-api-core
+    google-auth
+    grpc-google-iam-v1
+    grpcio
+    proto-plus
+    protobuf
+  ]
+  ++ google-api-core.optional-dependencies.grpc;
+
+  pythonImportsCheck = [
+    "google.cloud.storage_control"
+    "google.cloud.storage_control_v2"
+  ];
+
+  meta = {
+    description = "Google Cloud Storage Control API client library";
+    homepage = "https://github.com/googleapis/google-cloud-python/tree/main/packages/google-cloud-storage-control";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
