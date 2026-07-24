890bdfc7904192f3437bcefce5f0b031d46b3430
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:59 -0700

    python3Packages.opentelemetry-instrumentation-redis: init at unknown

---
+++ python/pkgs/opentelemetry-instrumentation-redis/default.nix
@@ -0,0 +1,38 @@
+{
+  buildPythonPackage,
+  hatchling,
+  opentelemetry-api,
+  opentelemetry-instrumentation,
+  opentelemetry-semantic-conventions,
+  wrapt,
+  redis,
+  opentelemetry-test-utils,
+  fakeredis,
+}:
+
+buildPythonPackage {
+  inherit (opentelemetry-instrumentation) version src;
+  pname = "opentelemetry-instrumentation-redis";
+  pyproject = true;
+
+  sourceRoot = "${opentelemetry-instrumentation.src.name}/instrumentation/opentelemetry-instrumentation-redis";
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    opentelemetry-api
+    opentelemetry-instrumentation
+    opentelemetry-semantic-conventions
+    wrapt
+  ];
+  optional-dependencies = {
+    instruments = [ redis ];
+  };
+
+  pythonImportsCheck = [ "opentelemetry.instrumentation.redis" ];
+
+  meta = opentelemetry-instrumentation.meta // {
+    homepage = "https://github.com/open-telemetry/opentelemetry-python-contrib/blob/main/instrumentation/opentelemetry-instrumentation-redis";
+    description = "Redis instrumentation for OpenTelemetry";
+  };
+}
