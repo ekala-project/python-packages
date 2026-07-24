e6e7d7500d162cc587ff63ff77be28c43bfcc0f7
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:49 -0700

    python3Packages.cssutils: init at 2.15.0

---
+++ python/pkgs/cssutils/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools-scm,
+  encutils,
+  more-itertools,
+  cssselect,
+  jaraco-test,
+  lxml,
+  mock,
+}:
+
+buildPythonPackage rec {
+  pname = "cssutils";
+  version = "2.15.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jaraco";
+    repo = "cssutils";
+    tag = "v${version}";
+    hash = "sha256-K9jbuX7AueSB3AB7PAVjpQhzb3Umn9OoHaL4RrMzKEs=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail '"coherent.licensed",' ""
+  '';
+
+  build-system = [ setuptools-scm ];
+
+  dependencies = [
+    encutils
+    more-itertools
+  ];
+  pythonImportsCheck = [ "cssutils" ];
+
+  meta = {
+    description = "CSS Cascading Style Sheets library for Python";
+    homepage = "https://github.com/jaraco/cssutils";
+    license = lib.licenses.lgpl3Plus;
+    maintainers = [ ];
+  };
+}
