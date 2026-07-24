32bd57745c019708bc6297f9ffca04043858ec6d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:57 -0700

    python3Packages.tableauserverclient: init at 0.38

---
+++ python/pkgs/tableauserverclient/default.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  buildPythonPackage,
+  defusedxml,
+  fetchPypi,
+  packaging,
+  requests,
+  requests-mock,
+  setuptools,
+  typing-extensions,
+  versioneer,
+}:
+
+buildPythonPackage rec {
+  pname = "tableauserverclient";
+  version = "0.38";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-Td2QW10vsKojhk9eeO90QbArdIuNn+hbNk9LvCYwgyo=";
+  };
+
+  pythonRelaxDeps = [
+    "defusedxml"
+    "urllib3"
+  ];
+
+  build-system = [
+    setuptools
+    versioneer
+  ];
+
+  dependencies = [
+    defusedxml
+    requests
+    packaging
+    typing-extensions
+  ];
+  # Tests attempt to create some file artifacts and fails
+  doCheck = false;
+
+  pythonImportsCheck = [ "tableauserverclient" ];
+
+  meta = {
+    description = "Module for working with the Tableau Server REST API";
+    homepage = "https://github.com/tableau/server-client-python";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
