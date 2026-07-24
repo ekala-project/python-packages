9567996f15a20916f184740f32d85a5acb444595
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 13:51:31 -0700

    python3Packages.gspread: init at 6.2.1

---
+++ python/pkgs/gspread/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  flit-core,
+  google-auth,
+  google-auth-oauthlib,
+  strenum,
+}:
+
+buildPythonPackage rec {
+  pname = "gspread";
+  version = "6.2.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "burnash";
+    repo = "gspread";
+    tag = "v${version}";
+    hash = "sha256-j7UNti5N8c1mjw+1qTPIRCWJ6M4Ur0P9sG1uJnp170M=";
+  };
+
+  nativeBuildInputs = [ flit-core ];
+
+  propagatedBuildInputs = [
+    google-auth
+    google-auth-oauthlib
+    strenum
+  ];
+  pythonImportsCheck = [ "gspread" ];
+
+  meta = {
+    description = "Google Spreadsheets client library";
+    homepage = "https://github.com/burnash/gspread";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
