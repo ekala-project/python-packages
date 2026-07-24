b711fd73d224443746c505ce6d434d33a1ce1b9d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:50 -0700

    python3Packages.dateparser: init at 1.4.1

---
+++ python/pkgs/dateparser/default.nix
@@ -0,0 +1,64 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  python-dateutil,
+  pytz,
+  regex,
+  tzlocal,
+  hijridate,
+  convertdate,
+  fasttext,
+  numpy,
+  langdetect,
+  parameterized,
+  gitpython,
+  parsel,
+  requests,
+  ruamel-yaml,
+}:
+
+buildPythonPackage rec {
+  pname = "dateparser";
+  version = "1.4.1";
+
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "scrapinghub";
+    repo = "dateparser";
+    tag = "v${version}";
+    hash = "sha256-TA4GZb24++RF1sw4tECJF5UzouRCwwhPiim5z5/hMzU=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    python-dateutil
+    pytz
+    regex
+    tzlocal
+  ];
+
+  optional-dependencies = {
+    calendars = [
+      hijridate
+      convertdate
+    ];
+    fasttext = [
+      fasttext
+      numpy
+    ];
+    langdetect = [ langdetect ];
+  };
+  pythonImportsCheck = [ "dateparser" ];
+
+  meta = {
+    description = "Date parsing library designed to parse dates from HTML pages";
+    homepage = "https://github.com/scrapinghub/dateparser";
+    license = lib.licenses.bsd3;
+    mainProgram = "dateparser-download";
+    maintainers = [ ];
+  };
+}
