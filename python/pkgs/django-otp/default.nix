33defe7b270486c36a1240960e7efabcdde2461b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:50 -0700

    python3Packages.django-otp: init at 1.7.0

---
+++ python/pkgs/django-otp/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  django,
+  freezegun,
+  qrcode,
+  pytest,
+  python,
+}:
+
+buildPythonPackage rec {
+  pname = "django-otp";
+  version = "1.7.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "django-otp";
+    repo = "django-otp";
+    tag = "v${version}";
+    hash = "sha256-Tqi6FHXJToOJsGETgIRl8rOUTfkn3kBkG5/bI8CxT24=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    django
+    qrcode
+  ];
+
+  env.DJANGO_SETTINGS_MODUOLE = "test.test_project.settings";
+  checkPhase = ''
+    runHook preCheck
+
+    export PYTHONPATH=$PYTHONPATH:test
+    export DJANGO_SETTINGS_MODULE=test_project.settings
+    ${python.interpreter} -m django test django_otp
+
+    runHook postCheck
+  '';
+  pythonImportsCheck = [ "django_otp" ];
+
+  meta = {
+    homepage = "https://github.com/django-otp/django-otp";
+    description = "Pluggable framework for adding two-factor authentication to Django using one-time passwords";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
