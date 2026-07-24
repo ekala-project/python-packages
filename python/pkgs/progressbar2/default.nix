57dcbdd7252c87a8f57ec26970e266f897de29de
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:54 -0700

    python3Packages.progressbar2: init at 4.5.0

---
+++ python/pkgs/progressbar2/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  dill,
+  freezegun,
+  python-utils,
+  setuptools,
+  setuptools-scm,
+}:
+
+buildPythonPackage rec {
+  pname = "progressbar2";
+  version = "4.5.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-ZmLLYkiG7THrlNr2HidYO1FE68c4Ohe64Hb49PWQiPs=";
+  };
+
+  postPatch = ''
+    sed -i "/-cov/d" pytest.ini
+  '';
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  propagatedBuildInputs = [ python-utils ];
+  pythonImportsCheck = [ "progressbar" ];
+
+  meta = {
+    description = "Text progressbar library";
+    homepage = "https://progressbar-2.readthedocs.io/";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
