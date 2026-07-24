ee228e5803bb0183ed63c0d744c65433f9f84442
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:31:56 -0700

    python3Packages.mkdocs-git-revision-date-localized-plugin: init at 1.5.3

---
+++ python/pkgs/mkdocs-git-revision-date-localized-plugin/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  setuptools-scm,
+  babel,
+  gitpython,
+  mkdocs,
+  pytz,
+}:
+
+buildPythonPackage rec {
+  pname = "mkdocs-git-revision-date-localized-plugin";
+  version = "1.5.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "timvink";
+    repo = "mkdocs-git-revision-date-localized-plugin";
+    tag = "v${version}";
+    hash = "sha256-Fk8xh40uQY15iCkDY/0y0y4hMAHo07cfLXL1ZyFp30w=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    babel
+    gitpython
+    mkdocs
+    pytz
+  ];
+  pythonImportsCheck = [ "mkdocs_git_revision_date_localized_plugin" ];
+
+  meta = {
+    description = "MkDocs plugin that enables displaying the date of the last git modification of a page";
+    homepage = "https://github.com/timvink/mkdocs-git-revision-date-localized-plugin";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
