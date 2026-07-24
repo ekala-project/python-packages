bfbbdd3b3020314eab6d4e67a35433d1258e4893
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 14:00:49 -0700

    python3Packages.snuggs: init at 1.4.7

---
+++ python/pkgs/snuggs/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  numpy,
+  setuptools,
+  pyparsing,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "snuggs";
+  version = "1.4.7";
+  pyproject = true;
+
+  # Pypi doesn't ship the tests, so we fetch directly from GitHub
+  src = fetchFromGitHub {
+    owner = "mapbox";
+    repo = "snuggs";
+    tag = finalAttrs.version;
+    hash = "sha256-jj8H9Rgq0MSOObDiN3UXXtx67BY+jKTXz1ZTL3SCdNw=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    numpy
+    pyparsing
+  ];
+  meta = {
+    description = "S-expressions for Numpy";
+    license = lib.licenses.mit;
+    homepage = "https://github.com/mapbox/snuggs";
+    maintainers = [ ];
+  };
+})
