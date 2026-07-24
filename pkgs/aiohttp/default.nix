24c0dde3cc8c39d5584572b0d5e43cfb280df190
Jonathan Ringer <jonringer117@gmail.com>
2026-07-23 23:34:34 -0700

    aiohttp: 3.13.2 -> 3.14.3

--- pkgs/aiohttp/default.nix
+++ pkgs/aiohttp/default.nix
@@ -34,14 +34,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "aiohttp";
-  version = "3.13.2";
+  version = "3.14.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "aio-libs";
     repo = "aiohttp";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-LqYGrrWgSZazk0hjQvTFwqtU/PtMEaPi+m1Ya8Ds+pU=";
+    hash = "sha256-n8LH34N9V2Smqc23q/49gqRbP0U1glJAYiyPEGFtEmM=";
   };
 
   postPatch = ''
