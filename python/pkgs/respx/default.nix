d3cb6b5068279b4798c0d5b3a502a5163cf88938
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:26:05 -0700

    python3Packages.respx: init at 0.23.1

---
+++ python/pkgs/respx/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  flask,
+  httpcore,
+  httpx,
+  pytest-asyncio,
+  pytest-cov-stub,
+  setuptools,
+  starlette,
+  trio,
+}:
+
+buildPythonPackage rec {
+  pname = "respx";
+  version = "0.23.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "lundberg";
+    repo = "respx";
+    tag = version;
+    hash = "sha256-Fz3CS9rIm+H6axVIRErlFTTDgtAcTmGj4/wabLxDV2I=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ httpx ];
+  pythonImportsCheck = [ "respx" ];
+
+  meta = {
+    description = "Python library for mocking HTTPX";
+    homepage = "https://lundberg.github.io/respx/";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
