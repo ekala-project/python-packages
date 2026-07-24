8bde519f9cd99accaa9eb83c532640630c2c60b0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:53 -0700

    python3Packages.lru-dict: init at 1.4.0

---
+++ python/pkgs/lru-dict/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "lru-dict";
+  version = "1.4.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "amitdev";
+    repo = "lru-dict";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-pHjBTAXoOUyTSzzHzOBZeMFkJhzspylMhxwqXYLFOQg=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "setuptools==" "setuptools>="
+  '';
+
+  build-system = [ setuptools ];
+  pythonImportsCheck = [ "lru" ];
+
+  meta = {
+    description = "Fast and memory efficient LRU cache for Python";
+    homepage = "https://github.com/amitdev/lru-dict";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
