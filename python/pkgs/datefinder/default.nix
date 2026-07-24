40cf3c59fe3c9fa85ff16ec8f70b8cc5147dbd4d
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:50 -0700

    python3Packages.datefinder: init at 0.7.3

---
+++ python/pkgs/datefinder/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  python-dateutil,
+  pytz,
+  regex,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "datefinder";
+  version = "0.7.3";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "akoumjian";
+    repo = "datefinder";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-uOSwS+mHgbvEL+rTfs4Ax9NvJnhYemxFVqqDssy2i7g=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    regex
+    pytz
+    python-dateutil
+  ];
+  pythonImportsCheck = [ "datefinder" ];
+
+  meta = {
+    description = "Extract datetime objects from strings";
+    homepage = "https://github.com/akoumjian/datefinder";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
