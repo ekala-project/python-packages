6cfe24dd13e078e3d845cf8e261ac1a6f18130f2
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:51 -0700

    python3Packages.django-allauth: init at 65.18.0

---
+++ python/pkgs/django-allauth/default.nix
@@ -0,0 +1,103 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromCodeberg,
+  python,
+
+  # build-system
+  setuptools,
+  setuptools-scm,
+
+  # build-time dependencies
+  gettext,
+
+  # dependencies
+  asgiref,
+  django,
+
+  # optional-dependencies
+  fido2,
+  oauthlib,
+  python3-openid,
+  python3-saml,
+  requests,
+  requests-oauthlib,
+  pyjwt,
+  qrcode,
+
+  # tests
+  django-ninja,
+  djangorestframework,
+  pillow,
+  psycopg2,
+  pytest-asyncio,
+  pytest-django,
+  pyyaml,
+
+  # passthru tests
+  dj-rest-auth,
+}:
+
+buildPythonPackage rec {
+  pname = "django-allauth";
+  version = "65.18.0";
+  pyproject = true;
+
+  src = fetchFromCodeberg {
+    owner = "allauth";
+    repo = "django-allauth";
+    tag = version;
+    hash = "sha256-OUU42yld3CTutgu7XOkC/f4U2Yo9HpQV8GCfZsM2P5w=";
+  };
+
+  nativeBuildInputs = [ gettext ];
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    asgiref
+    django
+  ];
+
+  preBuild = ''
+    ${python.pythonOnBuildForHost.interpreter} -m django compilemessages
+  '';
+
+  optional-dependencies = {
+    headless = [
+      pyjwt
+    ]
+    ++ pyjwt.optional-dependencies.crypto;
+    headless-spec = [ pyyaml ];
+    idp-oidc = [
+      oauthlib
+      pyjwt
+    ]
+    ++ pyjwt.optional-dependencies.crypto;
+    mfa = [
+      fido2
+      qrcode
+    ];
+    openid = [ python3-openid ];
+    saml = [ python3-saml ];
+    socialaccount = [
+      requests
+      requests-oauthlib
+      pyjwt
+    ]
+    ++ pyjwt.optional-dependencies.crypto;
+    steam = [ python3-openid ];
+  };
+
+  pythonImportsCheck = [ "allauth" ];
+  meta = {
+    description = "Integrated set of Django applications addressing authentication, registration, account management as well as 3rd party (social) account authentication";
+    downloadPage = "https://codeberg.org/allauth/django-allauth";
+    homepage = "https://allauth.org";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
