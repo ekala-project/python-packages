505664a91c0ac92b0b01866e216079eb79053bb9
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:58 -0700

    python3Packages.motor: init at 3.7.1

---
+++ python/pkgs/motor/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  hatch-requirements-txt,
+  mockupdb,
+  pymongo,
+}:
+
+buildPythonPackage rec {
+  pname = "motor";
+  version = "3.7.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "mongodb";
+    repo = "motor";
+    tag = version;
+    hash = "sha256-ul2GKzSiAewwGEuCpQQ61h3cqrJikaJeKs5KlX+aAjo=";
+  };
+
+  build-system = [
+    hatchling
+    hatch-requirements-txt
+  ];
+
+  pythonRelaxDeps = [ "pymongo" ];
+
+  dependencies = [ pymongo ];
+  # network connections
+  doCheck = false;
+
+  pythonImportsCheck = [ "motor" ];
+
+  meta = {
+    description = "Non-blocking MongoDB driver for Tornado or asyncio";
+    license = lib.licenses.asl20;
+    homepage = "https://github.com/mongodb/motor";
+    maintainers = [ ];
+  };
+}
