f66cda59e0e8d785b9b478f5151bdbe10bb720bd
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:56 -0700

    python3Packages.latex2mathml: init at 3.81.0

---
+++ python/pkgs/latex2mathml/default.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  uv-build,
+  pytest-cov-stub,
+  multidict,
+  syrupy,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "latex2mathml";
+  version = "3.81.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "roniemartinez";
+    repo = "latex2mathml";
+    tag = finalAttrs.version;
+    hash = "sha256-NY8SVEN9i8OcT8YS8887/TgLuIYAsS26me2BqGW0ubs=";
+  };
+
+  build-system = [ uv-build ];
+  # nixpkgs is only at uv_build 0.10.0
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail 'uv_build>=0.10.11,<0.11.0' 'uv_build'
+  '';
+
+  pythonImportsCheck = [ "latex2mathml" ];
+
+  meta = {
+    description = "Pure Python library for LaTeX to MathML conversion";
+    homepage = "https://github.com/roniemartinez/latex2mathml";
+    license = lib.licenses.mit;
+    mainProgram = "latex2mathml";
+    maintainers = [ ];
+  };
+})
