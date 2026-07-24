9d94439701e6489fe9518a35f2835c7e8b8b3e87
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:37:46 -0700

    python3Packages.cons: init at 0.4.7

---
+++ python/pkgs/cons/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  logical-unification,
+  py,
+  pytest-html,
+  setuptools,
+  setuptools-scm,
+}:
+
+buildPythonPackage rec {
+  pname = "cons";
+  version = "0.4.7";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pythological";
+    repo = "python-cons";
+    tag = "v${version}";
+    hash = "sha256-BS7lThnv+dxtztvw2aRhQa8yx2cRfrZLiXjcwvZ8QR0=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [ logical-unification ];
+  pythonImportsCheck = [ "cons" ];
+
+  meta = {
+    description = "Implementation of Lisp/Scheme-like cons in Python";
+    homepage = "https://github.com/pythological/python-cons";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+  };
+}
