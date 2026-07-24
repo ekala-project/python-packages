8cac62617dd2a15cf98677c876afdc681fcc9ae7
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 14:01:13 -0700

    python3Packages.supabase: init at 2.29.0

---
+++ python/pkgs/supabase/default.nix
@@ -0,0 +1,64 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  uv-build,
+  realtime,
+  supabase-functions,
+  supabase-auth,
+  postgrest,
+  httpx,
+  yarl,
+  storage3,
+  pytestCheckHook,
+  python-dotenv,
+  pytest-asyncio,
+  pytest-cov-stub,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "supabase";
+  version = "2.29.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "supabase";
+    repo = "supabase-py";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-LaSlAYFvx/HHdfmc9J+KScVQ9JFGS98Yfihzn8F7t3g=";
+  };
+
+  sourceRoot = "${finalAttrs.src.name}/src/supabase";
+
+  build-system = [ uv-build ];
+  dependencies = [
+    realtime
+    supabase-auth
+    supabase-functions
+    postgrest
+    httpx
+    yarl
+    storage3
+  ];
+
+  nativeBuildInputs = [
+    pytestCheckHook
+    python-dotenv
+    pytest-asyncio
+    pytest-cov-stub
+  ];
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail 'uv_build>=0.8.3,<0.9.0' 'uv_build>=0.8.3'
+  '';
+
+  pythonImportsCheck = [ "supabase" ];
+
+  meta = {
+    homepage = "https://github.com/supabase/supabase-py";
+    license = lib.licenses.mit;
+    description = "Supabase client for Python";
+    maintainers = [ ];
+  };
+})
