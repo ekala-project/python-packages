89e0370c9d142303eec1839e4cb5ba99f457eba9
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:59 -0700

    python3Packages.opentelemetry-instrumentation-system-metrics: init at unknown

---
+++ python/pkgs/opentelemetry-instrumentation-system-metrics/default.nix
@@ -0,0 +1,31 @@
+{
+  buildPythonPackage,
+  pythonOlder,
+  hatchling,
+  opentelemetry-instrumentation,
+  opentelemetry-api,
+  opentelemetry-test-utils,
+  psutil,
+}:
+
+buildPythonPackage {
+  inherit (opentelemetry-instrumentation) version src;
+  pname = "opentelemetry-instrumentation-system-metrics";
+  pyproject = true;
+
+  sourceRoot = "${opentelemetry-instrumentation.src.name}/instrumentation/opentelemetry-instrumentation-system-metrics";
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    opentelemetry-instrumentation
+    opentelemetry-api
+    psutil
+  ];
+  pythonImportsCheck = [ "opentelemetry.instrumentation.system_metrics" ];
+
+  meta = opentelemetry-instrumentation.meta // {
+    homepage = "https://github.com/open-telemetry/opentelemetry-python-contrib/blob/main/instrumentation/opentelemetry-instrumentation-system-metrics";
+    description = "OpenTelemetry System Metrics Instrumentation";
+  };
+}
