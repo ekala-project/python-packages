735888771b3fe6f63ff99b14160936de8a0e79f3
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:25:56 -0700

    python3Packages.kagglesdk: init at 0.1.34

---
+++ python/pkgs/kagglesdk/default.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  hatchling,
+
+  # dependencies
+  protobuf,
+  requests,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "kagglesdk";
+  version = "0.1.34";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "Kaggle";
+    repo = "kagglesdk";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Yd4QZpSdcOWpEqDGf+uFe4JGDknSjkjTYWn1j89oRLM=";
+  };
+
+  build-system = [
+    hatchling
+  ];
+
+  dependencies = [
+    protobuf
+    requests
+  ];
+
+  pythonImportsCheck = [ "kagglesdk" ];
+
+  # The two available tests fail with:
+  # AssertionError: 'https://api.kaggle.com' != 'https://www.kaggle.com'
+  doCheck = false;
+
+  meta = {
+    description = "Bindings to access Kaggle endpoints";
+    homepage = "https://github.com/Kaggle/kagglesdk";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
