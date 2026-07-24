e277e55cd713282987685682214b6aba018a2b61
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:48 -0700

    python3Packages.apsw: init at 3.51.0.0

---
+++ python/pkgs/apsw/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchurl,
+  setuptools,
+  sqlite,
+}:
+
+buildPythonPackage rec {
+  pname = "apsw";
+  version = "3.51.0.0";
+  pyproject = true;
+
+  # https://github.com/rogerbinns/apsw/issues/548
+  src = fetchurl {
+    url = "https://github.com/rogerbinns/apsw/releases/download/${version}/apsw-${version}.tar.gz";
+    hash = "sha256-8I1/HnGO9eOs9CUFwvN5BcpHtCxXD7qlF9WBA4E1Rls=";
+  };
+
+  build-system = [ setuptools ];
+
+  buildInputs = [ sqlite ];
+
+  # apsw explicitly doesn't use pytest
+  # see https://github.com/rogerbinns/apsw/issues/548#issuecomment-2891633403
+  checkPhase = ''
+    runHook preCheck
+    python -m apsw.tests
+    runHook postCheck
+  '';
+
+  pythonImportsCheck = [ "apsw" ];
+
+  meta = {
+    description = "Python wrapper for the SQLite embedded relational database engine";
+    homepage = "https://github.com/rogerbinns/apsw";
+    license = lib.licenses.zlib;
+    maintainers = [ ];
+  };
+}
