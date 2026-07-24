f60769a0356cf733495d9343b109f4ffd5414151
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 13:29:58 -0700

    python3Packages.pytest-test-utils: init at 0.1.0

---
+++ python/pkgs/pytest-test-utils/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  setuptools-scm,
+  pytest,
+}:
+
+buildPythonPackage rec {
+  pname = "pytest-test-utils";
+  version = "0.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "iterative";
+    repo = "pytest-test-utils";
+    tag = version;
+    hash = "sha256-19oNAFff++7ntMdlnMXYc2w5I+EzGwWJh+rB1IjNZGk=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  buildInputs = [ pytest ];
+  pythonImportsCheck = [ "pytest_test_utils" ];
+
+  meta = {
+    description = "Pytest utilities for tests";
+    homepage = "https://github.com/iterative/pytest-test-utils";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
