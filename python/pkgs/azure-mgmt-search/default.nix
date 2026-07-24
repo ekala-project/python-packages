f2f8db18a874ea690ae6631447d2cf07d3fa20a0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:49 -0700

    python3Packages.azure-mgmt-search: init at 9.2.0

---
+++ python/pkgs/azure-mgmt-search/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  azure-common,
+  azure-mgmt-core,
+  buildPythonPackage,
+  fetchPypi,
+  isodate,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "azure-mgmt-search";
+  version = "9.2.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "azure_mgmt_search";
+    inherit version;
+    hash = "sha256-oNoOwzLR9D0PastjuM/YAIWwdeka/PgS+MdprZ/crYQ=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    azure-common
+    azure-mgmt-core
+    isodate
+  ];
+
+  # Module has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "azure.mgmt.search" ];
+
+  meta = {
+    description = "This is the Microsoft Azure Search Management Client Library";
+    homepage = "https://github.com/Azure/azure-sdk-for-python";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
