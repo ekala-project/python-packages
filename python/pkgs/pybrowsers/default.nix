e5358b3a388f06b3f9982274810b215c87b72cb0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:26:01 -0700

    python3Packages.pybrowsers: init at 1.3.2

---
+++ python/pkgs/pybrowsers/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  poetry-core,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pybrowsers";
+  version = "1.3.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "roniemartinez";
+    repo = "browsers";
+    tag = finalAttrs.version;
+    hash = "sha256-MpTCeu2rxIx6JByosL2C3hayrMIfKD/2kZT3AJpjKZw=";
+  };
+
+  build-system = [ poetry-core ];
+
+  # Tests want to interact with actual browsers
+  doCheck = false;
+
+  pythonImportsCheck = [ "browsers" ];
+
+  meta = {
+    description = "Python library for detecting and launching browsers";
+    homepage = "https://github.com/roniemartinez/browsers";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
