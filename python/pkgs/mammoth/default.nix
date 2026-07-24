e7e3467f3129fa19d709b68e44000bbfe555c06f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:37:58 -0700

    python3Packages.mammoth: init at 1.12.0

---
+++ python/pkgs/mammoth/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  cobble,
+  funk,
+  spur,
+  tempman,
+  gitUpdater,
+}:
+
+buildPythonPackage rec {
+  pname = "mammoth";
+  version = "1.12.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "mwilliamson";
+    repo = "python-mammoth";
+    tag = version;
+    hash = "sha256-8aqO5JPiyONu5rfR76qrutPHGE4nJn+716uPtYlZ+xA=";
+  };
+
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace-fail 'read("README")' '""'
+  '';
+
+  build-system = [ setuptools ];
+
+  dependencies = [ cobble ];
+
+  pythonImportsCheck = [ "mammoth" ];
+  passthru.updateScript = gitUpdater { };
+
+  meta = {
+    description = "Convert Word documents (.docx files) to HTML";
+    homepage = "https://github.com/mwilliamson/python-mammoth";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
