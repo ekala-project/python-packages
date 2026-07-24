2483a8072807cce4e21cd02d848f0e1d34005b50
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:53 -0700

    python3Packages.marshmallow-oneofschema: init at 3.2.0

---
+++ python/pkgs/marshmallow-oneofschema/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  marshmallow,
+  flit-core,
+}:
+
+buildPythonPackage rec {
+  pname = "marshmallow-oneofschema";
+  version = "3.2.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "marshmallow-code";
+    repo = "marshmallow-oneofschema";
+    tag = version;
+    hash = "sha256-Hk36wxZV1hVqIbqDOkEDlqABRKE6s/NyA/yBEXzj/yM=";
+  };
+
+  nativeBuildInputs = [ flit-core ];
+
+  propagatedBuildInputs = [ marshmallow ];
+  pythonImportsCheck = [ "marshmallow_oneofschema" ];
+
+  meta = {
+    description = "Marshmallow library extension that allows schema (de)multiplexing";
+    homepage = "https://github.com/marshmallow-code/marshmallow-oneofschema";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
