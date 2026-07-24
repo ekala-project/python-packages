755b20c7d84b8bf4c5e5eef8900322ca4b9c9dde
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:49:59 -0700

    python3Packages.laces: init at 0.1.2

---
+++ python/pkgs/laces/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildPythonPackage,
+  django,
+  fetchFromGitHub,
+  flit-core,
+}:
+
+buildPythonPackage rec {
+  pname = "laces";
+  version = "0.1.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "tbrlpld";
+    repo = "laces";
+    tag = "v${version}";
+    hash = "sha256-ELpPq7pqcLfAqUuHh8NOAOOiGPDImTFsA7WUHvVfMiI=";
+  };
+
+  nativeBuildInputs = [ flit-core ];
+
+  propagatedBuildInputs = [ django ];
+
+  pythonImportsCheck = [ "laces" ];
+
+  meta = {
+    description = "Django components that know how to render themselves";
+    homepage = "https://github.com/tbrlpld/laces";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
