5263351a4d27b272573e1f6e3d3fb9d995fd5c06
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:52 -0700

    python3Packages.icdiff: init at 2.0.10

---
+++ python/pkgs/icdiff/default.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  stdenv,
+  python,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  pythonAtLeast,
+  pkgs,
+}:
+let
+  inherit (pkgs) bash git less;
+in
+
+buildPythonPackage rec {
+  pname = "icdiff";
+  version = "2.0.10";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jeffkaufman";
+    repo = "icdiff";
+    tag = "release-${version}";
+    hash = "sha256-7/EvuHNWE9kdb35TFuqT8ShjyciodsRNkBMG0WvTy1c=";
+  };
+
+  patches = [ ./0001-Don-t-test-black-or-flake8.patch ];
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "icdiff" ];
+  # Before the wheel gets created, fix up the shebangs.
+  preBuild = ''
+    patchShebangs test.sh icdiff git-icdiff
+  '';
+
+  # Odd behavior in the sandbox
+  doCheck = !stdenv.hostPlatform.isDarwin;
+
+  checkPhase = ''
+    runHook preCheck
+
+    ./test.sh ${python.interpreter}
+
+    runHook postCheck
+  '';
+
+  meta = {
+    description = "Improved colorized diff";
+    homepage = "https://github.com/jeffkaufman/icdiff";
+    license = lib.licenses.psfl;
+    maintainers = [ ];
+  };
+}
