bbe3c126dd046fe366ab7cdf036614ebcdfcaa85
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:26:10 -0700

    python3Packages.zope-deprecation: init at 6.0

---
+++ python/pkgs/zope-deprecation/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  zope-testrunner,
+}:
+
+buildPythonPackage rec {
+  pname = "zope-deprecation";
+  version = "6.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "zopefoundation";
+    repo = "zope.deprecation";
+    tag = version;
+    hash = "sha256-N/+RtilRY/8NfhUjd/Y4T6dmZHt6PW4ofP1UE8Aj1e8=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "setuptools ==" "setuptools >="
+  '';
+
+  build-system = [ setuptools ];
+  checkPhase = ''
+    runHook preCheck
+
+    zope-testrunner --test-path=src
+
+    runHook postCheck
+  '';
+
+  pythonImportsCheck = [ "zope.deprecation" ];
+
+  pythonNamespaces = [ "zope" ];
+
+  meta = {
+    homepage = "https://github.com/zopefoundation/zope.deprecation";
+    description = "Zope Deprecation Infrastructure";
+    license = lib.licenses.zpl21;
+    maintainers = [ ];
+  };
+}
