bb6960699fab8e08a6cf6296b08f95916d1ba745
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:44:00 -0700

    python3Packages.minikanren: init at 1.0.5

---
+++ python/pkgs/minikanren/default.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  setuptools,
+  setuptools-scm,
+
+  # dependencies
+  cons,
+  etuples,
+  logical-unification,
+  multipledispatch,
+  toolz,
+  typing-extensions,
+
+  # tests
+  py,
+  pytest-html,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "minikanren";
+  version = "1.0.5";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "pythological";
+    repo = "kanren";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-lCQ0mKT99zK5A74uoo/9bP+eFdm3MC43Fh8+P2krXrs=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    cons
+    etuples
+    logical-unification
+    multipledispatch
+    toolz
+    typing-extensions
+  ];
+  pythonImportsCheck = [ "kanren" ];
+
+  meta = {
+    description = "Relational programming in Python";
+    homepage = "https://github.com/pythological/kanren";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+})
