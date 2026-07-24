9eb4cd86ff4872711dc86e990ffcf200986b3666
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:48 -0700

    python3Packages.audioread: init at 3.1.0

---
+++ python/pkgs/audioread/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  poetry-core,
+  standard-aifc,
+  standard-sunau,
+  ffmpeg-headless,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "audioread";
+  version = "3.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "sampsyo";
+    repo = "audioread";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-QvgwjUGuzeHH69YAdZdImjMT+9t4YxAukbuZKk0lBro=";
+  };
+
+  build-system = [ poetry-core ];
+
+  dependencies = [
+    standard-aifc
+    standard-sunau
+  ];
+  meta = {
+    description = "Cross-platform audio decoding";
+    homepage = "https://github.com/sampsyo/audioread";
+    license = lib.licenses.mit;
+  };
+})
