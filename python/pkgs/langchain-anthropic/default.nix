2d01bd5514133d893b8bec8bb78cece6d05d6429
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:52 -0700

    python3Packages.langchain-anthropic: init at 1.4.8

---
+++ python/pkgs/langchain-anthropic/default.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  hatchling,
+
+  # dependencies
+  anthropic,
+  langchain-core,
+  pydantic,
+
+  # tests
+  blockbuster,
+  langchain,
+  langchain-tests,
+  pytest-asyncio,
+  gitUpdater,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "langchain-anthropic";
+  version = "1.4.8";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "langchain-ai";
+    repo = "langchain";
+    tag = "langchain-anthropic==${finalAttrs.version}";
+    hash = "sha256-MX+DhFEkRNZ3IEKMXFT61XR6hEx2WPdGGaA0b/KlPZE=";
+  };
+
+  sourceRoot = "${finalAttrs.src.name}/libs/partners/anthropic";
+
+  build-system = [ hatchling ];
+
+  # Langchain always tracks the latest release of anthropic whether or not it's needed
+  pythonRelaxDeps = [ "anthropic" ];
+
+  dependencies = [
+    anthropic
+    langchain-core
+    pydantic
+  ];
+  pythonImportsCheck = [ "langchain_anthropic" ];
+
+  passthru = {
+    # python updater script sets the wrong tag
+    skipBulkUpdate = true;
+    updateScript = gitUpdater {
+      rev-prefix = "langchain-anthropic==";
+      ignoredVersions = "a|b|dev|rc";
+    };
+  };
+
+  meta = {
+    description = "Build LangChain applications with Anthropic";
+    homepage = "https://github.com/langchain-ai/langchain/tree/master/libs/partners/anthropic";
+    license = lib.licenses.mit;
+    maintainers = [
+      lib.maintainers.sarahec
+    ];
+  };
+})
