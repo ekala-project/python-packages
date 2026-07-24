a9c1c04c3cffc908eb19912accb3c6ec8f784520
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 12:19:54 -0700

    python3Packages.pycep-parser: init at 0.7.0

---
+++ python/pkgs/pycep-parser/default.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  assertpy,
+  buildPythonPackage,
+  fetchFromGitHub,
+  lark,
+  regex,
+  typing-extensions,
+  uv-build,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pycep-parser";
+  version = "0.7.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "gruebel";
+    repo = "pycep";
+    tag = finalAttrs.version;
+    hash = "sha256-pEFgpLfGcJhUWfs/nG1r7GfIS045cfNh7MVQokluXmM=";
+  };
+
+  build-system = [ uv-build ];
+
+  # We can't use pythonRelaxDeps to relax the build-system
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "uv_build~=0.9.0" "uv_build"
+  '';
+
+  pythonRelaxDeps = [ "regex" ];
+
+  dependencies = [
+    lark
+    regex
+    typing-extensions
+  ];
+  pythonImportsCheck = [ "pycep" ];
+
+  meta = {
+    description = "Python based Bicep parser";
+    homepage = "https://github.com/gruebel/pycep";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
