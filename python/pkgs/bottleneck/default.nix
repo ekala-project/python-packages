21827e5a7ff1c1673e93c85af6a5e94db8d88c43
Jonathan Ringer <jonringer117@gmail.com>
2026-07-24 13:48:54 -0700

    python3Packages.bottleneck: init at 1.6.0

---
+++ python/pkgs/bottleneck/default.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  versioneer,
+  numpy,
+  python,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "bottleneck";
+  version = "1.6.0";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-Ao1G7ksCWtmrTXmSQROBb4JfYrF7h8nh0NjOFEpKDjE=";
+  };
+
+  build-system = [
+    setuptools
+    versioneer
+  ];
+
+  dependencies = [ numpy ];
+  postCheck = "popd";
+
+  pythonImportsCheck = [ "bottleneck" ];
+
+  meta = {
+    description = "Fast NumPy array functions";
+    homepage = "https://github.com/pydata/bottleneck";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+})
