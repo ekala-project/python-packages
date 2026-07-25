7663c42822d6f1addaf881869cf5c7f8918c5152
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 09:50:24 -0700

    python3Packages.freezegun: init at 1.5.5

---
+++ python/pkgs/freezegun/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  python-dateutil,
+  pythonAtLeast,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "freezegun";
+  version = "1.5.5";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-rHdCpsxsJaLDXpKS39VUuJe1F9LewmiRoujevyBcuUo=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ python-dateutil ];
+  disabledTests = lib.optionals (pythonAtLeast "3.13") [
+    # https://github.com/spulec/freezegun/issues/547
+    "test_method_decorator_works_on_unittest_kwarg_frozen_time"
+    "test_method_decorator_works_on_unittest_kwarg_frozen_time_with_func"
+    "test_method_decorator_works_on_unittest_kwarg_hello"
+  ];
+
+  pythonImportsCheck = [ "freezegun" ];
+
+  meta = {
+    description = "Library that allows your Python tests to travel through time";
+    homepage = "https://github.com/spulec/freezegun";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
