47bd617ee4b1432b2d5ec5f72e50c3fd5b9bdca1
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 10:19:23 -0700

    python3Packages.importlib-metadata: init at 9.0.0

---
+++ python/pkgs/importlib-metadata/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  setuptools-scm,
+  toml,
+  zipp,
+}:
+
+buildPythonPackage rec {
+  pname = "importlib-metadata";
+  version = "9.0.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "importlib_metadata";
+    inherit version;
+    hash = "sha256-pPV6tZnmouMBbXWVz9cutGYaUQbnh6lbzJDHEFuDHvw=";
+  };
+
+  postPatch = ''
+    sed -i "/coherent.licensed/d" pyproject.toml
+  '';
+
+  build-system = [
+    setuptools # otherwise cross build fails
+    setuptools-scm
+  ];
+
+  dependencies = [
+    toml
+    zipp
+  ];
+
+  # Cyclic dependencies due to pyflakefs
+  doCheck = false;
+
+  pythonImportsCheck = [ "importlib_metadata" ];
+  meta = {
+    description = "Read metadata from Python packages";
+    homepage = "https://importlib-metadata.readthedocs.io/";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
