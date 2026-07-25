7f58eef7fc006d16b07dd238af5222c888c53c9b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 15:52:08 -0700

    python3Packages.nbclassic: init at 1.2.0

---
+++ python/pkgs/nbclassic/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  babel,
+  ipykernel,
+  ipython-genutils,
+  jupyter-packaging,
+  jupyter-server,
+  nest-asyncio,
+  notebook-shim,
+}:
+
+buildPythonPackage rec {
+  pname = "nbclassic";
+  version = "1.2.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-c27FBIOlRIWXHbITvpIH405R/BRMeDQ2JbaZF0I2RLo=";
+  };
+
+  build-system = [
+    babel
+    jupyter-packaging
+    jupyter-server
+  ];
+
+  dependencies = [
+    ipykernel
+    ipython-genutils
+    nest-asyncio
+    notebook-shim
+  ];
+  pythonImportsCheck = [ "nbclassic" ];
+  meta = {
+    description = "Jupyter lab environment notebook server extension";
+    homepage = "https://github.com/jupyter/nbclassic";
+    license = lib.licenses.bsd3;
+  };
+}
