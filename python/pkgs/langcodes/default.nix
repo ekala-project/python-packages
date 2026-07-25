844f4912b073a1e0816d03dc40c94a36716acd0f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 15:38:27 -0700

    python3Packages.langcodes: init at 3.5.1

---
+++ python/pkgs/langcodes/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  marisa-trie,
+  fetchPypi,
+  language-data,
+  setuptools,
+  setuptools-scm,
+}:
+
+buildPythonPackage rec {
+  pname = "langcodes";
+  version = "3.5.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-QL/zFeAbAdEcKuOSjdT1y9dN04+b2RLBK5o2BsFD9zE=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    language-data
+    marisa-trie
+    setuptools # pkg_resources import in language_data/util.py
+  ];
+
+  pythonImportsCheck = [ "langcodes" ];
+
+  meta = {
+    description = "Python toolkit for working with and comparing the standardized codes for languages";
+    homepage = "https://github.com/georgkrause/langcodes";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
