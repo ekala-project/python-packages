f3b5a288996d2ff1b64b1dd17b36b31479ff022b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 09:49:47 -0700

    python3Packages.argcomplete: init at 3.6.3

---
+++ python/pkgs/argcomplete/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  hatch-vcs,
+}:
+
+buildPythonPackage rec {
+  pname = "argcomplete";
+  version = "3.6.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "kislyuk";
+    repo = "argcomplete";
+    tag = "v${version}";
+    hash = "sha256-GK78gW54cFE0yXra56wG8LnBL9CLbf0TuIgxFwA9zZY=";
+  };
+
+  build-system = [
+    hatchling
+    hatch-vcs
+  ];
+
+  # Tries to build and install test packages which fails
+  doCheck = false;
+
+  pythonImportsCheck = [ "argcomplete" ];
+
+  meta = {
+    description = "Bash tab completion for argparse";
+    homepage = "https://kislyuk.github.io/argcomplete/";
+    downloadPage = "https://github.com/kislyuk/argcomplete";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
