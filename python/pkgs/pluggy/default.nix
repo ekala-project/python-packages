823560d200d7ffb95bc2b252b21d84cc9b7be8e4
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 14:03:20 -0700

    python3Packages.pluggy: init at 1.6.0

---
+++ python/pkgs/pluggy/default.nix
@@ -0,0 +1,31 @@
+{
+  buildPythonPackage,
+  lib,
+  fetchFromGitHub,
+  setuptools-scm,
+  callPackage,
+}:
+
+buildPythonPackage rec {
+  pname = "pluggy";
+  version = "1.6.0";
+
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pytest-dev";
+    repo = "pluggy";
+    tag = version;
+    hash = "sha256-pkQjPJuSASWmzwzp9H/UTJBQDr2r2RiofxpF135lAgc=";
+  };
+
+  build-system = [ setuptools-scm ];
+
+  # To prevent infinite recursion with pytest
+  meta = {
+    description = "Plugin and hook calling mechanisms for Python";
+    homepage = "https://github.com/pytest-dev/pluggy";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
