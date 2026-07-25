b6e1fb7cbe4746769a5b5d848ffdb2e21c0a82f7
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 16:45:50 -0700

    apache-orc: init at 2.3.0

---
+++ pkgs/apache-orc/default.nix
@@ -0,0 +1,87 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  fetchurl,
+  cmake,
+  gtest,
+  lz4,
+  protobuf,
+  snappy,
+  zlib,
+  zstd,
+}:
+
+let
+  orc-format =
+    let
+      version = "1.1.1";
+      name = "orc-format-${version}";
+      archiveName = "${name}.tar.gz";
+    in
+    fetchurl {
+      name = archiveName;
+      url = "https://www.apache.org/dyn/closer.lua/orc/${name}/${archiveName}?action=download";
+      hash = "sha256-WE3+KkIClGF4/Y/H0SOb54BbntRZarIELe5znniAmSs=";
+    };
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "apache-orc";
+  version = "2.3.0";
+
+  src = fetchFromGitHub {
+    owner = "apache";
+    repo = "orc";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-QQdRzwmUF1Qwxg53kJv1Q6yFuHqSrLYwUxKt+6wK9Hs=";
+  };
+
+  patches = [
+    ./cmake-link-abseil.patch
+    ./protobuf34-nodiscard.patch
+  ];
+
+  nativeBuildInputs = [
+    cmake
+    cmake.configurePhaseHook
+  ];
+
+  buildInputs = [
+    gtest
+    lz4
+    protobuf
+    snappy
+    zlib
+    zstd
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeFeature "CMAKE_BUILD_TYPE" "Release")
+    (lib.cmakeBool "BUILD_JAVA" false)
+    (lib.cmakeBool "STOP_BUILD_ON_WARNING" stdenv.hostPlatform.isLinux)
+    (lib.cmakeBool "INSTALL_VENDORED_LIBS" false)
+  ]
+  ++ lib.optional (stdenv.hostPlatform != stdenv.buildPlatform) [
+    (lib.cmakeFeature "HAS_PRE_1970_EXITCODE" "0")
+    (lib.cmakeFeature "HAS_POST_2038_EXITCODE" "0")
+    (lib.cmakeFeature "CMAKE_CXX_FLAGS" "-Wno-unused-parameter")
+  ];
+
+  env = {
+    GTEST_HOME = gtest.dev;
+    LZ4_HOME = lz4;
+    ORC_FORMAT_URL = orc-format;
+    PROTOBUF_HOME = protobuf;
+    SNAPPY_HOME = snappy.dev;
+    ZLIB_HOME = zlib.dev;
+    ZSTD_HOME = zstd.dev;
+  };
+
+  meta = {
+    description = "Smallest, fastest columnar storage for Hadoop workloads";
+    homepage = "https://github.com/apache/orc/";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
