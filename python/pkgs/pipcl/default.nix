3a383d68fb5fbea618b4bda034430e3b1f40fccf
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 15:37:08 -0700

    python3Packages.pipcl: init at 11

---
+++ python/pkgs/pipcl/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pipcl";
+  version = "11";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ArtifexSoftware";
+    repo = "pipcl";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Gefz+BUWojruztnb1zEJWqBNU3HzdPjl/pw9lce+R7Y=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  pythonImportsCheck = [
+    "pipcl"
+  ];
+
+  meta = {
+    description = "Python packaging operations for use by setup.py";
+    homepage = "https://github.com/ArtifexSoftware/pipcl";
+    license = lib.licenses.agpl3Only;
+    maintainers = [ ];
+  };
+})
