29d997831d3a0c8517261cddce9b9af8d96b1649
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 15:00:06 -0700

    nkeys: init at 0.4.16

---
+++ pkgs/nkeys/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "nkeys";
+  version = "0.4.16";
+
+  src = fetchFromGitHub {
+    owner = "nats-io";
+    repo = "nkeys";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-GR1OvyA8bjZRTPTn12izxQXjACIaXFbGsSquHucusRY=";
+  };
+
+  vendorHash = "sha256-WszCsYEK0xOuSNI3UxJJLWKus8viVREaNj4xVQY6eBM=";
+
+  meta = {
+    description = "Public-key signature system for NATS";
+    homepage = "https://github.com/nats-io/nkeys";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "nk";
+  };
+})
