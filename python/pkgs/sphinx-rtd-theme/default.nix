973725519a2ad336ee2e4b052edbc6a8369028e0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 09:52:40 -0700

    python3Packages.sphinx-rtd-theme: init at 3.1.0

---
+++ python/pkgs/sphinx-rtd-theme/default.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  docutils,
+  sphinx,
+  sphinxcontrib-jquery,
+}:
+
+buildPythonPackage rec {
+  pname = "sphinx-rtd-theme";
+  version = "3.1.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "sphinx_rtd_theme";
+    inherit version;
+    hash = "sha256-tEJ28sJ26Qkjmk9slVqmZ6qv63hZeSOxxgurx223jkw=";
+  };
+
+  build-system = [ setuptools ];
+
+  preBuild = ''
+    # Don't use npm to fetch assets. Assets are included in sdist.
+    export CI=1
+  '';
+
+  dependencies = [
+    docutils
+    sphinx
+    sphinxcontrib-jquery
+  ];
+
+  pythonRelaxDeps = [
+    "docutils"
+    "sphinxcontrib-jquery"
+    # https://github.com/readthedocs/sphinx_rtd_theme/pull/1666
+    "sphinx"
+  ];
+  pythonImportsCheck = [ "sphinx_rtd_theme" ];
+
+  meta = {
+    description = "Sphinx theme for readthedocs.org";
+    homepage = "https://github.com/readthedocs/sphinx_rtd_theme";
+    license = lib.licenses.mit;
+  };
+}
