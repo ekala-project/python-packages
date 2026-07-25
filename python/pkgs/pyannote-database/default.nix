395e158c60fda30d7d0688853021b0f73b7207a2
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 10:28:14 -0700

    python3Packages.pyannote-database: init at 6.1.1

---
+++ python/pkgs/pyannote-database/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  hatch-vcs,
+  hatchling,
+
+  # dependencies
+  pandas,
+  pyannote-core,
+  pyyaml,
+  typer,
+}:
+
+buildPythonPackage rec {
+  pname = "pyannote-database";
+  version = "6.1.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pyannote";
+    repo = "pyannote-database";
+    tag = version;
+    hash = "sha256-72H1tVLtDSYHAgjHWmI7pPQhKGchHz5VP0eRU6NRj2g=";
+  };
+
+  build-system = [
+    hatch-vcs
+    hatchling
+  ];
+
+  dependencies = [
+    pandas
+    pyannote-core
+    pyyaml
+    # Imported in pyannote/database/cli.py
+    typer
+  ];
+
+  pythonImportsCheck = [ "pyannote.database" ];
+  meta = {
+    description = "Reproducible experimental protocols for multimedia (audio, video, text) database";
+    homepage = "https://github.com/pyannote/pyannote-database";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "pyannote-database";
+  };
+}
