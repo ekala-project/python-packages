9720831954c9931de4b94753cabeee717063ccf1
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 09:48:25 -0700

    python3Packages.sortedcontainers: init at 2.4.0

---
+++ python/pkgs/sortedcontainers/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+}:
+
+let
+  sortedcontainers = buildPythonPackage rec {
+    pname = "sortedcontainers";
+    version = "2.4.0";
+    format = "setuptools";
+
+    src = fetchFromGitHub {
+      owner = "grantjenks";
+      repo = "python-sortedcontainers";
+      rev = "v${version}";
+      hash = "sha256-YRbSM2isWi7AzfquFvuZBlpEMNUnBJTBLBn0/XYVHKQ=";
+    };
+
+    doCheck = false;
+    pythonImportsCheck = [ "sortedcontainers" ];
+    meta = {
+      description = "Python Sorted Container Types: SortedList, SortedDict, and SortedSet";
+      homepage = "https://grantjenks.com/docs/sortedcontainers/";
+      license = lib.licenses.asl20;
+      maintainers = [ ];
+    };
+  };
+in
+sortedcontainers
