161041753063fdeabfcf1c012a56070fb52cc252
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 09:50:01 -0700

    python3Packages.pyflakes: init at 3.4.0

---
+++ python/pkgs/pyflakes/default.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  buildPythonPackage,
+  isPyPy,
+  fetchFromGitHub,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "pyflakes";
+  version = "3.4.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "PyCQA";
+    repo = "pyflakes";
+    tag = version;
+    hash = "sha256-4UEJjn9Eey1vHeaG468x/nMlbfGu3ohZX1R7RR2R5ik=";
+  };
+
+  build-system = [ setuptools ];
+  disabledTests = lib.optionals isPyPy [
+    # https://github.com/PyCQA/pyflakes/issues/779
+    "test_eofSyntaxError"
+    "test_misencodedFileUTF16"
+    "test_misencodedFileUTF8"
+    "test_multilineSyntaxError"
+  ];
+
+  pythonImportsCheck = [ "pyflakes" ];
+
+  meta = {
+    homepage = "https://github.com/PyCQA/pyflakes";
+    description = "Simple program which checks Python source files for errors";
+    mainProgram = "pyflakes";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
