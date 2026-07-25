ae5457b9feb9ae57dc66da6390d587e69ce3d139
Jonathan Ringer <jonringer117@gmail.com>
2026-07-25 09:48:36 -0700

    python3Packages.jaraco-functools: init at 4.4.0

---
+++ python/pkgs/jaraco-functools/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  stdenv,
+  buildPythonPackage,
+  fetchPypi,
+  more-itertools,
+  setuptools-scm,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "jaraco-functools";
+  version = "4.4.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "jaraco_functools";
+    inherit version;
+    hash = "sha256-2iGTOwQXuJUVViZWVHp3tJMfmBdusXNkTA01Ayoz1rs=";
+  };
+
+  postPatch = ''
+    sed -i "/coherent\.licensed/d" pyproject.toml
+  '';
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [ more-itertools ];
+  # test is flaky on darwin
+  disabledTests = if stdenv.hostPlatform.isDarwin then [ "test_function_throttled" ] else null;
+
+  pythonNamespaces = [ "jaraco" ];
+
+  pythonImportsCheck = [ "jaraco.functools" ];
+
+  meta = {
+    description = "Additional functools in the spirit of stdlib's functools";
+    homepage = "https://github.com/jaraco/jaraco.functools";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
