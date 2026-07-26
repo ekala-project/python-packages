99f887f42f9470065866a04dc3dc97a4e4c97c3c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-26 11:44:07 -0700

    python3Packages.etils: add jax and full optional-dependencies

--- python/pkgs/etils/default.nix
+++ python/pkgs/etils/default.nix
@@ -1,20 +1,35 @@
-{ lib
-, buildPythonPackage
-, fetchFromGitHub
-, # build-system
-  flit-core
-, # optional-dependencies
-  numpy
-, packaging
-, typing-extensions
-, zipp
-,
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  flit-core,
+
+  # optional-dependencies
+  jupyter,
+  mediapy,
+  numpy,
+  packaging,
+  protobuf,
+  fsspec,
+  typing-extensions,
+  zipp,
+  absl-py,
+  simple-parsing,
+  einops,
+  gcsfs,
+  s3fs,
+  tqdm,
+  dm-tree,
+  jax,
 }:
 
 buildPythonPackage (finalAttrs: {
   pname = "etils";
   version = "1.14.0";
   pyproject = true;
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "google";
@@ -25,16 +40,47 @@ buildPythonPackage (finalAttrs: {
 
   build-system = [ flit-core ];
 
-  optional-dependencies = {
+  optional-dependencies = lib.fix (self: {
+    array-types = self.enp;
+    eapp = [
+      absl-py
+      simple-parsing
+    ]
+    ++ self.epy;
+    ecolab = [
+      jupyter
+      numpy
+      mediapy
+      packaging
+      protobuf
+    ]
+    ++ self.enp
+    ++ self.epy
+    ++ self.etree;
+    edc = self.epy;
     enp = [
       numpy
-    ];
+      einops
+    ]
+    ++ self.epy;
     epath = [
+      fsspec
       typing-extensions
       zipp
-    ];
+    ]
+    ++ self.epy;
+    epath-gcs = [ gcsfs ] ++ self.epath;
+    epath-s3 = [ s3fs ] ++ self.epath;
     epy = [ typing-extensions ];
-  };
+    etqdm = [
+      absl-py
+      tqdm
+    ]
+    ++ self.epy;
+    etree = self.array-types ++ self.epy ++ self.enp ++ self.etqdm;
+    etree-dm = [ dm-tree ] ++ self.etree;
+    etree-jax = [ jax ] ++ self.etree;
+  });
 
   pythonImportsCheck = [ "etils" ];
 
