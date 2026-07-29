c18da02604ba6a0a136ca27d30b18123b97aaf65
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:22 -0700

    python3Packages.faadelays: init at 2023.9.1

---
+++ python/pkgs/faadelays/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  aiohttp,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "faadelays";
+  version = "2023.9.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-ngMFd+BE3hKeaeGEX4xHpzDIrtGFDsSwxBbrc4ZMFas=";
+  };
+
+  nativeBuildInputs = [ setuptools ];
+
+  propagatedBuildInputs = [ aiohttp ];
+
+  # Project has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "faadelays" ];
+
+  meta = {
+    description = "Python package to retrieve FAA airport status";
+    homepage = "https://github.com/ntilley905/faadelays";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
