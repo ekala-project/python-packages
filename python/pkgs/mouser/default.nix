87561a50e2412a6ad96605eb88e1efee8363b2d3
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:14 -0700

    python3Packages.mouser: init at 0.1.6

---
+++ python/pkgs/mouser/default.nix
@@ -0,0 +1,52 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  poetry-core,
+
+  # dependencies
+  click,
+  requests,
+
+  # tests
+  pytestCheckHook,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "mouser";
+  version = "0.1.6";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "sparkmicro";
+    repo = "mouser-api";
+    tag = finalAttrs.version;
+    hash = "sha256-E8RYtuY4OONl9fI25I2utk3JfElVJHlpfCuOPvHo5Dg=";
+  };
+
+  build-system = [
+    poetry-core
+  ];
+
+  dependencies = [
+    click
+    requests
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+  pythonImportsCheck = [ "mouser" ];
+  disabledTests = [
+    # Search tests require an API key and network access
+    "search"
+  ];
+
+  meta = {
+    description = "Mouser Python API";
+    homepage = "https://github.com/sparkmicro/mouser-api";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
