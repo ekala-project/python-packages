af0cb7c265c517882e5e3b76025a4275e4a16c7c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:26 -0700

    python3Packages.ascii-magic: init at 2.7.4

---
+++ python/pkgs/ascii-magic/default.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  buildPythonPackage,
+  colorama,
+  fetchFromGitHub,
+  pillow,
+  pytestCheckHook,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "ascii-magic";
+  version = "2.7.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "LeandroBarone";
+    repo = "python-ascii_magic";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-werCg7LW7MKMoYp/QxZU74MSc6WmscwWfvGRG4Dn60c=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    colorama
+    pillow
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "ascii_magic" ];
+
+  preCheck = ''
+    ln -s ascii_magic/tests/*.{jpg,png} ./
+  '';
+
+  disabledTests = [
+    # Test requires network access
+    "test_from_url"
+    "test_quick_test"
+    "test_wrong_url"
+    # No clipboard in the sandbox
+    "test_from_clipboard"
+  ];
+
+  meta = {
+    description = "Python module to converts pictures into ASCII art";
+    homepage = "https://github.com/LeandroBarone/python-ascii_magic";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
