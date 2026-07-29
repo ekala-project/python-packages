ab68d0775c3b2ecab5e4bce3ad800fe290d8ae79
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:54 -0700

    python3Packages.bond-async: init at 0.2.1

---
+++ python/pkgs/bond-async/default.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  aiohttp,
+  aioresponses,
+  orjson,
+  pytest-asyncio,
+  pytestCheckHook,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "bond-async";
+  version = "0.2.1";
+
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "bondhome";
+    repo = "bond-async";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-YRJHUOYFLf4dtQGIFKHLdUQxWTnZzG1MPirMsGvDor8=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    aiohttp
+    orjson
+  ];
+
+  nativeCheckInputs = [
+    aioresponses
+    pytest-asyncio
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "bond_async" ];
+
+  meta = {
+    description = "Asynchronous Python wrapper library over Bond Local API";
+    homepage = "https://github.com/bondhome/bond-async";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
