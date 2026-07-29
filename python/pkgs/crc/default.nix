8c8e260814651876916045d3274b3f5f2118c900
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:23 -0700

    python3Packages.crc: init at 7.1.0

---
+++ python/pkgs/crc/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  poetry-core,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "crc";
+  version = "7.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Nicoretti";
+    repo = "crc";
+    tag = version;
+    hash = "sha256-Oa2VSzNT+8O/rWZurIr7RnP8m3xAEVOQLs+ObT4xIa0=";
+  };
+
+  build-system = [ poetry-core ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "crc" ];
+
+  disabledTestPaths = [ "test/bench" ];
+
+  meta = {
+    description = "Python module for calculating and verifying predefined & custom CRC's";
+    homepage = "https://nicoretti.github.io/crc/";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+    mainProgram = "crc";
+  };
+}
