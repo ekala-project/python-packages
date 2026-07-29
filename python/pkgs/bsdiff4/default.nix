6b34899f0b48e9de4390cba646c409e0deef2bd3
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:57 -0700

    python3Packages.bsdiff4: init at 1.2.6

---
+++ python/pkgs/bsdiff4/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "bsdiff4";
+  version = "1.2.6";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-KrV9AaeLOeKeWszJz+rUEwmC3tncy8QmG9DpxR1rdR0=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "bsdiff4" ];
+
+  checkPhase = ''
+    mv bsdiff4 _bsdiff4
+    python -c 'import bsdiff4; bsdiff4.test()'
+  '';
+
+  meta = {
+    description = "Binary diff and patch using the BSDIFF4-format";
+    homepage = "https://github.com/ilanschnell/bsdiff4";
+    license = lib.licenses.bsdProtection;
+    maintainers = [ ];
+  };
+})
