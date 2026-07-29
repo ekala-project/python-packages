135df277d9a9bc4741d7352a64dbd6fc53d9f311
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:09 -0700

    python3Packages.aioslimproto: init at 3.1.9

---
+++ python/pkgs/aioslimproto/default.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  aiohttp,
+  async-timeout,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pillow,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "aioslimproto";
+  version = "3.1.9";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "home-assistant-libs";
+    repo = "aioslimproto";
+    tag = finalAttrs.version;
+    hash = "sha256-IyDgnBQC8EbDzwRFWgmVOf4lVNsmdiTaMfpWXHUa4oM=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail 'version = "0.0.0"' 'version = "${finalAttrs.version}"'
+  '';
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    aiohttp
+    async-timeout
+    pillow
+  ];
+
+  # Module has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "aioslimproto" ];
+
+  meta = {
+    description = "Module to control Squeezebox players";
+    homepage = "https://github.com/home-assistant-libs/aioslimproto";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
