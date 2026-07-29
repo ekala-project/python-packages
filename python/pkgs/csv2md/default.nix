f21a9ae1903093bf62e8c40c44f3908dac887424
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:27 -0700

    python3Packages.csv2md: init at 1.7.1

---
+++ python/pkgs/csv2md/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildPythonPackage,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "csv2md";
+  version = "1.7.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "lzakharov";
+    repo = "csv2md";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-UgX05ObIG3+Cucb0lC/+5w1WRB8eZbOeBpJoYHTotiw=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "csv2md" ];
+
+  meta = {
+    description = "Command line tool for converting CSV files into Markdown tables";
+    homepage = "https://github.com/lzakharov/csv2md";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "csv2md";
+  };
+})
