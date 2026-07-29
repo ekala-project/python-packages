99eca76a63a1633ca1eb4bdf61cd126bb6420046
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:30 -0700

    python3Packages.asyncsleepiq: init at 1.7.1

---
+++ python/pkgs/asyncsleepiq/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  aiohttp,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "asyncsleepiq";
+  version = "1.7.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-gOg1cxd2OsDRg5jtc6MfEMsK9T0Croo8K1jzsvbAbdY=";
+  };
+
+  nativeBuildInputs = [ setuptools ];
+
+  propagatedBuildInputs = [ aiohttp ];
+
+  # upstream has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "asyncsleepiq" ];
+
+  meta = {
+    description = "Async interface to SleepIQ API";
+    homepage = "https://github.com/kbickar/asyncsleepiq";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
