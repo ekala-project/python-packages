9e2999b0bb51ae10d6e0f5ea4939a3243091d1ce
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:52 -0700

    python3Packages.juliandate: init at 1.0.5

---
+++ python/pkgs/juliandate/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "juliandate";
+  version = "1.0.5";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "seanredmond";
+    repo = "juliandate";
+    tag = "v${version}";
+    hash = "sha256-pOWyrPBFqKmG9oKbXY/L14LblIcc8KfZSqZAEQP29V8=";
+  };
+
+  build-system = [ setuptools ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "juliandate" ];
+
+  meta = {
+    description = "Conversions between Julian Dates and Julian/Gregorian calendar dates";
+    homepage = "https://github.com/seanredmond/juliandate";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+  };
+}
