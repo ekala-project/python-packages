a4b299abf1460325ceda971f8d11851451e7c84b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:36 -0700

    python3Packages.flask-sock: init at 0.7.0

---
+++ python/pkgs/flask-sock/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  pytestCheckHook,
+  fetchFromGitHub,
+  flask,
+  setuptools,
+  simple-websocket,
+}:
+
+buildPythonPackage rec {
+  pname = "flask-sock";
+  version = "0.7.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "miguelgrinberg";
+    repo = "flask-sock";
+    rev = "v${version}";
+    hash = "sha256-GKfOVdeLPag2IKGCWrMjQp4NTL1/9GiyLhXhf9jQKhQ=";
+  };
+
+  nativeBuildInputs = [ setuptools ];
+
+  propagatedBuildInputs = [
+    flask
+    simple-websocket
+  ];
+
+  enabledTestPaths = [ "tests/test_flask_sock.py" ];
+
+  pythonImportsCheck = [ "flask_sock" ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  meta = {
+    description = "WebSocket support for Flask";
+    homepage = "https://github.com/miguelgrinberg/flask-sock/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
