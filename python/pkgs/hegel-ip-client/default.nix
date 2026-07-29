5cb88813c1b77398a0ceef6c2824e7b1cb638707
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:15 -0700

    python3Packages.hegel-ip-client: init at 0.1.4

---
+++ python/pkgs/hegel-ip-client/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "hegel-ip-client";
+  version = "0.1.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "boazca";
+    repo = "hegel-ip-client";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-IKOtZ6mxbFvH1cLT45j/OD3OzAwy6vmtdF584LS5M7A=";
+  };
+
+  build-system = [ setuptools ];
+
+  # no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "hegel_ip_client" ];
+
+  meta = {
+    description = "Python client library for Hegel amplifiers";
+    homepage = "https://github.com/boazca/hegel-ip-client";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
