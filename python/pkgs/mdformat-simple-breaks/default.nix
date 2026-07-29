64f7bc562d29bcd866e78ca6245363c442738f6c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:42:52 -0700

    python3Packages.mdformat-simple-breaks: init at 0.1.0

---
+++ python/pkgs/mdformat-simple-breaks/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  flit-core,
+  mdformat,
+  pytestCheckHook,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "mdformat-simple-breaks";
+  version = "0.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "csala";
+    repo = "mdformat-simple-breaks";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-w0qPxIlCFMvs7p2Lya/ATkQN9wVt8ipsePZgonN/qpc=";
+  };
+
+  build-system = [
+    flit-core
+  ];
+
+  dependencies = [
+    mdformat
+  ];
+
+  pythonImportsCheck = [ "mdformat_simple_breaks" ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  meta = {
+    description = "Mdformat plugin to render thematic breaks using three dashes";
+    homepage = "https://github.com/csala/mdformat-simple-breaks";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
