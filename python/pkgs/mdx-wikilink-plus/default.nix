26609dfaf3a219fb2b1cf161e3bb734daa910209
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:42:53 -0700

    python3Packages.mdx-wikilink-plus: init at 1.4.1

---
+++ python/pkgs/mdx-wikilink-plus/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  markdown,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "mdx-wikilink-plus";
+  version = "1.4.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "neurobin";
+    repo = "mdx_wikilink_plus";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-d0F0blSYYRNbkwnbL9kzkNbfNVG2NZV74WapP5ubkoo=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
+    markdown
+  ];
+
+  pythonImportsCheck = [
+    "mdx_wikilink_plus"
+  ];
+
+  # No tests available.
+  doCheck = false;
+
+  meta = {
+    description = "A wikilink extension for Python Markdown";
+    homepage = "https://github.com/neurobin/mdx_wikilink_plus";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+    mainProgram = "mdx-wikilink-plus";
+  };
+})
