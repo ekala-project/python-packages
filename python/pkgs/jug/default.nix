c4e049f188aa705fcae298ace1a86fa975d7def6
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:52 -0700

    python3Packages.jug: init at 2.5.0

---
+++ python/pkgs/jug/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  bottle,
+  buildPythonPackage,
+  fetchFromGitHub,
+  numpy,
+  pytestCheckHook,
+  pyyaml,
+  redis,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "jug";
+  version = "2.5.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "luispedro";
+    repo = "jug";
+    tag = "v${version}";
+    hash = "sha256-YjBhA+yEdMQ/4yYf25kkXwbvw+ta9Nb4CX8Rnr0du6k=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependenciesk = [ bottle ];
+
+  nativeCheckInputs = [
+    numpy
+    pytestCheckHook
+    pyyaml
+    redis
+  ];
+
+  pythonImportsCheck = [ "jug" ];
+
+  meta = {
+    description = "Task-Based Parallelization Framework";
+    homepage = "https://jug.readthedocs.io/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
