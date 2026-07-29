ef62c14c359bfe71ff25f0ec0551a6ba78103222
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:37:58 -0700

    python3Packages.aioeagle: init at 1.1.0

---
+++ python/pkgs/aioeagle/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  aiohttp,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  xmltodict,
+}:
+
+buildPythonPackage rec {
+  pname = "aioeagle";
+  version = "1.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "home-assistant-libs";
+    repo = "aioeagle";
+    tag = version;
+    hash = "sha256-ZO5uODCGebggItsEVKtis0uwU67dmSxc7DHzzkBZ9oQ=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    aiohttp
+    xmltodict
+  ];
+
+  # Project has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "aioeagle" ];
+
+  meta = {
+    description = "Python library to control EAGLE-200";
+    homepage = "https://github.com/home-assistant-libs/aioeagle";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
