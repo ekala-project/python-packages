dcd4c2a4a3cabffb5b06747587ad33f9dfd93161
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:52 -0700

    python3Packages.blurhash: init at 1.1.5

---
+++ python/pkgs/blurhash/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytest-cov-stub,
+  pytestCheckHook,
+  pillow,
+  numpy,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "blurhash";
+  version = "1.1.5";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "halcy";
+    repo = "blurhash-python";
+    tag = "v${version}";
+    hash = "sha256-lTPn2GTD7eQ9XkZyuttFqEvNgzcx6b7OdeMc5WOXrJs=";
+  };
+
+  build-system = [ setuptools ];
+
+  nativeCheckInputs = [
+    pytest-cov-stub
+    pytestCheckHook
+    pillow
+    numpy
+  ];
+
+  pythonImportsCheck = [ "blurhash" ];
+
+  meta = {
+    description = "Pure-Python implementation of the blurhash algorithm";
+    homepage = "https://github.com/halcy/blurhash-python";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
