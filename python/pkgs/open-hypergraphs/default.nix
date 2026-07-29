d572f6ca2dde34b1deb714660f6f7fa549651fad
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:51 -0700

    python3Packages.open-hypergraphs: init at 0.1.3

---
+++ python/pkgs/open-hypergraphs/default.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  numpy,
+  scipy,
+  hypothesis,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "open-hypergraphs";
+  version = "0.1.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "statusfailed";
+    repo = "open-hypergraphs";
+    tag = "pypi-${version}";
+    hash = "sha256-sBF/+VENDajLN72UJ6iHekmk11pOqfxeKs8Kqszy6mQ=";
+  };
+
+  build-system = [
+    hatchling
+  ];
+
+  dependencies = [
+    numpy
+    scipy
+  ];
+
+  pythonRelaxDeps = [
+    "numpy"
+    "scipy"
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    hypothesis
+  ];
+
+  pythonImportsCheck = [
+    "open_hypergraphs"
+  ];
+
+  meta = {
+    description = "Implementation of open hypergraphs for string diagrams";
+    homepage = "https://github.com/statusfailed/open-hypergraphs";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
