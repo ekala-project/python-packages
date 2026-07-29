5a4421ed87c2c863fe6df0bca92e6aad1b4be3c9
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:42:51 -0700

    python3Packages.mdformat-frontmatter: init at 2.0.10

---
+++ python/pkgs/mdformat-frontmatter/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  flit-core,
+
+  # dependencies
+  mdformat,
+  mdit-py-plugins,
+  ruamel-yaml,
+
+  # tests
+  pytestCheckHook,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "mdformat-frontmatter";
+  version = "2.0.10";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "butler54";
+    repo = "mdformat-frontmatter";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-snW9L9vnRHjNchhWZ5sIrn1r4piEYJeKQwib/4rarOo=";
+  };
+
+  build-system = [ flit-core ];
+
+  dependencies = [
+    mdformat
+    mdit-py-plugins
+    ruamel-yaml
+  ];
+
+  pythonImportsCheck = [ "mdformat_frontmatter" ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  meta = {
+    description = "Mdformat plugin to ensure frontmatter is respected";
+    homepage = "https://github.com/butler54/mdformat-frontmatter";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
