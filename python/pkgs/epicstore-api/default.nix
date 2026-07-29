eadabb0193d5a8be41ed23eb4ef4828ac55a252e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:17 -0700

    python3Packages.epicstore-api: init at 0.2.0

---
+++ python/pkgs/epicstore-api/default.nix
@@ -0,0 +1,39 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  lib,
+  pytestCheckHook,
+  cloudscraper,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "epicstore-api";
+  version = "0.2.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "SD4RK";
+    repo = "epicstore_api";
+    tag = "v_${version}";
+    hash = "sha256-XSynUz8rAl/+jcPMCZoVKlGZLVcTCAr36VEWVhAydoM=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ cloudscraper ];
+
+  pythonImportsCheck = [ "epicstore_api" ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  # tests directory exists but contains no test cases
+  doCheck = false;
+
+  meta = {
+    description = "Epic Games Store Web API Wrapper written in Python";
+    homepage = "https://github.com/SD4RK/epicstore_api";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
