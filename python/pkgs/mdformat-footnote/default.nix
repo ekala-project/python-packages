fac4c5f27783793603bd4cb92abd6ac47ac42348
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:42:51 -0700

    python3Packages.mdformat-footnote: init at 0.1.3

---
+++ python/pkgs/mdformat-footnote/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  flit-core,
+  mdformat,
+  mdit-py-plugins,
+}:
+
+buildPythonPackage rec {
+  pname = "mdformat-footnote";
+  version = "0.1.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "executablebooks";
+    repo = "mdformat-footnote";
+    tag = "v${version}";
+    hash = "sha256-KPxPDKG3aC5zLdrJRL3kzFMpLJ0Yk5UZXW4CamC1/NQ=";
+  };
+
+  build-system = [ flit-core ];
+
+  dependencies = [
+    mdformat
+    mdit-py-plugins
+  ];
+
+  pythonImportsCheck = [ "mdformat_footnote" ];
+
+  meta = {
+    description = "Footnote format addition for mdformat";
+    homepage = "https://github.com/executablebooks/mdformat-footnote";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
