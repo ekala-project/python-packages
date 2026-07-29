3b3c9ef39574810db54ce8ce263f85577dad3d52
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:42:51 -0700

    python3Packages.mdformat-gfm: init at 1.0.0

---
+++ python/pkgs/mdformat-gfm/default.nix
@@ -0,0 +1,52 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  flit-core,
+
+  # dependencies
+  markdown-it-py,
+  mdformat,
+  mdit-py-plugins,
+  wcwidth,
+
+  # tests
+  pytestCheckHook,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "mdformat-gfm";
+  version = "1.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "hukkin";
+    repo = "mdformat-gfm";
+    tag = finalAttrs.version;
+    hash = "sha256-Vijt5P3KRL4jkU8AI2lAsJkvFne/l3utUkjHUs8PQHI=";
+  };
+
+  build-system = [
+    flit-core
+  ];
+
+  dependencies = [
+    markdown-it-py
+    mdformat
+    mdit-py-plugins
+    wcwidth
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "mdformat_gfm" ];
+
+  meta = {
+    description = "Mdformat plugin for GitHub Flavored Markdown compatibility";
+    homepage = "https://github.com/hukkin/mdformat-gfm";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
