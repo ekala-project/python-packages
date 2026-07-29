62b9563fb7b869b46a2d0bc99fc7c319710d915e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:14 -0700

    python3Packages.motionblinds: init at 0.6.30

---
+++ python/pkgs/motionblinds/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pycryptodomex,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "motionblinds";
+  version = "0.6.30";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "starkillerOG";
+    repo = "motion-blinds";
+    tag = version;
+    hash = "sha256-xV9od7xTKBBE4f4Mqg57Mp0MXO8/lG+bBKzG+jv6gf4=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ pycryptodomex ];
+
+  # Module has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "motionblinds" ];
+
+  meta = {
+    description = "Python library for interfacing with Motion Blinds";
+    homepage = "https://github.com/starkillerOG/motion-blinds";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
