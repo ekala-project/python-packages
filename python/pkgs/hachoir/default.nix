1e2623d777d20007799bd3d2774511cdfe1b1118
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:09 -0700

    python3Packages.hachoir: init at 3.3.0

---
+++ python/pkgs/hachoir/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytestCheckHook,
+  urwid,
+}:
+
+buildPythonPackage rec {
+  pname = "hachoir";
+  version = "3.3.0";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "vstinner";
+    repo = "hachoir";
+    tag = version;
+    hash = "sha256-sTUJx8Xyhw4Z6juRtREw/okuVjSTSVWpSLKeZ7T8IR8=";
+  };
+
+  propagatedBuildInputs = [ urwid ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "hachoir" ];
+
+  meta = {
+    description = "Python library to view and edit a binary stream";
+    homepage = "https://hachoir.readthedocs.io/";
+    license = lib.licenses.gpl2Only;
+    maintainers = [ ];
+  };
+}
