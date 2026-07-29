8aad24e359a20a00086ab0912b8e81bfe9701390
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:21 -0700

    python3Packages.mwparserfromhell: init at 0.7.2

---
+++ python/pkgs/mwparserfromhell/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytestCheckHook,
+  setuptools,
+  setuptools-scm,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "mwparserfromhell";
+  version = "0.7.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "earwig";
+    repo = "mwparserfromhell";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-yPj272bMh/pLapc7lDgP4+AnDBpE2FrDICRUxizIcSA=";
+  };
+
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace '"pytest-runner"' ""
+  '';
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "mwparserfromhell" ];
+
+  meta = {
+    description = "Parser for MediaWiki wikicode";
+    homepage = "https://mwparserfromhell.readthedocs.io/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
