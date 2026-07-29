a5e26bb86a53b5b0cde1d33318dcf0b5ad74e7f3
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:37:53 -0700

    python3Packages.aio-geojson-geonetnz-quakes: init at 2026.6.0

---
+++ python/pkgs/aio-geojson-geonetnz-quakes/default.nix
@@ -0,0 +1,52 @@
+{
+  lib,
+  aio-geojson-client,
+  aiohttp,
+  aiointercept,
+  aioresponses,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytest-asyncio,
+  pytestCheckHook,
+  pytz,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "aio-geojson-geonetnz-quakes";
+  version = "2026.6.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "exxamalte";
+    repo = "python-aio-geojson-geonetnz-quakes";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-JzhKlQR7tNRCtiEVSQGWFIDxUl6o+jhR3kKYjRCMTAU=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    aio-geojson-client
+    aiohttp
+    pytz
+  ];
+
+  nativeCheckInputs = [
+    aiointercept
+    aioresponses
+    pytest-asyncio
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "aio_geojson_geonetnz_quakes" ];
+
+  __darwinAllowLocalNetworking = true;
+
+  meta = {
+    description = "Python module for accessing the GeoNet NZ Quakes GeoJSON feeds";
+    homepage = "https://github.com/exxamalte/python-aio-geojson-geonetnz-quakes";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
