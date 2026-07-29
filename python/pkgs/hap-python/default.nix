510021437ba1a4d2b4dd41e0432fe8d989dec921
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:10 -0700

    python3Packages.hap-python: init at 5.0.0

---
+++ python/pkgs/hap-python/default.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  base36,
+  buildPythonPackage,
+  chacha20poly1305-reuseable,
+  cryptography,
+  fetchFromGitHub,
+  h11,
+  orjson,
+  pyqrcode,
+  pytest-asyncio,
+  pytest-timeout,
+  pytestCheckHook,
+  pythonAtLeast,
+  setuptools,
+  zeroconf,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "hap-python";
+  version = "5.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ikalchev";
+    repo = "HAP-python";
+    tag = finalAttrs.version;
+    hash = "sha256-+EhxoO5X/ANGh008WE0sJeBsu8SRnuds3hXGxNWpKnk=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    chacha20poly1305-reuseable
+    cryptography
+    h11
+    orjson
+    zeroconf
+  ];
+
+  optional-dependencies.QRCode = [
+    base36
+    pyqrcode
+  ];
+
+  nativeCheckInputs = [
+    pytest-asyncio
+    pytest-timeout
+    pytestCheckHook
+  ]
+  ++ lib.flatten (builtins.attrValues finalAttrs.passthru.optional-dependencies);
+
+  pythonImportsCheck = [ "pyhap" ];
+
+  disabledTests = lib.optionals (pythonAtLeast "3.14") [
+    # https://github.com/ikalchev/HAP-python/issues/490
+    "test_start_from_sync"
+  ];
+
+  meta = {
+    description = "HomeKit Accessory Protocol implementation";
+    homepage = "https://github.com/ikalchev/HAP-python";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
