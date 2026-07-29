108f55ab07940bbd9ffba83a54a0dce6d79c7ab0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:39 -0700

    python3Packages.denonavr: init at 1.3.3

---
+++ python/pkgs/denonavr/default.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  asyncstdlib,
+  attrs,
+  buildPythonPackage,
+  defusedxml,
+  fetchFromGitHub,
+  ftfy,
+  httpx,
+  netifaces,
+  pytest-asyncio,
+  pytest-httpx,
+  pytest-timeout,
+  pytestCheckHook,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "denonavr";
+  version = "1.3.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ol-iver";
+    repo = "denonavr";
+    tag = finalAttrs.version;
+    hash = "sha256-5yjrBG7Vufi+O4mHV1yGvunUJY38eWc5ZIoeoZyG5ak=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    asyncstdlib
+    attrs
+    defusedxml
+    ftfy
+    httpx
+    netifaces
+  ];
+
+  nativeCheckInputs = [
+    pytest-asyncio
+    pytestCheckHook
+    pytest-httpx
+    pytest-timeout
+  ];
+
+  pythonImportsCheck = [ "denonavr" ];
+
+  meta = {
+    description = "Automation Library for Denon AVR receivers";
+    homepage = "https://github.com/ol-iver/denonavr";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
