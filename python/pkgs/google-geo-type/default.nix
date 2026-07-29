06b8a4d0888fecb118894831af0e8d319578b7aa
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:59 -0700

    python3Packages.google-geo-type: init at 0.7.0

---
+++ python/pkgs/google-geo-type/default.nix
@@ -0,0 +1,54 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  gitUpdater,
+  google-api-core,
+  google-auth,
+  lib,
+  proto-plus,
+  protobuf,
+  pytestCheckHook,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "google-geo-type";
+  version = "0.7.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "googleapis";
+    repo = "google-cloud-python";
+    tag = "google-geo-type-v${version}";
+    hash = "sha256-M/7uDWWz4YCfxa4gyM9BaAo10iyTMvtR2MhNpdFYnis=";
+  };
+
+  sourceRoot = "${src.name}/packages/google-geo-type";
+
+  build-system = [ setuptools ];
+
+  pythonRelaxDeps = [
+    "protobuf"
+  ];
+
+  dependencies = [
+    google-api-core
+    google-auth
+    proto-plus
+    protobuf
+  ]
+  ++ google-api-core.optional-dependencies.grpc;
+
+  pythonImportsCheck = [ "google.geo.type" ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  meta = {
+    description = "Google Geo Type API client library";
+    homepage = "https://github.com/googleapis/google-cloud-python/tree/main/packages/google-geo-type";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
