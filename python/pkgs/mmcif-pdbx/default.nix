d8f6566521a4f1ec92cc3476f3ae7a94b737e17e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:08 -0700

    python3Packages.mmcif-pdbx: init at 2.1.0

---
+++ python/pkgs/mmcif-pdbx/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "mmcif-pdbx";
+  version = "2.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Electrostatics";
+    repo = "mmcif_pdbx";
+    tag = "v${version}";
+    hash = "sha256-HzRJ8bzUHAmF7WA20DefvgSNDxMaqJCxfgqTHrS0BqU=";
+  };
+
+  nativeBuildInputs = [ setuptools ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "pdbx" ];
+
+  meta = {
+    description = "Yet another version of PDBx/mmCIF Python implementation";
+    homepage = "https://github.com/Electrostatics/mmcif_pdbx";
+    license = lib.licenses.cc0;
+    maintainers = [ ];
+  };
+}
