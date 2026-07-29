75f8f4a4eeea1087c7366bb567c742cec1e3a884
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:07 -0700

    python3Packages.chameleon: init at 4.6.0

---
+++ python/pkgs/chameleon/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "chameleon";
+  version = "4.6.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "malthe";
+    repo = "chameleon";
+    tag = version;
+    hash = "sha256-zCEM5yl8Y11FbexD7veS9bFJgm30L6fsTde59m2t1ec=";
+  };
+
+  build-system = [ setuptools ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "chameleon" ];
+
+  meta = {
+    description = "Fast HTML/XML Template Compiler";
+    downloadPage = "https://github.com/malthe/chameleon";
+    homepage = "https://chameleon.readthedocs.io";
+    license = lib.licenses.bsd0;
+    maintainers = [ ];
+  };
+}
