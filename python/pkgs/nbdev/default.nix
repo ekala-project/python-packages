214438a0929795a5ae3519be072375c06ac80e18
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:27 -0700

    python3Packages.nbdev: init at 3.0.15

---
+++ python/pkgs/nbdev/default.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  build,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  ipywidgets,
+  fastcore,
+  fastgit,
+  astunparse,
+  watchdog,
+  execnb,
+  ghapi,
+  pyyaml,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "nbdev";
+  version = "3.0.15";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-CSpEJr8IYUFa54VGdPy0p8erNh5qKrGBTsfNYvE3uIo=";
+  };
+
+  pythonRelaxDeps = [ "ipywidgets" ];
+
+  build-system = [
+    build
+    setuptools
+  ];
+
+  dependencies = [
+    astunparse
+    execnb
+    fastcore
+    fastgit
+    ghapi
+    ipywidgets
+    pyyaml
+    watchdog
+  ];
+
+  # no real tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "nbdev" ];
+
+  meta = {
+    homepage = "https://github.com/AnswerDotAI/nbdev";
+    description = "Create delightful software with Jupyter Notebooks";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
