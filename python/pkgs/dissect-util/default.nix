2ff1428d379fe7c3cc4f070de7e89391cb753ba0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:42 -0700

    python3Packages.dissect-util: init at 3.24

---
+++ python/pkgs/dissect-util/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  setuptools-scm,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "dissect-util";
+  version = "3.24";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "fox-it";
+    repo = "dissect.util";
+    tag = version;
+    hash = "sha256-AbkIVtUbQkkui7H1ZT/xHl1tCfZMvlrbZ2RD3YJAh0E=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "dissect.util" ];
+
+  disabledTests = [
+    # File handling issue
+    "test_cpio_formats"
+  ];
+
+  meta = {
+    description = "Dissect module implementing various utility functions for the other Dissect modules";
+    homepage = "https://github.com/fox-it/dissect.util";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    mainProgram = "dump-nskeyedarchiver";
+  };
+}
