7fc134113b6dc21a02d61d904d929be704ce84ea
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:42:52 -0700

    python3Packages.mdformat-myst: init at 0.3.0

---
+++ python/pkgs/mdformat-myst/default.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  flit-core,
+
+  # dependencies
+  mdformat,
+  mdformat-footnote,
+  mdformat-front-matters,
+  mdformat-gfm,
+  mdit-py-plugins,
+  ruamel-yaml,
+
+  # tests
+  pytestCheckHook,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "mdformat-myst";
+  version = "0.3.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "executablebooks";
+    repo = "mdformat-myst";
+    tag = finalAttrs.version;
+    hash = "sha256-y0zN47eK0UqTHx6ft/OrczAjdHdmPKIByCnz1c1JURQ=";
+  };
+
+  build-system = [ flit-core ];
+
+  dependencies = [
+    mdformat
+    mdformat-footnote
+    mdformat-front-matters
+    mdformat-gfm
+    mdit-py-plugins
+    ruamel-yaml
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "mdformat_myst" ];
+
+  meta = {
+    description = "Mdformat plugin for MyST compatibility";
+    homepage = "https://github.com/executablebooks/mdformat-myst";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
