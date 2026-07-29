ec1357ae6d16a9ea16107e1163cd3b6f7519cdd4
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:54 -0700

    python3Packages.bot-safe-agents: init at 1.2

---
+++ python/pkgs/bot-safe-agents/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "bot-safe-agents";
+  version = "1.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ivan-sincek";
+    repo = "bot-safe-agents";
+    tag = "v${version}";
+    hash = "sha256-b4X3z8jX8kF3G1CEYxWl2jhWid3fRl7WfIMRfvAWlko=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "bot_safe_agents" ];
+
+  meta = {
+    description = "Library for fetching a list of bot-safe user agents";
+    homepage = "https://github.com/ivan-sincek/bot-safe-agents";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
