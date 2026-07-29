56682864f2e4bac59af9da789c737bbc94ff3784
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:39 -0700

    python3Packages.dicomweb-client: init at 0.61.0

---
+++ python/pkgs/dicomweb-client/default.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  uv-dynamic-versioning,
+  pytestCheckHook,
+  pytest-localserver,
+  numpy,
+  pillow,
+  pydicom,
+  requests,
+  retrying,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "dicomweb-client";
+  version = "0.61.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ImagingDataCommons";
+    repo = "dicomweb-client";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-uCImuJDZr2gyWnLCU2JCmkGO/EloRty1fIRujwzYzAg=";
+  };
+
+  build-system = [
+    hatchling
+    uv-dynamic-versioning
+  ];
+
+  dependencies = [
+    numpy
+    pillow
+    pydicom
+    requests
+    retrying
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-localserver
+  ];
+
+  pythonImportsCheck = [ "dicomweb_client" ];
+
+  meta = {
+    description = "Python client for DICOMweb RESTful services";
+    homepage = "https://dicomweb-client.readthedocs.io";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "dicomweb_client";
+  };
+})
