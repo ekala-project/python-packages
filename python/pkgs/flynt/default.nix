d325e9e82331803d4ff83413b862ee8501717177
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:40 -0700

    python3Packages.flynt: init at 1.0.6

---
+++ python/pkgs/flynt/default.nix
@@ -0,0 +1,44 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  lib,
+  pytestCheckHook,
+  hatch-vcs,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "flynt";
+  version = "1.0.6";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ikamensh";
+    repo = "flynt";
+    tag = finalAttrs.version;
+    hash = "sha256-SkkCA4fEHplt9HkEn+QOq4k9lW5qJeZzLZEbNEtKBSo=";
+  };
+
+  build-system = [
+    hatchling
+    hatch-vcs
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "flynt" ];
+
+  disabledTests = [
+    # AssertionError
+    "test_fstringify"
+    "test_mixed_quote_types_unsafe"
+  ];
+
+  meta = {
+    description = "Tool to automatically convert old string literal formatting to f-strings";
+    homepage = "https://github.com/ikamensh/flynt";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "flynt";
+  };
+})
