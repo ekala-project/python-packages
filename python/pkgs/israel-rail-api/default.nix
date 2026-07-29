784fac226aa0a45df6343ef8d5aa96876e6b76db
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:45 -0700

    python3Packages.israel-rail-api: init at 0.1.5

---
+++ python/pkgs/israel-rail-api/default.nix
@@ -0,0 +1,40 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  lib,
+  pytestCheckHook,
+  pytz,
+  requests,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "israel-rail-api";
+  version = "0.1.5";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "sh0oki";
+    repo = "israel-rail-api";
+    tag = "v${version}";
+    hash = "sha256-kcux4IBA3FoNnsqNGHsEta9OAkvjYB40234VlidrNzM=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    pytz
+    requests
+  ];
+
+  pythonImportsCheck = [ "israelrailapi" ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  meta = {
+    description = "Python wrapping of the Israeli Rail API";
+    homepage = "https://github.com/sh0oki/israel-rail-api";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
