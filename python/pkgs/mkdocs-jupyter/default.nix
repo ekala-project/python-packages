fd7ad80015cc604387accc6fd6c226adb88d8797
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:06 -0700

    python3Packages.mkdocs-jupyter: init at 0.26.1

---
+++ python/pkgs/mkdocs-jupyter/default.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  hatchling,
+  ipykernel,
+  jupytext,
+  mkdocs,
+  mkdocs-material,
+  nbconvert,
+  pygments,
+  pytestCheckHook,
+  pytest-cov-stub,
+  writableTmpDirAsHomeHook,
+}:
+
+buildPythonPackage rec {
+  pname = "mkdocs-jupyter";
+  version = "0.26.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "mkdocs_jupyter";
+    inherit version;
+    hash = "sha256-fIDA05U96R5bQKDTIJIzeVyPgAJDqymOTsOOBQTtpjA=";
+  };
+
+  pythonRelaxDeps = [
+    "ipykernel"
+    "nbconvert"
+  ];
+
+  build-system = [ hatchling ];
+
+  nativeBuildInputs = [
+    writableTmpDirAsHomeHook
+  ];
+
+  dependencies = [
+    ipykernel
+    jupytext
+    mkdocs
+    mkdocs-material
+    nbconvert
+    pygments
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-cov-stub
+  ];
+
+  pythonImportsCheck = [ "mkdocs_jupyter" ];
+
+  __darwinAllowLocalNetworking = true;
+
+  meta = {
+    description = "Use Jupyter Notebook in mkdocs";
+    homepage = "https://github.com/danielfrg/mkdocs-jupyter";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
