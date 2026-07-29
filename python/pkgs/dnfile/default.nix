d0cfc84bd1202b5c025821b52ffaf855efff871e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:00 -0700

    python3Packages.dnfile: init at 0.18.0

---
+++ python/pkgs/dnfile/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pefile,
+  pytestCheckHook,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "dnfile";
+  version = "0.18.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "malwarefrank";
+    repo = "dnfile";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-lIhhiOtMZnYziGeLUK7awJSibP3k8JCYg43jvIl5Puw=";
+    fetchSubmodules = true;
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ pefile ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "dnfile" ];
+
+  meta = {
+    description = "Module to parse .NET executable files";
+    homepage = "https://github.com/malwarefrank/dnfile";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
