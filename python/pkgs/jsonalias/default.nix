2e20e00848afe85d8d672d8eda92fecbe377fe67
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:51 -0700

    python3Packages.jsonalias: init at 0.1.2

---
+++ python/pkgs/jsonalias/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  poetry-core,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "jsonalias";
+  version = "0.1.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "kevinheavey";
+    repo = "jsonalias";
+    tag = finalAttrs.version;
+    hash = "sha256-1Pb0VpwnAZiv3z+Ur6FS0LV4D9xKvrfAdUtulvr6ACg=";
+  };
+
+  build-system = [ poetry-core ];
+
+  pythonImportsCheck = [ "jsonalias" ];
+
+  # Module has no tests
+  doCheck = false;
+
+  meta = {
+    description = "Library that defines a Json type alias for Python";
+    homepage = "https://github.com/kevinheavey/jsonalias";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
