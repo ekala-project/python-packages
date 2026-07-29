d0ddc4562e9e11e408c01972971458be8ae92a4f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:28 -0700

    python3Packages.hyponcloud: init at 1.0.1

---
+++ python/pkgs/hyponcloud/default.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  aiohttp,
+  buildPythonPackage,
+  fetchFromGitHub,
+  mashumaro,
+  pytest-asyncio,
+  pytestCheckHook,
+  setuptools,
+  setuptools-scm,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "hyponcloud";
+  version = "1.0.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jcisio";
+    repo = "hyponcloud";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Mn+OZHpDSMgA3mUi1s2t+HTlsnsN9eFfzdNNddDz6OA=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    aiohttp
+    mashumaro
+  ];
+
+  nativeCheckInputs = [
+    pytest-asyncio
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "hyponcloud" ];
+
+  meta = {
+    description = "Python library for Hypontech Cloud API for solar inverter monitoring";
+    homepage = "https://github.com/jcisio/hyponcloud";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
