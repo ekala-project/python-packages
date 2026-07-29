5d1a1fde19fa5b21e0493779378e42c941b53013
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:24 -0700

    python3Packages.apykuma: init at 1.2.0

---
+++ python/pkgs/apykuma/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildPythonPackage,
+  poetry-core,
+  aiohttp,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "apykuma";
+  version = "1.2.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "PerchunPak";
+    repo = "apykuma";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Dxlyi0syoq+sfgjMLWHhpeKhDFgpfQrp18DJeBjrAEg=";
+  };
+
+  build-system = [
+    poetry-core
+  ];
+
+  dependencies = [
+    aiohttp
+  ];
+
+  # has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "apykuma"
+  ];
+
+  meta = {
+    description = "Small library to notify Uptime Kuma that the service is up";
+    homepage = "https://github.com/PerchunPak/apykuma";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
