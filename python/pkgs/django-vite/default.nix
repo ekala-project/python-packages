4528d9a520058af3cf3afef0dfa8cefe705fdee6
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:57 -0700

    python3Packages.django-vite: init at 3.1.0

---
+++ python/pkgs/django-vite/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildPythonPackage,
+  django,
+}:
+
+buildPythonPackage rec {
+  pname = "django-vite";
+  version = "3.1.0";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "MrBin99";
+    repo = "django-vite";
+    tag = version;
+    hash = "sha256-S5DpU0Sw0TOY1SNici6djeTrvg4gehH/a2UCzju1e/s=";
+  };
+
+  propagatedBuildInputs = [ django ];
+
+  # Package doesn’t have any tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "django_vite" ];
+
+  meta = {
+    description = "Integration of ViteJS in a Django project";
+    homepage = "https://github.com/MrBin99/django-vite";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
