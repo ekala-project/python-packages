dea768c3541569400f513b34d5022d755b8e26fc
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:29 -0700

    python3Packages.find-libpython: init at 0.5.1

---
+++ python/pkgs/find-libpython/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytestCheckHook,
+  setuptools,
+  setuptools-scm,
+}:
+
+buildPythonPackage rec {
+  pname = "find-libpython";
+  version = "0.5.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ktbarrett";
+    repo = "find_libpython";
+    tag = "v${version}";
+    hash = "sha256-g2Gl+usa1mJMvvumynnoy/ckFTSrFA57o339t2j9lWQ=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "find_libpython" ];
+
+  meta = {
+    description = "Finds the libpython associated with your environment, wherever it may be hiding";
+    homepage = "https://github.com/ktbarrett/find_libpython";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "find_libpython";
+  };
+}
