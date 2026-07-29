e65d142f614fe544786ed69315c3eb2ab83df93b
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:21 -0700

    python3Packages.execnb: init at 0.1.18

---
+++ python/pkgs/execnb/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  buildPythonPackage,
+  fastcore,
+  fetchPypi,
+  ipython,
+  setuptools,
+  traitlets,
+}:
+
+buildPythonPackage rec {
+  pname = "execnb";
+  version = "0.1.18";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-VM2WsdFICc7trCjfupL5wW0UFTzRW5RPfWs6jYpCSDM=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    fastcore
+    ipython
+    traitlets
+  ];
+
+  # no real tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "execnb" ];
+
+  meta = {
+    description = "Execute a jupyter notebook, fast, without needing jupyter";
+    homepage = "https://github.com/fastai/execnb";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    mainProgram = "exec_nb";
+  };
+}
