1e5cfe2fb07c92edabc65c05adc8d8b6ec6bf865
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:22 -0700

    python3Packages.countryinfo: init at 1.0.1

---
+++ python/pkgs/countryinfo/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  poetry-core,
+  pydantic,
+  pytestCheckHook,
+  typer,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "countryinfo";
+  version = "1.0.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "porimol";
+    repo = "countryinfo";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-PE9XiVH6XE+OSySL5Lo0MPWyIEX8xgeHQB7MttMfmz8=";
+  };
+
+  pythonRelaxDeps = [ "typer" ];
+
+  build-system = [ poetry-core ];
+
+  dependencies = [
+    pydantic
+    typer
+  ];
+
+  pythonImportsCheck = [ "countryinfo" ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  meta = {
+    description = "Data about countries, ISO info and states/provinces within them";
+    homepage = "https://github.com/porimol/countryinfo";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
