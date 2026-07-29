f3ebbd435e5488bc430ab547c7f2aec1d72db86f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:37:55 -0700

    python3Packages.aioairzone-cloud: init at 0.7.2

---
+++ python/pkgs/aioairzone-cloud/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  aiohttp,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "aioairzone-cloud";
+  version = "0.7.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Noltari";
+    repo = "aioairzone-cloud";
+    tag = version;
+    hash = "sha256-fWd5feCWE2o0HqvzGhGngWsIkXtS+VdZJ0d6B10Jq1E=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ aiohttp ];
+
+  pythonImportsCheck = [ "aioairzone_cloud" ];
+
+  # Module has no tests
+  doCheck = false;
+
+  meta = {
+    description = "Library to control Airzone via Cloud API";
+    homepage = "https://github.com/Noltari/aioairzone-cloud";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
