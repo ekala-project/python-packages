326a215ac9011bd760e91dec9ce0dd8cd3a55feb
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:37:57 -0700

    python3Packages.aiocomelit: init at 2.0.7

---
+++ python/pkgs/aiocomelit/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  aiohttp,
+  aioresponses,
+  anyio,
+  buildPythonPackage,
+  fetchFromGitHub,
+  orjson,
+  pint,
+  pytestCheckHook,
+  setuptools,
+  writableTmpDirAsHomeHook,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "aiocomelit";
+  version = "2.0.7";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "chemelli74";
+    repo = "aiocomelit";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-T48aRtuF9eNrW5L97CGkjc2PCdRzbuGCvhdWCuqe7yk=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    aiohttp
+    orjson
+    pint
+  ];
+
+  nativeCheckInputs = [
+    aioresponses
+    anyio
+    pytestCheckHook
+    writableTmpDirAsHomeHook
+  ];
+
+  pythonImportsCheck = [ "aiocomelit" ];
+
+  meta = {
+    description = "Library to control Comelit Simplehome";
+    homepage = "https://github.com/chemelli74/aiocomelit";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
