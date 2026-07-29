779f3970458d3ea97dfcd7d526d843402e0c5353
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:36 -0700

    python3Packages.in-place: init at 1.0.1

---
+++ python/pkgs/in-place/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  pytest-cov-stub,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "in-place";
+  version = "1.0.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jwodder";
+    repo = "inplace";
+    tag = "v${version}";
+    hash = "sha256-PyOSuHHtftEPwL3mTwWYStZNXYX3EhptKfTu0PJjOZ8=";
+  };
+
+  build-system = [ hatchling ];
+
+  nativeCheckInputs = [
+    pytest-cov-stub
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "in_place" ];
+
+  meta = {
+    description = "In-place file processing";
+    homepage = "https://github.com/jwodder/inplace";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
