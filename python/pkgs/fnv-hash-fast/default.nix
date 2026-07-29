86e4f82e91da147ccc841fc3e4ad6973fee13342
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:40 -0700

    python3Packages.fnv-hash-fast: init at 2.0.3

---
+++ python/pkgs/fnv-hash-fast/default.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  cython,
+  poetry-core,
+  setuptools,
+  fnvhash,
+  pytest-codspeed,
+  pytest-cov-stub,
+  pytestCheckHook,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "fnv-hash-fast";
+  version = "2.0.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "bdraco";
+    repo = "fnv-hash-fast";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-yDEgVNaSqZ1AJivpkpinZznKlPEXH6mjXBe5aVp/3hQ=";
+  };
+
+  build-system = [
+    cython
+    poetry-core
+    setuptools
+  ];
+
+  dependencies = [ fnvhash ];
+
+  pythonImportsCheck = [ "fnv_hash_fast" ];
+
+  nativeCheckInputs = [
+    pytest-codspeed
+    pytest-cov-stub
+    pytestCheckHook
+  ];
+
+  meta = {
+    description = "Fast version of fnv1a";
+    homepage = "https://github.com/bdraco/fnv-hash-fast";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
