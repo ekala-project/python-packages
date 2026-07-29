57a1327cf7ddee063bd299b4f47a3ed080737f47
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:42:26 -0700

    python3Packages.locate: init at 1.1.2

---
+++ python/pkgs/locate/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytestCheckHook,
+  setuptools,
+  setuptools-scm,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "locate";
+  version = "1.1.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "AutoActuary";
+    repo = "locate";
+    tag = finalAttrs.version;
+    hash = "sha256-cPBxSjcX0y0Ul7B6yOY+U2ylrQ1oBV6t3lN4qyutGlE=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "locate" ];
+
+  meta = {
+    description = "Locate files relative to the current Python script";
+    homepage = "https://github.com/AutoActuary/locate";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
