41fd128f3bcf4d9699fa9487049d14a15ef6536f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:03 -0700

    python3Packages.aiokem: init at 1.1.4

---
+++ python/pkgs/aiokem/default.nix
@@ -0,0 +1,51 @@
+{
+  aiohttp,
+  buildPythonPackage,
+  fetchFromGitHub,
+  lib,
+  pyjwt,
+  pytest-asyncio,
+  pytest-cov-stub,
+  pytestCheckHook,
+  pythonOlder,
+  setuptools,
+  syrupy,
+}:
+
+buildPythonPackage rec {
+  pname = "aiokem";
+  version = "1.1.4";
+  pyproject = true;
+
+  disabled = pythonOlder "3.12";
+
+  src = fetchFromGitHub {
+    owner = "kohlerlibs";
+    repo = "aiokem";
+    tag = "v${version}";
+    hash = "sha256-4LbpTov81LMr4V8jMgttlUCyHWJoR6tExOvt8X4Telc=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    aiohttp
+    pyjwt
+  ];
+
+  pythonImportsCheck = [ "aiokem" ];
+
+  nativeCheckInputs = [
+    pytest-asyncio
+    pytest-cov-stub
+    pytestCheckHook
+    syrupy
+  ];
+
+  meta = {
+    description = "Async API for Kohler Energy Management";
+    homepage = "https://github.com/kohlerlibs/aiokem";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
