24ba8a561fcffd7f91a4b8deee450a34dcd7d80f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:35 -0700

    python3Packages.flask-mysqldb: init at 2.0.0

---
+++ python/pkgs/flask-mysqldb/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  flask,
+  mysqlclient,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "flask-mysqldb";
+  version = "2.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "alexferl";
+    repo = "flask-mysqldb";
+    rev = "v${version}";
+    hash = "sha256-RHAB9WGRzojH6eAOG61QguwF+4LssO9EcFjbWxoOtF4=";
+  };
+
+  nativeBuildInputs = [ setuptools ];
+
+  propagatedBuildInputs = [
+    flask
+    mysqlclient
+  ];
+
+  pythonImportsCheck = [ "flask_mysqldb" ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  meta = {
+    description = "MySQL connection support for Flask";
+    homepage = "https://github.com/alexferl/flask-mysqldb";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
