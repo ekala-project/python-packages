81d6b96b18af249c91984a0ecbf4258e0cedf7b5
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:38 -0700

    python3Packages.intbitset: init at 4.1.2

---
+++ python/pkgs/intbitset/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  fetchPypi,
+  buildPythonPackage,
+  pytestCheckHook,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "intbitset";
+  version = "4.1.2";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-+C+v4Ly0/noBDZQgmbWoTXIdN8iXU47WMveIliwUEfg=";
+  };
+
+  build-system = [ setuptools ];
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "intbitset" ];
+
+  meta = {
+    description = "C-based extension implementing fast integer bit sets";
+    homepage = "https://github.com/inveniosoftware/intbitset";
+    license = lib.licenses.lgpl3Plus;
+    maintainers = [ ];
+  };
+})
