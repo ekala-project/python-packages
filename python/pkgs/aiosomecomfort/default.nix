093c70d4f6c87f05ad97a00eb077071e7cfbcd62
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:09 -0700

    python3Packages.aiosomecomfort: init at 0.0.38

---
+++ python/pkgs/aiosomecomfort/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  aiohttp,
+  buildPythonPackage,
+  fetchFromGitHub,
+  prettytable,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "aiosomecomfort";
+  version = "0.0.38";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "mkmer";
+    repo = "AIOSomecomfort";
+    tag = version;
+    hash = "sha256-tjJazA5MFIr3Z3I8l9g6SngH6U616e9jWXfMrim2wPo=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  pythonRelaxDeps = [
+    "aiohttp"
+  ];
+
+  dependencies = [
+    aiohttp
+    prettytable
+  ];
+
+  pythonImportsCheck = [ "aiosomecomfort" ];
+
+  # Tests only run on Windows, due to WindowsSelectorEventLoopPolicy
+  doCheck = false;
+
+  meta = {
+    description = "AsyicIO client for US models of Honeywell Thermostats";
+    homepage = "https://github.com/mkmer/AIOSomecomfort";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+  };
+}
