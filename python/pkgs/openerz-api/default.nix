5dbb2e668d9f7de7a95a3a9cd74a3b552d761ef5
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:52 -0700

    python3Packages.openerz-api: init at 0.3.0

---
+++ python/pkgs/openerz-api/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytestCheckHook,
+  requests,
+  setuptools,
+  testfixtures,
+}:
+
+buildPythonPackage rec {
+  pname = "openerz-api";
+  version = "0.3.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "misialq";
+    repo = "openerz-api";
+    tag = "v${version}";
+    hash = "sha256-CwK61StspZJt0TALv76zfibUzlriwp9HRoYOtX9bU+c=";
+  };
+
+  nativeBuildInputs = [ setuptools ];
+
+  propagatedBuildInputs = [ requests ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    testfixtures
+  ];
+
+  pythonImportsCheck = [ "openerz_api" ];
+
+  meta = {
+    description = "Python module to interact with the OpenERZ API";
+    homepage = "https://github.com/misialq/openerz-api";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
