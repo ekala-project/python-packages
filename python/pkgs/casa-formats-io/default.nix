dde5e918bf8b1017f5d83df7463e4699cbf6e340
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:02 -0700

    python3Packages.casa-formats-io: init at 0.3.0

---
+++ python/pkgs/casa-formats-io/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  astropy,
+  buildPythonPackage,
+  dask,
+  fetchPypi,
+  numpy,
+  oldest-supported-numpy,
+  setuptools-scm,
+}:
+
+buildPythonPackage rec {
+  pname = "casa-formats-io";
+  version = "0.3.0";
+  format = "setuptools";
+  prproject = true;
+
+  src = fetchPypi {
+    pname = "casa_formats_io";
+    inherit version;
+    hash = "sha256-FpQj0XeZ7vvOzUM/+5qG6FRwNXl3gzoUBItYdQ1M4m4=";
+  };
+
+  build-system = [ setuptools-scm ];
+
+  nativeBuildInputs = [ oldest-supported-numpy ];
+
+  dependencies = [
+    astropy
+    dask
+    numpy
+  ];
+
+  # Tests require a large (800 Mb) dataset
+  doCheck = false;
+
+  pythonImportsCheck = [ "casa_formats_io" ];
+
+  meta = {
+    description = "Dask-based reader for CASA data";
+    homepage = "https://casa-formats-io.readthedocs.io/";
+    license = lib.licenses.lgpl2Only;
+    maintainers = [ ];
+  };
+}
