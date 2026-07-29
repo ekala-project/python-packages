1f5a53a3e63ab246e5479bd4c554b746cd269ccd
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:43 -0700

    python3Packages.bayesian-optimization: init at 3.3.0

---
+++ python/pkgs/bayesian-optimization/default.nix
@@ -0,0 +1,70 @@
+{
+  lib,
+  stdenv,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  uv-build,
+
+  # dependencies
+  scikit-learn,
+  numpy,
+  scipy,
+  colorama,
+  packaging,
+
+  # tests
+  jupyter,
+  matplotlib,
+  nbconvert,
+  nbformat,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "bayesian-optimization";
+  version = "3.3.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "bayesian-optimization";
+    repo = "BayesianOptimization";
+    tag = "v${version}";
+    hash = "sha256-tehmtySHiwXQBFZDcj87DAP0WMyp3kDXW0LZApkHZwY=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "uv_build>=0.7.21,<0.8.0" "uv_build"
+  '';
+
+  build-system = [ uv-build ];
+
+  dependencies = [
+    scikit-learn
+    numpy
+    scipy
+    colorama
+    packaging
+  ];
+
+  nativeCheckInputs = [
+    jupyter
+    matplotlib
+    nbconvert
+    nbformat
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "bayes_opt" ];
+
+  __darwinAllowLocalNetworking = true;
+
+  meta = {
+    description = "Python implementation of global optimization with gaussian processes";
+    homepage = "https://github.com/bayesian-optimization/BayesianOptimization";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
