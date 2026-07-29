a163432a01d825b9de32e89986eb436cf198bca2
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:21 -0700

    python3Packages.annoy: init at 1.17.3

---
+++ python/pkgs/annoy/default.nix
@@ -0,0 +1,64 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  setuptools,
+
+  # nativeBuildInputs
+  h5py,
+
+  # tests
+  numpy,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "annoy";
+  version = "1.17.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "spotify";
+    repo = "annoy";
+    tag = "v${version}";
+    hash = "sha256-oJHW4lULRun2in35pBGOKg44s5kgLH2BKiMOzVu4rf4=";
+  };
+
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace-fail "'nose>=1.0'" ""
+  '';
+
+  build-system = [ setuptools ];
+
+  nativeBuildInputs = [ h5py ];
+
+  nativeCheckInputs = [
+    numpy
+    pytestCheckHook
+  ];
+
+  preCheck = ''
+    rm -rf annoy
+  '';
+
+  disabledTestPaths = [
+    # network access
+    "test/accuracy_test.py"
+  ];
+
+  pythonImportsCheck = [ "annoy" ];
+
+  meta = {
+    description = "Approximate Nearest Neighbors in C++/Python optimized for memory usage and loading/saving to disk";
+    homepage = "https://github.com/spotify/annoy";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    badPlatforms = [
+      # Several tests fail with AssertionError
+      lib.systems.inspect.patterns.isDarwin
+    ];
+  };
+}
