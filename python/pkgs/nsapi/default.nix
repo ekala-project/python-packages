f0c68d5b2c233d8c38c240281ee4e1bd6ff0bbba
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:38 -0700

    python3Packages.nsapi: init at 3.1.3

---
+++ python/pkgs/nsapi/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytz,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "nsapi";
+  version = "3.1.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "aquatix";
+    repo = "ns-api";
+    tag = "v${version}";
+    hash = "sha256-Buhc0643WeX/4ZU/RkzNWiFjfEAJUtNL6uJ98unTnCg=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ pytz ];
+
+  # Project has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "ns_api" ];
+
+  meta = {
+    description = "Python module to query routes of the Dutch railways";
+    homepage = "https://github.com/aquatix/ns-api/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
