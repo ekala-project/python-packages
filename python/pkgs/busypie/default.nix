d24cc603f30de41beec691d1b406da119d8b1140
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:38:59 -0700

    python3Packages.busypie: init at 0.6.1

---
+++ python/pkgs/busypie/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytest-asyncio,
+  pytest-timeout,
+  pytestCheckHook,
+  setuptools,
+  setuptools-scm,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "busypie";
+  version = "0.6.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "rockem";
+    repo = "busypie";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-MIwME5QM0BDpYP9frraJP/1v0lTZpPzgbqAawpGAcU0=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  nativeCheckInputs = [
+    pytest-asyncio
+    pytest-timeout
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "busypie" ];
+
+  meta = {
+    description = "Expressive busy wait for Python";
+    homepage = "https://github.com/rockem/busypie";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
