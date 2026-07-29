3fb2c5410c083973f5e8d08a4b8324ed8fdbb7a0
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:42:57 -0700

    python3Packages.metakernel: init at 0.32.0

---
+++ python/pkgs/metakernel/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  hatchling,
+  ipykernel,
+  jedi,
+  jupyter-core,
+  pexpect,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "metakernel";
+  version = "0.32.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-AxmEtMBinBKchhYtJ72N8mTWmTv5Ya7HMP23H6zv3bw=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    ipykernel
+    jedi
+    jupyter-core
+    pexpect
+  ];
+
+  # Tests hang, so disable
+  doCheck = false;
+
+  pythonImportsCheck = [ "metakernel" ];
+
+  meta = {
+    description = "Jupyter/IPython Kernel Tools";
+    homepage = "https://github.com/Calysto/metakernel";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+})
