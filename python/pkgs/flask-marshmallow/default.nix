05ec359b08ff8c62e2bd1cef18b0057aa6ca1553
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:35 -0700

    python3Packages.flask-marshmallow: init at 1.5.0

---
+++ python/pkgs/flask-marshmallow/default.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  flask,
+  flask-sqlalchemy,
+  flit-core,
+  marshmallow,
+  marshmallow-sqlalchemy,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "flask-marshmallow";
+  version = "1.5.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "marshmallow-code";
+    repo = "flask-marshmallow";
+    tag = version;
+    hash = "sha256-IzeVVkyf4BRxtUVQIfzAvyjaKG+BLwhruXZHFJ6iGmw=";
+  };
+
+  build-system = [ flit-core ];
+
+  dependencies = [
+    flask
+    marshmallow
+  ];
+
+  optional-dependencies = {
+    sqlalchemy = [
+      flask-sqlalchemy
+      marshmallow-sqlalchemy
+    ];
+  };
+
+  nativeCheckInputs = [ pytestCheckHook ] ++ optional-dependencies.sqlalchemy;
+
+  pythonImportsCheck = [ "flask_marshmallow" ];
+
+  pytestFlags = [
+    "-Wignore::DeprecationWarning"
+  ];
+
+  meta = {
+    description = "Flask + marshmallow for beautiful APIs";
+    homepage = "https://github.com/marshmallow-code/flask-marshmallow";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
