08e70359825ece737779fb03ce3d75987a98d9a8
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:37 -0700

    python3Packages.deezer-python: init at 7.3.0

---
+++ python/pkgs/deezer-python/default.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  buildPythonPackage,
+  environs,
+  fetchFromGitHub,
+  httpx,
+  setuptools,
+  pytest-asyncio,
+  pytest-cov-stub,
+  pytest-mock,
+  pytest-vcr,
+  pytestCheckHook,
+  tornado,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "deezer-python";
+  version = "7.3.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "browniebroke";
+    repo = "deezer-python";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-pCrPlEbt5Mx8qGjewR5+Z/W7rFEehqd7QRrtvPGyKJk=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ httpx ];
+
+  nativeCheckInputs = [
+    environs
+    pytest-asyncio
+    pytest-cov-stub
+    pytest-mock
+    pytest-vcr
+    pytestCheckHook
+    tornado
+  ];
+
+  pythonImportsCheck = [ "deezer" ];
+
+  disabledTests = [
+    # JSONDecodeError issue
+    "test_get_user_flow"
+    "test_with_language_header"
+  ];
+
+  meta = {
+    description = "Python wrapper around the Deezer API";
+    homepage = "https://github.com/browniebroke/deezer-python";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
