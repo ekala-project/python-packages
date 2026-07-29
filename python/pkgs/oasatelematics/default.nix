ab9b0e6454f94712361d53b965adfe13670ce415
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:42 -0700

    python3Packages.oasatelematics: init at 0.4

---
+++ python/pkgs/oasatelematics/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytestCheckHook,
+  requests,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "oasatelematics";
+  version = "0.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "panosmz";
+    repo = "oasatelematics";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-pqjzvLeVGW5rC3lK/9WnYdCLyF1fM7GGwzEG5h2lKPA=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ requests ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "oasatelematics" ];
+
+  meta = {
+    description = "Python wrapper for the OASA Telematics API";
+    homepage = "https://github.com/panosmz/oasatelematics";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+  };
+})
