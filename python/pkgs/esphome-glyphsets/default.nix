d64530f665ad15945f7019a9e76ed6c0ae79a22f
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:18 -0700

    python3Packages.esphome-glyphsets: init at 0.2.0

---
+++ python/pkgs/esphome-glyphsets/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "esphome-glyphsets";
+  version = "0.2.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "esphome";
+    repo = "esphome-glyphsets";
+    tag = "v${version}";
+    hash = "sha256-nM8omtLIWwIY6AGVqVR2/4twmMlOj21+9cSuyXzAAXY=";
+    fetchSubmodules = true;
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [
+    "esphome_glyphsets"
+  ];
+
+  meta = {
+    description = "Lightweight version of glyphsets for ESPHome";
+    homepage = "https://github.com/esphome/esphome-glyphsets";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
