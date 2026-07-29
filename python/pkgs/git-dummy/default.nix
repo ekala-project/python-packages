20be3300f916760e026490e4250c944588f3db71
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:40:54 -0700

    python3Packages.git-dummy: init at 0.1.2

---
+++ python/pkgs/git-dummy/default.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  stdenv,
+  buildPythonPackage,
+  fetchFromGitHub,
+
+  # build-system
+  setuptools,
+
+  # dependencies
+  gitpython,
+  pydantic-settings,
+  typer,
+
+  # nativeBuildInputs
+  installShellFiles,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "git-dummy";
+  version = "0.1.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "initialcommit-com";
+    repo = "git-dummy";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-viybxn2J7SO7NgSvjwlP+tgtm+H6QrACafIy82d9XEk=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    gitpython
+    pydantic-settings
+    typer
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  postInstall =
+    # https://github.com/NixOS/nixpkgs/issues/308283
+    lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+      installShellCompletion --cmd git-dummy \
+        --bash <($out/bin/git-dummy --show-completion bash) \
+        --fish <($out/bin/git-dummy --show-completion fish) \
+        --zsh <($out/bin/git-dummy --show-completion zsh)
+    '';
+
+  meta = {
+    description = "Generate dummy Git repositories populated with the desired number of commits, branches, and structure";
+    homepage = "https://github.com/initialcommit-com/git-dummy";
+    license = lib.licenses.gpl2Only;
+    maintainers = [ ];
+    mainProgram = "git-dummy";
+  };
+})
