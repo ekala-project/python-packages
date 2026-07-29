d0a3bba3b81e4f3f30ec0a8136cd01c66d765b3a
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:43:44 -0700

    python3Packages.ocifs: init at 1.3.1

---
+++ python/pkgs/ocifs/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  flit-core,
+  fsspec,
+  oci,
+  requests,
+}:
+
+buildPythonPackage rec {
+  pname = "ocifs";
+  version = "1.3.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "oracle";
+    repo = "ocifs";
+    tag = "v${version}";
+    hash = "sha256-IGl9G4NyzhcqrfYfgeZin+wt1OwHmh6780MPfZBwsXA=";
+  };
+
+  build-system = [ flit-core ];
+
+  dependencies = [
+    fsspec
+    oci
+    requests
+  ];
+
+  # Module has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "ocifs" ];
+
+  meta = {
+    description = "Oracle Cloud Infrastructure Object Storage fsspec implementation";
+    homepage = "https://ocifs.readthedocs.io";
+    license = lib.licenses.upl;
+    maintainers = [ ];
+  };
+}
