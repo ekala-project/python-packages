b23c29a7930218110bbda252eccbb6225d576464
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:39:57 -0700

    python3Packages.django-waffle: init at 5.0.0

---
+++ python/pkgs/django-waffle/default.nix
@@ -0,0 +1,47 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  django,
+  lib,
+  setuptools,
+  pytestCheckHook,
+  pytest-django,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "django-waffle";
+  version = "5.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "django-waffle";
+    repo = "django-waffle";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-wirB2Y4iONmAMVt9o8aTkeB1WQzcvktQOAMEeXMM1x8=";
+  };
+
+  patches = [
+    # Middleware object requires a request -> response callable
+    ./middleware-compat.patch
+  ];
+
+  build-system = [ setuptools ];
+
+  dependencies = [ django ];
+
+  nativeCheckInputs = [
+    pytest-django
+    pytestCheckHook
+  ];
+
+  preCheck = ''
+    export DJANGO_SETTINGS_MODULE=test_settings
+  '';
+
+  meta = {
+    description = "Feature flipper for Django";
+    homepage = "https://waffle.readthedocs.io/en/stable/";
+    maintainers = [ ];
+    license = lib.licenses.bsd3;
+  };
+})
