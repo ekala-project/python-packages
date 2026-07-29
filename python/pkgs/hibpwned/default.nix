1c5eaaeee0ba081e519ea667c045e59cb4735903
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:17 -0700

    python3Packages.hibpwned: init at 1.3.9

---
+++ python/pkgs/hibpwned/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  requests,
+  setuptools,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "hibpwned";
+  version = "1.3.9";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "plasticuproject";
+    repo = "hibpwned";
+    tag = finalAttrs.version;
+    hash = "sha256-d3EhRu7HcvbyjWWHVSax0j39yE4+hJp8zvtyRKoh4sY=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ requests ];
+
+  # Test require network access
+  doCheck = false;
+
+  pythonImportsCheck = [ "hibpwned" ];
+
+  meta = {
+    description = "Python API wrapper for haveibeenpwned.com";
+    homepage = "https://github.com/plasticuproject/hibpwned";
+    license = lib.licenses.agpl3Only;
+    maintainers = [ ];
+  };
+})
