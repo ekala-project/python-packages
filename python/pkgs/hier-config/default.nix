d7742298dfdf343874a7f88c6f1440a41d92cb3c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:41:17 -0700

    python3Packages.hier-config: init at 3.6.2

---
+++ python/pkgs/hier-config/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  poetry-core,
+  pydantic,
+  pytestCheckHook,
+  pyyaml,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "hier-config";
+  version = "3.6.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "netdevops";
+    repo = "hier_config";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-hE9W5f+RUDBu0AaqP++QjrTJo6biKIxUMSE18C++AVI=";
+  };
+
+  build-system = [ poetry-core ];
+
+  dependencies = [ pydantic ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    pyyaml
+  ];
+
+  pythonImportsCheck = [ "hier_config" ];
+
+  meta = {
+    description = "Module to handle hierarchical configurations";
+    homepage = "https://github.com/netdevops/hier_config";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
