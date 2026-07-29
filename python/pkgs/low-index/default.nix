8d28203a8ca3142339d310f76048afc4915fab3a
Jonathan Ringer <jonringer117@gmail.com>
2026-07-29 12:42:32 -0700

    python3Packages.low-index: init at 1.3

---
+++ python/pkgs/low-index/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  python,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  nix-update-script,
+}:
+
+buildPythonPackage rec {
+  pname = "low-index";
+  version = "1.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "3-manifolds";
+    repo = "low_index";
+    tag = "v${version}_as_released";
+    hash = "sha256-m3p05bqu70pMOsb9drW1B6+N893eBSZBFTNNS23OY6w=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "low_index" ];
+
+  checkPhase = ''
+    runHook preCheck
+    ${python.interpreter} -m low_index.test
+    runHook postCheck
+  '';
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--version-regex"
+      "v(.*)_as_released"
+    ];
+  };
+
+  meta = {
+    description = "Enumerates low index subgroups of a finitely presented group";
+    homepage = "https://github.com/3-manifolds/low_index";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+  };
+}
