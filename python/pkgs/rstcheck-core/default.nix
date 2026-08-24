4266a8aa861b0b4ea7d048205d1df5e952d8cb5d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:26 -0700

    python3Packages.rstcheck-core: init at 1.3.1

---
+++ python/pkgs/rstcheck-core/default.nix
@@ -0,0 +1,44 @@
+{ lib
+, stdenv
+, buildPythonPackage
+, docutils
+, fetchFromGitHub
+, pydantic
+, setuptools
+, setuptools-scm
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "rstcheck-core";
+  version = "1.3.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "rstcheck";
+    repo = "rstcheck-core";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-4NvaBG2LQqHJgDLcUpjVjknkc0unVyAM8d5Z8SlZ2jw=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isClang "-Wno-strict-prototypes";
+
+  dependencies = [
+    docutils
+    pydantic
+  ];
+
+  pythonImportsCheck = [ "rstcheck_core" ];
+
+  meta = {
+    description = "Library for checking syntax of reStructuredText";
+    homepage = "https://github.com/rstcheck/rstcheck-core";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
