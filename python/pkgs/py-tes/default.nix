9d1a2f5943d89bfacb30ddad0e7871f0439be2c8
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:21 -0700

    python3Packages.py-tes: init at 1.1.4

---
+++ python/pkgs/py-tes/default.nix
@@ -0,0 +1,41 @@
+{ lib
+, attrs
+, buildPythonPackage
+, fetchFromGitHub
+, python-dateutil
+, requests
+, setuptools
+, sphinx-rtd-theme
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "py-tes";
+  version = "1.1.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ohsu-comp-bio";
+    repo = "py-tes";
+    tag = finalAttrs.version;
+    hash = "sha256-/xgycSDFp17rPzC6ICf4e+vrIKWYPftDngx/u1/KHWk=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    attrs
+    python-dateutil
+    requests
+    sphinx-rtd-theme
+  ];
+
+  pythonImportsCheck = [ "tes" ];
+
+  meta = {
+    description = "Python SDK for the GA4GH Task Execution API";
+    homepage = "https://github.com/ohsu-comp-bio/py-tes";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
