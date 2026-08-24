a926294ebed11f2162e5b6330ee1566a9980e9fd
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:31 -0700

    python3Packages.tmdbsimple: init at 2.9.8

---
+++ python/pkgs/tmdbsimple/default.nix
@@ -0,0 +1,35 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, requests
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "tmdbsimple";
+  version = "2.9.8";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "celiao";
+    repo = "tmdbsimple";
+    tag = finalAttrs.version;
+    hash = "sha256-bKSvknFnqF/QXdJZfQu6RCufXp6u4zvr8IAo8WToWf8=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ requests ];
+
+  pythonImportsCheck = [ "tmdbsimple" ];
+
+  # The tests require an internet connection and an API key
+
+  meta = {
+    description = "Wrapper for The Movie Database API v3";
+    homepage = "https://github.com/celiao/tmdbsimple";
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ ];
+  };
+})
