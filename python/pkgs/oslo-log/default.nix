4dc9c051ac2aed70350af2e07982b88e8ed2cf4d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:14 -0700

    python3Packages.oslo-log: init at 8.1.0

---
+++ python/pkgs/oslo-log/default.nix
@@ -0,0 +1,53 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  setuptools
+, # dependencies
+  debtcollector
+, oslo-config
+, oslo-context
+, oslo-serialization
+, oslo-utils
+, pbr
+, python-dateutil
+,
+}:
+
+buildPythonPackage rec {
+  pname = "oslo-log";
+  version = "8.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "openstack";
+    repo = "oslo.log";
+    tag = version;
+    hash = "sha256-ThRJ2rfVStnVOwcu8ZaKDjqb4jT6YE+n+iOFtmR8rwQ=";
+  };
+
+  # Manually set version because prb wants to get it from the git upstream repository (and we are
+  # installing from tarball instead)
+  env.PBR_VERSION = version;
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    debtcollector
+    oslo-config
+    oslo-context
+    oslo-serialization
+    oslo-utils
+    pbr
+    python-dateutil
+  ];
+  pythonImportsCheck = [ "oslo_log" ];
+
+  meta = {
+    description = "oslo.log library";
+    mainProgram = "convert-json";
+    homepage = "https://github.com/openstack/oslo.log";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
