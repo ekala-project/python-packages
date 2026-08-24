a9804a5da27ba87ca8ef1dad02f224bbc5cf0ff0
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:14 -0700

    python3Packages.osc-placement: init at 4.8.0

---
+++ python/pkgs/osc-placement/default.nix
@@ -0,0 +1,55 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, keystoneauth1
+, openstackdocstheme
+, osc-lib
+, oslo-utils
+, pbr
+, setuptools
+, sphinxHook
+,
+}:
+
+buildPythonPackage rec {
+  pname = "osc-placement";
+  version = "4.8.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "openstack";
+    repo = "osc-placement";
+    tag = version;
+    hash = "sha256-txxLtg3fDrkPqU0k/PlwvpJJBzVLtJXz82mhPWo+rKc=";
+  };
+
+  env.PBR_VERSION = version;
+
+  build-system = [
+    pbr
+    setuptools
+  ];
+
+  nativeBuildInputs = [
+    openstackdocstheme
+    sphinxHook
+  ];
+
+  sphinxBuilders = [ "man" ];
+
+  dependencies = [
+    keystoneauth1
+    osc-lib
+    oslo-utils
+    pbr
+  ];
+
+  pythonImportsCheck = [ "osc_placement" ];
+
+  meta = {
+    homepage = "https://github.com/openstack/osc-placement";
+    description = "OpenStackClient plugin for the Placement service";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
