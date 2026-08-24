3bcf94d19963aa1fa426d0a9de982332e81188b7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:10 -0700

    python3Packages.eliot: init at 1.18.0

---
+++ python/pkgs/eliot/default.nix
@@ -0,0 +1,50 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  setuptools
+, setuptools-scm
+, versioneer
+, # dependencies
+  boltons
+, orjson
+, pyrsistent
+, zope-interface
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "eliot";
+  version = "1.18.0";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "itamarst";
+    repo = "eliot";
+    tag = finalAttrs.version;
+    hash = "sha256-YUvHdnpWtsy2NlrVLaaewcUPKGLfdfX/zvowV0jcXuw=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+    versioneer
+  ];
+
+  dependencies = [
+    boltons
+    orjson
+    pyrsistent
+    zope-interface
+  ];
+
+  pythonImportsCheck = [ "eliot" ];
+
+  meta = {
+    description = "Logging library that tells you why it happened";
+    homepage = "https://eliot.readthedocs.io";
+    mainProgram = "eliot-prettyprint";
+    license = lib.licenses.asl20;
+  };
+})
