f2f084215c90f93db77db32355c66ba4e7a836c5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:28 -0700

    python3Packages.sphinxcontrib-youtube: init at 1.5.0

---
+++ python/pkgs/sphinxcontrib-youtube/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, sphinx
+, requests
+, flit-core
+,
+}:
+
+buildPythonPackage rec {
+  pname = "sphinxcontrib-youtube";
+  version = "1.5.0";
+  pyproject = true;
+
+  nativeBuildInputs = [ flit-core ];
+
+  src = fetchFromGitHub {
+    owner = "sphinx-contrib";
+    repo = "youtube";
+    tag = "v${version}";
+    hash = "sha256-vzF1SC4fUIeR0OYesOq60eWjlX+N+YYA/h7mNfxWEtk=";
+  };
+
+  propagatedBuildInputs = [
+    sphinx
+    requests
+  ];
+
+  pythonImportsCheck = [ "sphinxcontrib.youtube" ];
+
+  pythonNamespaces = [ "sphinxcontrib" ];
+
+  meta = {
+    description = "Youtube extension for Sphinx";
+    homepage = "https://github.com/sphinx-contrib/youtube";
+    maintainers = [ ];
+    license = lib.licenses.bsd3;
+  };
+}
