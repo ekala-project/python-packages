648aa46116b6b2fbd6c03f785eac175cbda1100a
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:18 -0700

    python3Packages.pygdbmi: init at 0.11.0.0

---
+++ python/pkgs/pygdbmi/default.nix
@@ -0,0 +1,24 @@
+{ stdenv
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pygdbmi";
+  version = "0.11.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "cs01";
+    repo = "pygdbmi";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-JqEDN8Pg/JttyYQbwkxKkLYuxVnvV45VlClD23eaYyc=";
+  };
+
+  build-system = [ setuptools ];
+
+  doCheck = !stdenv.hostPlatform.isDarwin;
+
+})
