28e00cc123a285061869b3c00e92431eb4e61f5d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:19 -0700

    python3Packages.pylxd: init at 2.4.0

---
+++ python/pkgs/pylxd/default.nix
@@ -0,0 +1,49 @@
+{ lib
+, buildPythonPackage
+, cryptography
+, fetchFromGitHub
+, python-dateutil
+, requests
+, urllib3
+, requests-toolbelt
+, requests-unixsocket
+, setuptools
+, ws4py
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pylxd";
+  version = "2.4.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "canonical";
+    repo = "pylxd";
+    tag = version;
+    hash = "sha256-s3BdHZFNkXRT1MoLQCQ4+XPPFanZNZVgOSmYhJkx7JE=";
+  };
+
+  pythonRelaxDeps = [ "urllib3" ];
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    cryptography
+    python-dateutil
+    requests
+    requests-toolbelt
+    requests-unixsocket
+    urllib3
+    ws4py
+  ];
+
+  pythonImportsCheck = [ "pylxd" ];
+
+  meta = {
+    description = "Library for interacting with the LXD REST API";
+    homepage = "https://pylxd.readthedocs.io/";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
