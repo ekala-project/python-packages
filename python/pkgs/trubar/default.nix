fad7649e12349682f5afda508458fec2579e8fbf
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:31 -0700

    python3Packages.trubar: init at 0.3.4

---
+++ python/pkgs/trubar/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, gitUpdater
+, libcst
+, pyyaml
+,
+}:
+
+buildPythonPackage rec {
+  pname = "trubar";
+  version = "0.3.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "janezd";
+    repo = "trubar";
+    tag = version;
+    hash = "sha256-ChKmeACEMnFcMYSdkdVlFiE3td171ihUS2A+qsP5ASk=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    libcst
+    pyyaml
+  ];
+
+  pythonImportsCheck = [ "trubar" ];
+
+  passthru.updateScript = gitUpdater { };
+
+  meta = {
+    description = "Utility for translation of strings and f-strings in Python files";
+    homepage = "https://github.com/janezd/trubar";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
