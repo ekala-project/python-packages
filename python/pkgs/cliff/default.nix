cefe65a0af01716c26c4fba4b922d0c246f23489
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:08 -0700

    python3Packages.cliff: init at 4.15.0

---
+++ python/pkgs/cliff/default.nix
@@ -0,0 +1,49 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, autopage
+, cmd2
+, openstackdocstheme
+, pbr
+, prettytable
+, pyyaml
+, stevedore
+, sphinxHook
+,
+}:
+
+buildPythonPackage rec {
+  pname = "cliff";
+  version = "4.15.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-7KaZ9rOQx1Xm7RgWp2zfwav19e99HO4Z84M2Z6Oh/jk=";
+  };
+
+  build-system = [
+    openstackdocstheme
+    pbr
+    sphinxHook
+  ];
+
+  sphinxBuilders = [ "man" ];
+
+  dependencies = [
+    autopage
+    cmd2
+    prettytable
+    pyyaml
+    stevedore
+  ];
+
+  pythonImportsCheck = [ "cliff" ];
+
+  meta = {
+    description = "Command Line Interface Formulation Framework";
+    homepage = "https://github.com/openstack/cliff";
+    license = lib.licenses.asl20;
+    teams = [ lib.teams.openstack ];
+  };
+}
