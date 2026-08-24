c7457d9e4e3692d42240a9ce7fbf381c654ae7f9
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:32 -0700

    python3Packages.unique-log-filter: init at 0.1.0

---
+++ python/pkgs/unique-log-filter/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, flit-core
+,
+}:
+
+buildPythonPackage rec {
+  pname = "unique-log-filter";
+  version = "0.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "twizmwazin";
+    repo = "unique_log_filter";
+    tag = "v${version}";
+    hash = "sha256-av1pVPDsO2dto5fhBK74jKfVsVY2ChyUE5NNja2B1Qw=";
+  };
+
+  build-system = [ flit-core ];
+
+  pythonImportsCheck = [ "unique_log_filter" ];
+
+  meta = {
+    description = "Log filter that removes duplicate log messages";
+    homepage = "https://github.com/twizmwazin/unique_log_filter";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
