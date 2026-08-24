56cc58b9c772917258df0547caaea75e1c9559f1
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:09 -0700

    python3Packages.django-cacheops: init at 7.2

---
+++ python/pkgs/django-cacheops/default.nix
@@ -0,0 +1,42 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, django
+, funcy
+, redis
+, six
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "django-cacheops";
+  version = "7.2";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "django_cacheops";
+    inherit version;
+    hash = "sha256-y8EcwDISlaNkTie8smlA8Iy5wucdPuUGy8/wvdoanzM=";
+  };
+
+  pythonRelaxDeps = [ "funcy" ];
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    django
+    funcy
+    redis
+    six
+  ];
+
+  env.DJANGO_SETTINGS_MODULE = "tests.settings";
+
+  meta = {
+    description = "Slick ORM cache with automatic granular event-driven invalidation for Django";
+    homepage = "https://github.com/Suor/django-cacheops";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
