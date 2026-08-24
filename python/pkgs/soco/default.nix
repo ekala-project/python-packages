b9da89c2c03fe800c0874c7c9b52222d4fa08b15
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:27 -0700

    python3Packages.soco: init at 0.31.1

---
+++ python/pkgs/soco/default.nix
@@ -0,0 +1,43 @@
+{ lib
+, appdirs
+, buildPythonPackage
+, fetchFromGitHub
+, ifaddr
+, lxml
+, requests
+, setuptools
+, xmltodict
+,
+}:
+
+buildPythonPackage rec {
+  pname = "soco";
+  version = "0.31.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "SoCo";
+    repo = "SoCo";
+    tag = "v${version}";
+    hash = "sha256-YxIpTFGB82aBvRxfntou2dUZm7T0KSj7yhX3gcdAO7U=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    appdirs
+    ifaddr
+    lxml
+    requests
+    xmltodict
+  ];
+
+  pythonImportsCheck = [ "soco" ];
+
+  meta = {
+    description = "CLI and library to control Sonos speakers";
+    homepage = "http://python-soco.com/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
