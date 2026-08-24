1a73e8068625f6f476eacf7464f01b93e7df6902
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:31 -0700

    python3Packages.timeslot: init at 0.1.2

---
+++ python/pkgs/timeslot/default.nix
@@ -0,0 +1,32 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, poetry-core
+,
+}:
+
+buildPythonPackage {
+  pname = "timeslot";
+  version = "0.1.2";
+
+  # pypi distribution doesn't include tests, so build from source instead
+  src = fetchFromGitHub {
+    owner = "ErikBjare";
+    repo = "timeslot";
+    rev = "af35445e96cbb2f3fb671a75aac6aa93e4e7e7a6";
+    sha256 = "sha256-GEhg2iMsYMfalT7L9TCd1KHU6oa/wTl5m3mRC0zOH9Q=";
+  };
+
+  pyproject = true;
+
+  nativeBuildInputs = [ poetry-core ];
+
+  pythonImportsCheck = [ "timeslot" ];
+
+  meta = {
+    description = "Data type for representing time slots with a start and end";
+    homepage = "https://github.com/ErikBjare/timeslot";
+    maintainers = [ ];
+    license = lib.licenses.mit;
+  };
+}
