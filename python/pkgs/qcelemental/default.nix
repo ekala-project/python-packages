8fe800319005fbe912b551f70ea65e629e35e0f8
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:24 -0700

    python3Packages.qcelemental: init at 0.50.4

---
+++ python/pkgs/qcelemental/default.nix
@@ -0,0 +1,62 @@
+{ stdenv
+, buildPythonPackage
+, lib
+, fetchPypi
+, poetry-core
+, setuptools
+, setuptools-scm
+, ipykernel
+, networkx
+, numpy
+, packaging
+, pint
+, pydantic
+, scipy
+,
+}:
+
+buildPythonPackage rec {
+  pname = "qcelemental";
+  version = "0.50.4";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-jVOCbTP/FXyqL1yJbBkxHPPJ2vcZyrjG+GBg+V1fdEs=";
+  };
+
+  build-system = [
+    poetry-core
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    numpy
+    packaging
+    pint
+    pydantic
+  ];
+
+  optional-dependencies = {
+    viz = [
+      # TODO: nglview
+      ipykernel
+    ];
+    align = [
+      networkx
+      scipy
+    ];
+  };
+
+  pythonImportsCheck = [ "qcelemental" ];
+
+  # These tests require network access
+  meta = {
+    broken = stdenv.hostPlatform.isDarwin;
+    description = "Periodic table, physical constants and molecule parsing for quantum chemistry";
+    homepage = "https://github.com/MolSSI/QCElemental";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
