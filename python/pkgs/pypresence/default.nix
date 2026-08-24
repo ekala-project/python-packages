beee3429cf8e1d95e1b457363d62e7d9adb8140e
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:20 -0700

    python3Packages.pypresence: init at 4.6.1

---
+++ python/pkgs/pypresence/default.nix
@@ -0,0 +1,32 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+,
+}:
+let
+  version = "4.6.1";
+in
+buildPythonPackage {
+  pname = "pypresence";
+  inherit version;
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "qwertyquerty";
+    repo = "pypresence";
+    tag = "v${version}";
+    hash = "sha256-VvVHJ3S+Yusq4cK4KyDQlnL3VwAyrZqNKYzEgJPU8Vk=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "pypresence" ];
+
+  meta = {
+    homepage = "https://qwertyquerty.github.io/pypresence/html/index.html";
+    description = "Discord RPC client written in Python";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
