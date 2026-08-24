b4037cc7eb3e4c3a3d6a112d68c84132cb933d6c
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:32 -0700

    python3Packages.ukkonen: init at 1.1.0

---
+++ python/pkgs/ukkonen/default.nix
@@ -0,0 +1,36 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, cffi
+, setuptools
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "ukkonen";
+  version = "1.1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "asottile";
+    repo = "ukkonen";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-vXyOLAiY92Df7g57quiSnOz8yhaIsm8MTB6Fbiv6axQ=";
+  };
+
+  build-system = [
+    cffi
+    setuptools
+  ];
+
+  dependencies = [ cffi ];
+
+  pythonImportsCheck = [ "ukkonen" ];
+
+  meta = {
+    description = "Python implementation of bounded Levenshtein distance (Ukkonen)";
+    homepage = "https://github.com/asottile/ukkonen";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
