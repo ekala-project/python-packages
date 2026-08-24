f8a3f28f905c601d7a7fff18925dfaf286432627
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:21 -0700

    python3Packages.pysubs2: init at 1.8.0

---
+++ python/pkgs/pysubs2/default.nix
@@ -0,0 +1,31 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pysubs2";
+  version = "1.8.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "tkarabela";
+    repo = "pysubs2";
+    rev = version;
+    hash = "sha256-fKSb7MfBHGft8Tp6excjfkVXKnHRER11X0QxbR1zD4I=";
+  };
+
+  nativeBuildInputs = [ setuptools ];
+
+  pythonImportsCheck = [ "pysubs2" ];
+
+  meta = {
+    homepage = "https://github.com/tkarabela/pysubs2";
+    description = "Python library for editing subtitle files";
+    mainProgram = "pysubs2";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
