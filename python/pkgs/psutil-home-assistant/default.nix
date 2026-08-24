10a6f9dbd533376ae47b34895d645cc5f2233973
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:16 -0700

    python3Packages.psutil-home-assistant: init at 0.0.1

---
+++ python/pkgs/psutil-home-assistant/default.nix
@@ -0,0 +1,28 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, psutil
+,
+}:
+
+buildPythonPackage rec {
+  pname = "psutil-home-assistant";
+  version = "0.0.1";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "home-assistant-libs";
+    repo = "psutil-home-assistant";
+    tag = version;
+    hash = "sha256-6bj1aaa/JYZFVwUAJfxISRoldgTmumCG8WrlKhkb6kM=";
+  };
+
+  propagatedBuildInputs = [ psutil ];
+
+  meta = {
+    description = "Wrapper of psutil that removes reliance on globals";
+    homepage = "https://github.com/home-assistant-libs/psutil-home-assistant";
+    license = lib.licenses.asl20;
+    teams = [ lib.teams.home-assistant ];
+  };
+}
