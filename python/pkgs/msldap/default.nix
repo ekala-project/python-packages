2ea7f38fb7673bf641e76aa4c416f17477414ea7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:13 -0700

    python3Packages.msldap: init at 0.5.15

---
+++ python/pkgs/msldap/default.nix
@@ -0,0 +1,51 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, unicrypto
+, asyauth
+, asysocks
+, asn1crypto
+, winacl
+, prompt-toolkit
+, tqdm
+, wcwidth
+, tabulate
+,
+}:
+
+buildPythonPackage rec {
+  pname = "msldap";
+  version = "0.5.15";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-uAJKLAVZFY7EB8tjFyAezINicki6ruzuXf1EGcp3Pj0=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    unicrypto
+    asyauth
+    asysocks
+    asn1crypto
+    winacl
+    prompt-toolkit
+    tqdm
+    wcwidth
+    tabulate
+  ];
+
+  # Project doesn't have tests
+
+  pythonImportsCheck = [ "msldap" ];
+
+  meta = {
+    description = "Python LDAP library for auditing MS AD";
+    homepage = "https://github.com/skelsec/msldap";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
