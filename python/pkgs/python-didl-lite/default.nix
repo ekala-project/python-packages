843bf8da632bbc9bc557384684b902c2d3300db4
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:23 -0700

    python3Packages.python-didl-lite: init at 1.5.0

---
+++ python/pkgs/python-didl-lite/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, defusedxml
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "python-didl-lite";
+  version = "1.5.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "StevenLooman";
+    repo = "python-didl-lite";
+    tag = version;
+    hash = "sha256-pdXdGRycMB6M6qnPl+Z+ezRw6td45IqYkEpx4YtL1rQ=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ defusedxml ];
+
+  pythonImportsCheck = [ "didl_lite" ];
+
+  meta = {
+    description = "DIDL-Lite (Digital Item Declaration Language) tools for Python";
+    homepage = "https://github.com/StevenLooman/python-didl-lite";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
