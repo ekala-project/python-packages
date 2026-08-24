845d9b78456371fd26374e69b405f45ff41337a0
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:17 -0700

    python3Packages.pycrdt-websocket: init at 0.16.4

---
+++ python/pkgs/pycrdt-websocket/default.nix
@@ -0,0 +1,50 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  hatchling
+, # dependencies
+  anyio
+, pycrdt
+, pycrdt-store
+, sqlite-anyio
+, # optional-dependencies
+  channels
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pycrdt-websocket";
+  version = "0.16.4";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "y-crdt";
+    repo = "pycrdt-websocket";
+    tag = finalAttrs.version;
+    hash = "sha256-H9QxMxNCIvykGpdxNAtAbVpaJlpnq9O76nTh1raVfJU=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    anyio
+    pycrdt
+    pycrdt-store
+    sqlite-anyio
+  ];
+
+  optional-dependencies = {
+    django = [ channels ];
+  };
+
+  pythonImportsCheck = [ "pycrdt.websocket" ];
+
+  meta = {
+    description = "WebSocket Connector for pycrdt";
+    homepage = "https://github.com/jupyter-server/pycrdt-websocket";
+    license = lib.licenses.mit;
+    teams = [ lib.teams.jupyter ];
+  };
+})
