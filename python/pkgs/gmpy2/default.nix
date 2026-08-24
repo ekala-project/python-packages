2dc647484481e0703498688623187215dd71b03e
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:11 -0700

    python3Packages.gmpy2: init at 2.2.2

---
+++ python/pkgs/gmpy2/default.nix
@@ -0,0 +1,43 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, isPyPy
+, setuptools
+, gmp
+, mpfr
+, libmpc
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "gmpy2";
+  version = "2.2.2";
+  pyproject = true;
+
+  disabled = isPyPy;
+
+  src = fetchFromGitHub {
+    owner = "aleaxit";
+    repo = "gmpy";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-joeHec/d82sovfASCU3nlNL6SaThnS/XYPqujiZ9h8s=";
+  };
+
+  build-system = [ setuptools ];
+
+  buildInputs = [
+    gmp
+    mpfr
+    libmpc
+  ];
+
+  # make relative imports in tests work properly
+  pythonImportsCheck = [ "gmpy2" ];
+
+  meta = {
+    description = "Interface to GMP, MPFR, and MPC for Python 3.7+";
+    homepage = "https://github.com/aleaxit/gmpy/";
+    license = lib.licenses.lgpl3Plus;
+    maintainers = [ ];
+  };
+})
