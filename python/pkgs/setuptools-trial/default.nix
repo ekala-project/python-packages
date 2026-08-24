412306f51c745c6574e9640fb234e53a49a55061
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:27 -0700

    python3Packages.setuptools-trial: init at 0.6.0

---
+++ python/pkgs/setuptools-trial/default.nix
@@ -0,0 +1,34 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, twisted
+,
+}:
+
+buildPythonPackage rec {
+  pname = "setuptools-trial";
+  version = "0.6.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "setuptools_trial";
+    inherit version;
+    hash = "sha256-FCIPj3YcSLoeJSbwhxlQd89U+tcJizgs4iBCLw/1mxI=";
+  };
+
+  nativeBuildInputs = [ setuptools ];
+
+  propagatedBuildInputs = [ twisted ];
+
+  # Couldn't get tests working
+
+  pythonImportsCheck = [ "setuptools_trial" ];
+
+  meta = {
+    description = "Setuptools plugin that makes unit tests execute with trial instead of pyunit";
+    homepage = "https://github.com/rutsky/setuptools-trial";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
