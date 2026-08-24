d3389f09d67a2b2534cf4b28596083bd46d4c033
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:31 -0700

    python3Packages.transforms3d: init at 0.4.2

---
+++ python/pkgs/transforms3d/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, versioneer
+, numpy
+,
+}:
+
+buildPythonPackage rec {
+  pname = "transforms3d";
+  version = "0.4.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "matthew-brett";
+    repo = "transforms3d";
+    tag = version;
+    hash = "sha256-9wICu7zNYF54e6xcDpZxqctB4GVu5Knf79Z36016Rpw=";
+  };
+
+  build-system = [
+    setuptools
+    versioneer
+  ];
+
+  dependencies = [
+    numpy
+  ];
+
+  pythonImportsCheck = [ "transforms3d" ];
+
+  meta = {
+    homepage = "https://matthew-brett.github.io/transforms3d";
+    description = "Convert between various geometric transformations";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
