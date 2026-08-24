b05522c54089ec48741fc35500770483fde3e439
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:11 -0700

    python3Packages.hg-evolve: init at 11.1.10

---
+++ python/pkgs/hg-evolve/default.nix
@@ -0,0 +1,28 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, mercurial
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "hg-evolve";
+  version = "11.1.10";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "hg_evolve";
+    inherit version;
+    hash = "sha256-ccFq7sASkOkFJ4Or5dhZpfKR0FdZAmbziDfK3FGcaYM=";
+  };
+
+  build-system = [ setuptools ];
+
+  meta = {
+    description = "Enables the “changeset evolution” feature of Mercurial core";
+    homepage = "https://www.mercurial-scm.org/doc/evolution/";
+    maintainers = [ ];
+    license = lib.licenses.gpl2Plus;
+  };
+}
