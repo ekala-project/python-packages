b30bd14bdd0da4d2d07995e42ca12f46dd9fff22
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:27 -0700

    python3Packages.sip: init at 6.15.1

---
+++ python/pkgs/sip/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, setuptools-scm
+, packaging
+,
+}:
+
+buildPythonPackage rec {
+  pname = "sip";
+  version = "6.15.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-3C5YwXmKdOGzHCjoNzOYIv6PpVKIrjDomG6ygQDrylo=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    packaging
+    setuptools
+  ];
+
+  # There aren't tests
+
+  pythonImportsCheck = [ "sipbuild" ];
+
+  meta = {
+    description = "Creates C++ bindings for Python modules";
+    homepage = "https://riverbankcomputing.com/";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+  };
+}
