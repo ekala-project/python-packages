5b721807d8e3ffa29e7aa4d98f63257a294cf988
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:20 -0700

    python3Packages.py-radix: init at 1.1.0

---
+++ python/pkgs/py-radix/default.nix
@@ -0,0 +1,27 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "py-radix";
+  version = "1.1.0";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "mjschultz";
+    repo = "py-radix";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-++QuZEwOHKjOsIbwLmDy30PvyG0Xe29l45PvOF+YWYw=";
+  };
+
+  pythonImportsCheck = [ "radix" ];
+
+  meta = {
+    description = "Python radix tree implementation for IPv4 and IPv6 prefix matching";
+    homepage = "https://github.com/mjschultz/py-radix";
+    license = lib.licenses.bsdOriginal;
+    maintainers = [ ];
+  };
+})
