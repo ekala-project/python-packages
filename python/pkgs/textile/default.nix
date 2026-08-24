8fc3d43e4c57d98f0519fcc361d8f33da2fefa8a
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:30 -0700

    python3Packages.textile: init at 4.0.4

---
+++ python/pkgs/textile/default.nix
@@ -0,0 +1,47 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, nh3
+, pillow
+, regex
+, setuptools-scm
+, setuptools
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "textile";
+  version = "4.0.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "textile";
+    repo = "python-textile";
+    tag = finalAttrs.version;
+    hash = "sha256-fHji+TOIFVljkvlOaRp/8EnZ6KYgMu/DLpg6PmOSEbk=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    nh3
+    regex
+  ];
+
+  optional-dependencies = {
+    imagesize = [ pillow ];
+  };
+
+  pythonImportsCheck = [ "textile" ];
+
+  meta = {
+    description = "Module for generating web text";
+    homepage = "https://github.com/textile/python-textile";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+    mainProgram = "pytextile";
+  };
+})
