3c705dc75ca872226fc22e3430558266732bf67c
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:16 -0700

    python3Packages.ppk2-api: init at 0.9.2

---
+++ python/pkgs/ppk2-api/default.nix
@@ -0,0 +1,34 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, pyserial
+,
+}:
+
+buildPythonPackage rec {
+  pname = "ppk2-api";
+  version = "0.9.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "IRNAS";
+    repo = "ppk2-api-python";
+    tag = "v${version}";
+    hash = "sha256-fubDFtOXiv2YFYUCOUbuyXs1sHgs0/6ZVK9sAwxQ+Pk=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ pyserial ];
+
+  # Module has no tests
+  pythonImportsCheck = [ "ppk2_api" ];
+
+  meta = {
+    description = "Power Profiling Kit 2 unofficial Python API";
+    homepage = "https://github.com/IRNAS/ppk2-api-python";
+    license = lib.licenses.gpl2Only;
+    maintainers = [ ];
+  };
+}
