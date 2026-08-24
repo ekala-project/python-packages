6d5ca2a4c40cab47747e242d2b9d90f284e96672
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:25 -0700

    python3Packages.robotframework-seleniumlibrary: init at 6.9.0

---
+++ python/pkgs/robotframework-seleniumlibrary/default.nix
@@ -0,0 +1,33 @@
+{ buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, click
+, robotframework
+, robotframework-pythonlibcore
+, selenium
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "robotframework-seleniumlibrary";
+  version = "6.9.0";
+  pyproject = true;
+
+  # no tests included in PyPI tarball
+  src = fetchFromGitHub {
+    owner = "robotframework";
+    repo = "SeleniumLibrary";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-NiB1dJWivyDc1ucldQ2cs3jTWt3hHY6AGsboKPmY+mo=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    click
+    robotframework
+    robotframework-pythonlibcore
+    selenium
+  ];
+
+})
