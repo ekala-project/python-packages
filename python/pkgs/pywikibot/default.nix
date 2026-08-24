d3e847462788d4d4a65d18cef1dbc8a61402cdb0
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:24 -0700

    python3Packages.pywikibot: init at 10.7.4

---
+++ python/pkgs/pywikibot/default.nix
@@ -0,0 +1,38 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, mwparserfromhell
+, requests
+, packaging
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pywikibot";
+  version = "10.7.4";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-/hHfZRLoEgaPKZLus9x/d5O62GnwU/1A7PAsebGj634=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    mwparserfromhell
+    requests
+    packaging
+  ];
+
+  pythonImportsCheck = [ "pywikibot" ];
+
+  meta = {
+    description = "Python MediaWiki bot framework";
+    mainProgram = "pwb";
+    homepage = "https://www.mediawiki.org/wiki/Manual:Pywikibot";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
