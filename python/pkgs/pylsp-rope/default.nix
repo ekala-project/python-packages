a7a73bb57ed95141929428368657f59b0b9dbc00
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:19 -0700

    python3Packages.pylsp-rope: init at 0.1.17

---
+++ python/pkgs/pylsp-rope/default.nix
@@ -0,0 +1,41 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  setuptools
+, # dependencies
+  rope
+, python-lsp-server
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pylsp-rope";
+  version = "0.1.17";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "python-rope";
+    repo = "pylsp-rope";
+    tag = version;
+    hash = "sha256-gEmSZQZ2rtSljN8USsUiqsP2cr54k6kwvsz8cjam9dU=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
+    rope
+    python-lsp-server
+  ];
+
+  pythonImportsCheck = [ "pylsp_rope" ];
+
+  meta = {
+    description = "Extended refactoring capabilities for Python LSP Server using Rope";
+    homepage = "https://github.com/python-rope/pylsp-rope";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
