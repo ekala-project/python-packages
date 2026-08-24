2ffa09fb9ea4244be95afdeea8bc04315007fc2e
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:10 -0700

    python3Packages.ffmpeg-progress-yield: init at 1.1.3

---
+++ python/pkgs/ffmpeg-progress-yield/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, uv-build
+, tqdm
+, ffmpeg
+, versionCheckHook
+,
+}:
+
+buildPythonPackage rec {
+  pname = "ffmpeg-progress-yield";
+  version = "1.1.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "slhck";
+    repo = "ffmpeg-progress-yield";
+    tag = "v${version}";
+    hash = "sha256-OEE23gzPYcjKjrar+aV2zZuZyhrvqkYPhnWC3GzefUI=";
+  };
+
+  build-system = [ uv-build ];
+
+  dependencies = [
+    tqdm
+  ];
+
+  pythonImportsCheck = [ "ffmpeg_progress_yield" ];
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  meta = {
+    description = "Run an ffmpeg command with progress";
+    mainProgram = "ffmpeg-progress-yield";
+    homepage = "https://github.com/slhck/ffmpeg-progress-yield";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
