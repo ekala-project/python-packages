326649a4c1048ad8823327fad2a08f4e4b8cf6ec
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:15 -0700

    python3Packages.pegen: init at 0.3.0

---
+++ python/pkgs/pegen/default.nix
@@ -0,0 +1,34 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, setuptools-scm
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pegen";
+  version = "0.3.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "we-like-parsers";
+    repo = "pegen";
+    tag = "v${version}";
+    hash = "sha256-P4zX8za9lBlXhNPkQe9p136ggZEJh6fHfBr+DQKvtTg=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  pythonImportsCheck = [ "pegen" ];
+
+  meta = {
+    description = "Library to generate PEG parsers";
+    homepage = "https://github.com/we-like-parsers/pegen";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
