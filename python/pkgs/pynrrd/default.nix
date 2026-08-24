e2ef124120688934f9fb7375b698d3a71bc75304
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:20 -0700

    python3Packages.pynrrd: init at 1.1.3

---
+++ python/pkgs/pynrrd/default.nix
@@ -0,0 +1,37 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, numpy
+, typing-extensions
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pynrrd";
+  version = "1.1.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "mhe";
+    repo = "pynrrd";
+    tag = "v${version}";
+    hash = "sha256-qu3s3XswJCUchqYfYMuqIzI4sfeXrttvXSEW9/GSENA=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    numpy
+    typing-extensions
+  ];
+
+  pythonImportsCheck = [ "nrrd" ];
+
+  meta = {
+    homepage = "https://github.com/mhe/pynrrd";
+    description = "Simple pure-Python reader for NRRD files";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
