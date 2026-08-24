ed2754ec2d5d1a4dc1a01e84116ee1aa5cb027d8
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:21 -0700

    python3Packages.pyro5: init at 5.17

---
+++ python/pkgs/pyro5/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, serpent
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pyro5";
+  version = "5.17";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "irmen";
+    repo = "Pyro5";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-WVvUh/XPk+/1iRARAVD3fgmlJu60DZps7kRaZxuiwBo=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ serpent ];
+
+  pythonImportsCheck = [ "Pyro5" ];
+
+  meta = {
+    description = "Distributed object middleware for Python (RPC)";
+    homepage = "https://github.com/irmen/Pyro5";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
