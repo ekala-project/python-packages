22df5ec9e7d811f501c4fce86ed2292704d36af5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:18 -0700

    python3Packages.pyformlang: init at 1.0.11

---
+++ python/pkgs/pyformlang/default.nix
@@ -0,0 +1,41 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, networkx
+, numpy
+, pydot
+, setuptools
+, wheel
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pyformlang";
+  version = "1.0.11";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-4pLsi5z6ZMJrWS+vm3z8csT0sOsNUz8EWkYGHnXFzpk=";
+  };
+
+  build-system = [
+    setuptools
+    wheel
+  ];
+
+  dependencies = [
+    networkx
+    numpy
+    pydot
+  ];
+
+  pythonImportsCheck = [ "pyformlang" ];
+
+  meta = {
+    description = "Framework for formal grammars";
+    homepage = "https://github.com/Aunsiels/pyformlang";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
