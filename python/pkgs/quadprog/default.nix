577e81f8128e92dd95f3cd25720148919ff8677e
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:24 -0700

    python3Packages.quadprog: init at 0.1.13

---
+++ python/pkgs/quadprog/default.nix
@@ -0,0 +1,35 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, cython
+, setuptools
+, numpy
+,
+}:
+
+buildPythonPackage rec {
+  pname = "quadprog";
+  version = "0.1.13";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "quadprog";
+    repo = "quadprog";
+    rev = "v${version}";
+    hash = "sha256-8gPuqDY3ajw/+B6kJdtpq+HL+Oq2Nsy/O7m+IWzxP38=";
+  };
+
+  nativeBuildInputs = [
+    cython
+    setuptools
+  ];
+
+  propagatedBuildInputs = [ numpy ];
+
+  meta = {
+    homepage = "https://github.com/quadprog/quadprog";
+    description = "Quadratic Programming Solver";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+  };
+}
