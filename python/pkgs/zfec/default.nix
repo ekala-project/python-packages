bfb9bd2fee316aab563045be3a0aadf36a769a05
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:34 -0700

    python3Packages.zfec: init at 1.6.0.0

---
+++ python/pkgs/zfec/default.nix
@@ -0,0 +1,29 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pyutil
+, setuptools
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "zfec";
+  version = "1.6.0.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-xaGGHCU7USaYwuczrk2D9eLW6myIG32+ETNLaU51WgA=";
+  };
+
+  nativeBuildInputs = [ setuptools ];
+
+  propagatedBuildInputs = [ pyutil ];
+
+  meta = {
+    homepage = "https://github.com/tahoe-lafs/zfec";
+    description = "Fast erasure codec which can be used with the command-line, C, Python, or Haskell";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+  };
+})
