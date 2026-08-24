0a9c5a4db5f72e6f78bf9dfdb2d211f89000648c
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:25 -0700

    python3Packages.requests-gssapi: init at 1.4.0

---
+++ python/pkgs/requests-gssapi/default.nix
@@ -0,0 +1,36 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, gssapi
+, requests
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "requests-gssapi";
+  version = "1.4.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit version;
+    pname = "requests_gssapi";
+    hash = "sha256-uifrMp9IQNllvI+l02DGJ8dDSe+mFWylAa2Jr8ahNPQ=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    gssapi
+    requests
+  ];
+
+  pythonImportsCheck = [ "requests_gssapi" ];
+
+  meta = {
+    description = "GSSAPI authentication handler for python-requests";
+    homepage = "https://github.com/pythongssapi/requests-gssapi";
+    license = lib.licenses.isc;
+    maintainers = [ ];
+  };
+}
