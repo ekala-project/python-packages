41e9a0d072ec2a089ff4ff48469bc8d70c8bbd25
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:15 -0700

    python3Packages.pick: init at 2.6.0

---
+++ python/pkgs/pick/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, poetry-core
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pick";
+  version = "2.6.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "wong2";
+    repo = "pick";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-/cvnDTRS3V9mk1T0zHAqdrDeRuOrnco9UF7luy687BM=";
+  };
+
+  build-system = [ poetry-core ];
+
+  pythonImportsCheck = [ "pick" ];
+
+  meta = {
+    description = "Module to create curses-based interactive selection list in the terminal";
+    homepage = "https://github.com/wong2/pick";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
