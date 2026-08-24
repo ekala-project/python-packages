ab1a5f1ba63702078af761051799462d3c60f0ac
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:27 -0700

    python3Packages.siphashc: init at 2.7

---
+++ python/pkgs/siphashc/default.nix
@@ -0,0 +1,29 @@
+{ lib
+, fetchFromGitHub
+, buildPythonPackage
+, setuptools
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "siphashc";
+  version = "2.7";
+  pyproject = true;
+  build-system = [ setuptools ];
+
+  src = fetchFromGitHub {
+    owner = "WeblateOrg";
+    repo = "siphashc";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Rj+0oIlGs2Xs2TTN0PIwcVUlTgd1AZITC8xBf/Hn31Q=";
+  };
+
+  pythonImportsCheck = [ "siphashc" ];
+
+  meta = {
+    description = "Python c-module for siphash";
+    homepage = "https://github.com/WeblateOrg/siphashc";
+    license = lib.licenses.isc;
+    maintainers = [ ];
+  };
+})
