20f637b337127bb909913c748e633f111f096961
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:26 -0700

    python3Packages.secure: init at 1.0.1

---
+++ python/pkgs/secure/default.nix
@@ -0,0 +1,37 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, maya
+, requests
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "secure";
+  version = "1.0.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "typeerror";
+    repo = "secure.py";
+    tag = "v${version}";
+    hash = "sha256-lyosOejztFEINGKO0wAYv3PWBL7vpmAq+eQunwP9h5I=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    maya
+    requests
+  ];
+
+  pythonImportsCheck = [ "secure" ];
+
+  meta = {
+    description = "Adds optional security headers and cookie attributes for Python web frameworks";
+    homepage = "https://github.com/TypeError/secure.py";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
