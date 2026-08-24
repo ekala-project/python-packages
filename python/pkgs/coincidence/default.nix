6f3524f1a25d3ad44e8a90f39f42d2decaaebe41
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:08 -0700

    python3Packages.coincidence: init at 0.6.6

---
+++ python/pkgs/coincidence/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, typing-extensions
+, whey
+,
+}:
+
+buildPythonPackage rec {
+  pname = "coincidence";
+  version = "0.6.6";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "python-coincidence";
+    repo = "coincidence";
+    tag = "v${version}";
+    hash = "sha256-ktSuUzAwMych6Y2eJWMUfG1a3mGypg8L20f/105RFXc=";
+  };
+
+  build-system = [ whey ];
+
+  dependencies = [ typing-extensions ];
+
+  pythonImportsCheck = [ "coincidence" ];
+
+  meta = {
+    description = "Helper functions for pytest";
+    homepage = "https://github.com/python-coincidence/coincidence";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
