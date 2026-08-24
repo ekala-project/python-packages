33a09afc9e1182b77c93d38892b965c53c9f75f9
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:35 -0700

    python3Packages.zodbpickle: init at 4.4

---
+++ python/pkgs/zodbpickle/default.nix
@@ -0,0 +1,31 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "zodbpickle";
+  version = "4.4";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-efM8xJoJsoqLO0A2nBQhboBXF364x+iY12r9azGUy3g=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "zodbpickle" ];
+
+  meta = {
+    description = "Fork of Python's pickle module to work with ZODB";
+    homepage = "https://github.com/zopefoundation/zodbpickle";
+    license = with lib.licenses; [
+      psfl
+      zpl21
+    ];
+    maintainers = [ ];
+  };
+}
