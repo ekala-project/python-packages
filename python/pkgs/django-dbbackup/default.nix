38ef980c40ae7c073e641d0722d57527a396410e
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:09 -0700

    python3Packages.django-dbbackup: init at 5.3.0

---
+++ python/pkgs/django-dbbackup/default.nix
@@ -0,0 +1,32 @@
+{ buildPythonPackage
+, django
+, fetchFromGitHub
+, lib
+, hatchling
+,
+}:
+buildPythonPackage (finalAttrs: {
+  pname = "django-dbbackup";
+  version = "5.3.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Archmonger";
+    repo = "django-dbbackup";
+    tag = finalAttrs.version;
+    hash = "sha256-vSBZmYMcrpJQEhVVqKgn35vaI5TvMBbdwGXZOFjXQbw=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    django
+  ];
+
+  meta = {
+    description = "Management commands to help backup and restore your project database and media files";
+    homepage = "https://github.com/Archmonger/django-dbbackup";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+})
