45559ac52383411a0b27634091b51b385ceae8ac
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:13 -0700

    python3Packages.mutatormath: init at 3.0.1

---
+++ python/pkgs/mutatormath/default.nix
@@ -0,0 +1,30 @@
+{ buildPythonPackage
+, fetchPypi
+, defcon
+, fontmath
+, setuptools
+, unicodedata2
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "mutatormath";
+  version = "3.0.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "MutatorMath";
+    inherit (finalAttrs) version;
+    sha256 = "0r1qq45np49x14zz1zwkaayqrn7m8dn2jlipjldg2ihnmpzw29w1";
+    extension = "zip";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    fontmath
+    unicodedata2
+    defcon
+  ];
+
+})
