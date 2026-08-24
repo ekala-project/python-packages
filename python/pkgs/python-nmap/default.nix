61d65ddf8dec5e95781c838be28ca1a023d8f3cb
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:23 -0700

    python3Packages.python-nmap: init at 0.7.1

---
+++ python/pkgs/python-nmap/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, nmap
+,
+}:
+
+buildPythonPackage rec {
+  pname = "python-nmap";
+  version = "0.7.1";
+  format = "setuptools";
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-91r2uR3Y47DDH4adsyFj9iraaGlF5bfCX4S8D3+tO2Q=";
+  };
+
+  propagatedBuildInputs = [ nmap ];
+
+  postPatch = ''
+    substituteInPlace setup.cfg \
+      --replace "universal=3" "universal=1"
+  '';
+
+  pythonImportsCheck = [ "nmap" ];
+
+  meta = {
+    description = "Python library which helps in using nmap";
+    homepage = "https://xael.org/pages/python-nmap-en.html";
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ ];
+  };
+}
