09ff845c21d91486907d53b7016d32cfcbad7d5a
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:21 -0700

    python3Packages.pysodium: init at 0.7.18

---
+++ python/pkgs/pysodium/default.nix
@@ -0,0 +1,43 @@
+{ lib
+, stdenv
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, libsodium
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pysodium";
+  version = "0.7.18";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "stef";
+    repo = "pysodium";
+    tag = "v${version}";
+    hash = "sha256-F2215AAI8UIvn6UbaJ/YxI4ZolCzlwY6nS5IafTs+i4=";
+  };
+
+  postPatch =
+    let
+      soext = stdenv.hostPlatform.extensions.sharedLibrary;
+    in
+    ''
+      substituteInPlace ./pysodium/__init__.py --replace-fail \
+        "ctypes.util.find_library('sodium') or ctypes.util.find_library('libsodium')" "'${libsodium}/lib/libsodium${soext}'"
+    '';
+
+  build-system = [ setuptools ];
+
+  buildInputs = [ libsodium ];
+
+  pythonImportsCheck = [ "pysodium" ];
+
+  meta = {
+    description = "Wrapper for libsodium providing high level crypto primitives";
+    homepage = "https://github.com/stef/pysodium";
+    maintainers = [ ];
+    license = lib.licenses.bsd2;
+  };
+}
