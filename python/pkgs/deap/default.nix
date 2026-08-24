a03e596be5bdcdf208e5f287c878c5d049f1c052
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:09 -0700

    python3Packages.deap: init at 1.4.4

---
+++ python/pkgs/deap/default.nix
@@ -0,0 +1,38 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, moocore
+, numpy
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "deap";
+  version = "1.4.4";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) version;
+    pname = "deap";
+    hash = "sha256-UNS9kk/KWhaj26i/2xFApV6cJM5QgWq09Wg9LzHC1zQ=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
+    moocore
+    numpy
+  ];
+
+  meta = {
+    description = "Novel evolutionary computation framework for rapid prototyping and testing of ideas";
+    homepage = "https://github.com/DEAP/deap";
+    license = lib.licenses.lgpl3Plus;
+    maintainers = [ ];
+  };
+})
