481d530d491f30eacef66e1a3ff04639b683a1c4
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:28 -0700

    python3Packages.spdx: init at 2.5

---
+++ python/pkgs/spdx/default.nix
@@ -0,0 +1,31 @@
+{ lib
+, fetchFromGitHub
+, buildPythonPackage
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "spdx";
+  version = "2.5";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "bbqsrc";
+    repo = "spdx-python";
+    tag = "v${version}";
+    hash = "sha256-lfTgAX4Wl01xrvLA12ZUqjah7ZiLafMAU+yNNdVkRk0=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "spdx" ];
+
+  # upstream has no tests
+  meta = {
+    description = "SPDX license list database";
+    homepage = "https://github.com/bbqsrc/spdx-python";
+    license = lib.licenses.cc0;
+    maintainers = [ ];
+  };
+}
