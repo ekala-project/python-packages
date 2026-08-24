628138f5164931126f7669bc061ca4fbabf22df3
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:33 -0700

    python3Packages.websockify: init at 0.13.0

---
+++ python/pkgs/websockify/default.nix
@@ -0,0 +1,46 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, jwcrypto
+, numpy
+, redis
+, requests
+, simplejson
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "websockify";
+  version = "0.13.0";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "novnc";
+    repo = "websockify";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-b57L4o071zEt/gX9ZVzEpcnp0RCeo3peZrby2mccJgQ=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    jwcrypto
+    numpy
+    redis
+    requests
+    simplejson
+  ];
+
+  pythonImportsCheck = [ "websockify" ];
+
+  meta = {
+    description = "WebSockets support for any application/server";
+    mainProgram = "websockify";
+    homepage = "https://github.com/novnc/websockify";
+    license = lib.licenses.lgpl3Only;
+    maintainers = [ ];
+  };
+})
