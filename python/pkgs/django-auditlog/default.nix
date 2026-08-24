df8e8a5bc79dbb449e8f533f4007f25cc9b14750
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:09 -0700

    python3Packages.django-auditlog: init at 3.4.1

---
+++ python/pkgs/django-auditlog/default.nix
@@ -0,0 +1,45 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, setuptools-scm
+, django
+, python-dateutil
+, python
+,
+}:
+
+buildPythonPackage rec {
+  pname = "django-auditlog";
+  version = "3.4.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jazzband";
+    repo = "django-auditlog";
+    tag = "v${version}";
+    hash = "sha256-/IOzMGRR8EP/AGP7fcqwP4GeSKXPwE6NF6AZmiF1+lA=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    django
+    python-dateutil
+  ];
+
+  postgresqlTestUserOptions = "LOGIN SUPERUSER";
+
+  pythonImportsCheck = [ "auditlog" ];
+
+  meta = {
+    description = "Django app that keeps a log of changes made to an object";
+    homepage = "https://github.com/jazzband/django-auditlog";
+    downloadPage = "https://github.com/jazzband/django-auditlog";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
