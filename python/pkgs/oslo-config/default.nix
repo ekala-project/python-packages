24e863c49e14b93c5ebd1f69cd36be13dffaf46c
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:14 -0700

    python3Packages.oslo-config: init at 10.6.0

---
+++ python/pkgs/oslo-config/default.nix
@@ -0,0 +1,52 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, netaddr
+, oslo-i18n
+, pbr
+, pyyaml
+, requests
+, rfc3986
+, setuptools
+, stevedore
+,
+}:
+
+buildPythonPackage rec {
+  pname = "oslo-config";
+  version = "10.6.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "oslo_config";
+    inherit version;
+    hash = "sha256-vj/52hO+jxciDG2HocatOp2GQdLUEyTgS1f57vVyTFE=";
+  };
+
+  postPatch = ''
+    # only a small portion of the listed packages are actually needed for running the tests
+    # so instead of removing them one by one remove everything
+    rm test-requirements.txt
+  '';
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    netaddr
+    oslo-i18n
+    pbr
+    pyyaml
+    requests
+    rfc3986
+    stevedore
+  ];
+
+  pythonImportsCheck = [ "oslo_config" ];
+
+  meta = {
+    description = "Oslo Configuration API";
+    homepage = "https://github.com/openstack/oslo.config";
+    license = lib.licenses.asl20;
+    teams = [ lib.teams.openstack ];
+  };
+}
