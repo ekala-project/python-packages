9ac42b7d555ce3340c85f0ff5f1dccf62c39c8b4
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:16 -0700

    python3Packages.pudb: init at 2025.1.5

---
+++ python/pkgs/pudb/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, hatchling
+, fetchPypi
+, jedi
+, packaging
+, pygments
+, urwid
+, urwid-readline
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pudb";
+  version = "2025.1.5";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-5t7bgfw8jNzWbPYuhjN8uRNXDrssmUyatSAS0Fnghq0=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    jedi
+    packaging
+    pygments
+    urwid
+    urwid-readline
+  ];
+
+  meta = {
+    description = "Full-screen, console-based Python debugger";
+    mainProgram = "pudb";
+    homepage = "https://github.com/inducer/pudb";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
