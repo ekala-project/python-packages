e3d9b523056c8482fe334b40b6ebdf2ee129e91a
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:20 -0700

    python3Packages.pypykatz: init at 0.6.13

---
+++ python/pkgs/pypykatz/default.nix
@@ -0,0 +1,47 @@
+{ lib
+, aesedb
+, aiosmb
+, aiowinreg
+, buildPythonPackage
+, fetchPypi
+, minidump
+, minikerberos
+, msldap
+, setuptools
+, winsspi
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pypykatz";
+  version = "0.6.13";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-+T1E/Dk4OcXa8vBhspuB/8V23TORsXXetZpylW25SJM=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    aesedb
+    aiosmb
+    aiowinreg
+    minikerberos
+    msldap
+    winsspi
+    minidump
+  ];
+
+  # Project doesn't have tests
+  pythonImportsCheck = [ "pypykatz" ];
+
+  meta = {
+    description = "Mimikatz implementation in Python";
+    homepage = "https://github.com/skelsec/pypykatz";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "pypykatz";
+  };
+}
