58aaeef7abb7762e98d84ecbdc77c70f26543df7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:19 -0700

    python3Packages.pykeepass: init at 4.1.1.post1

---
+++ python/pkgs/pykeepass/default.nix
@@ -0,0 +1,43 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, argon2-cffi
+, construct
+, lxml
+, pycryptodomex
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pykeepass";
+  version = "4.1.1.post1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "libkeepass";
+    repo = "pykeepass";
+    tag = "v${version}";
+    hash = "sha256-DeEz3zrUK3cXIvMK/32Zn3FPiNsenhpAb17Zgel826s=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    argon2-cffi
+    construct
+    lxml
+    pycryptodomex
+  ];
+
+  propagatedNativeBuildInputs = [ argon2-cffi ];
+
+  pythonImportsCheck = [ "pykeepass" ];
+
+  meta = {
+    homepage = "https://github.com/libkeepass/pykeepass";
+    description = "Python library to interact with keepass databases (supports KDBX3 and KDBX4)";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+  };
+}
