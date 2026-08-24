8dd581ad67d84970c02cac6106bf4a9ebae96ea5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:07 -0700

    python3Packages.anyqt: init at 0.2.1

---
+++ python/pkgs/anyqt/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "anyqt";
+  version = "0.2.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ales-erjavec";
+    repo = "anyqt";
+    tag = finalAttrs.version;
+    hash = "sha256-iDUgu+x9rnpxpHzO7Rf2rJFXsheivrK7HI3FUbomkTU=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "AnyQt" ];
+
+  meta = {
+    description = "PyQt/PySide compatibility layer";
+    homepage = "https://github.com/ales-erjavec/anyqt";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+  };
+})
