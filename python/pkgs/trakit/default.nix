d8fd7e40d832484d4fbef83aa8f49004589f2d6d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:31 -0700

    python3Packages.trakit: init at 0.2.5

---
+++ python/pkgs/trakit/default.nix
@@ -0,0 +1,41 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build dependencies
+  poetry-core
+, # dependencies
+  babelfish
+, pyyaml
+, rebulk
+,
+}:
+
+buildPythonPackage rec {
+  pname = "trakit";
+  version = "0.2.5";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ratoaq2";
+    repo = "trakit";
+    tag = version;
+    hash = "sha256-x/83yRzvQ81+wS0lJr52KYBMoPvSVDr17ppxG/lSfUg=";
+  };
+
+  build-system = [ poetry-core ];
+
+  dependencies = [
+    babelfish
+    pyyaml
+    rebulk
+  ];
+
+  pythonImportsCheck = [ "trakit" ];
+
+  meta = {
+    description = "Guess additional information from track titles";
+    homepage = "https://github.com/ratoaq2/trakit";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
