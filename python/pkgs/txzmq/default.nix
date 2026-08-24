581b76d8d59c936578ca08af8c74334a25ea50f3
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:31 -0700

    python3Packages.txzmq: init at 1.0.0

---
+++ python/pkgs/txzmq/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pyzmq
+, twisted
+,
+}:
+
+buildPythonPackage rec {
+  pname = "txzmq";
+  version = "1.0.0";
+  format = "setuptools";
+
+  src = fetchPypi {
+    inherit version;
+    pname = "txZMQ";
+    hash = "sha256-jWB9C/CcqUYAuOQvByHb5D7lOgRwGCNErHrOfljcYXc=";
+  };
+
+  propagatedBuildInputs = [
+    pyzmq
+    twisted
+  ];
+
+  pythonImportsCheck = [ "txzmq" ];
+
+  meta = {
+    description = "Twisted bindings for ZeroMQ";
+    homepage = "https://github.com/smira/txZMQ";
+    license = lib.licenses.mpl20;
+    maintainers = [ ];
+  };
+}
