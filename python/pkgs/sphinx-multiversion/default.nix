3771f683370852dea02171486464a45381e696f7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:29 -0700

    python3Packages.sphinx-multiversion: init at 0.2.4

---
+++ python/pkgs/sphinx-multiversion/default.nix
@@ -0,0 +1,32 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, sphinx
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "sphinx-multiversion";
+  version = "0.2.4";
+
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit version;
+    pname = "sphinx-multiversion";
+    hash = "sha256-XNHKnste7WPLjWzl6cQ4yhOvT6mOfrbzdr5UHdSZC8s=";
+  };
+
+  build-system = [ setuptools ];
+  dependencies = [ sphinx ];
+
+  pythonImportsCheck = [ "sphinx_multiversion" ];
+
+  meta = {
+    description = "Sphinx extension for building self-hosted versioned docs";
+    homepage = "https://sphinx-contrib.github.io/multiversion";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
