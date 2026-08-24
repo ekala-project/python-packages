2fe9227a7fb46035b0cc970b33b653ed53d79948
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:34 -0700

    python3Packages.xknx: init at 3.18.0

---
+++ python/pkgs/xknx/default.nix
@@ -0,0 +1,38 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, cryptography
+, ifaddr
+, setuptools
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "xknx";
+  version = "3.18.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "XKNX";
+    repo = "xknx";
+    tag = finalAttrs.version;
+    hash = "sha256-AW7l5uLaaZj86UFRaRa3dPTORkcVqoRypNmgCRswmV8=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    cryptography
+    ifaddr
+  ];
+
+  pythonImportsCheck = [ "xknx" ];
+
+  meta = {
+    description = "KNX Library Written in Python";
+    homepage = "https://github.com/XKNX/xknx";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+})
