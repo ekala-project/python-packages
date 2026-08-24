562a191ff82e1ab40b11a6fe0d3b0faf47960d36
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:30 -0700

    python3Packages.textual-fastdatatable: init at 0.14.0

---
+++ python/pkgs/textual-fastdatatable/default.nix
@@ -0,0 +1,51 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, hatchling
+, pandas
+, pyarrow
+, pytz
+, textual
+, tzdata
+, polars
+,
+}:
+
+buildPythonPackage rec {
+  pname = "textual-fastdatatable";
+  version = "0.14.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "tconbeer";
+    repo = "textual-fastdatatable";
+    tag = "v${version}";
+    hash = "sha256-gm1h+r8rZO1/9sXoNwqVuBbv7CpZm2a3YAMHRHGg5uo=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    pandas
+    pyarrow
+    pytz
+    textual
+    tzdata
+  ];
+  optional-dependencies = {
+    polars = [ polars ];
+  };
+
+  pythonRelaxDeps = [
+    "numpy"
+  ];
+
+  pythonImportsCheck = [ "textual_fastdatatable" ];
+
+  meta = {
+    description = "Performance-focused reimplementation of Textual's DataTable widget, with a pluggable data storage backend";
+    homepage = "https://github.com/tconbeer/textual-fastdatatable";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
