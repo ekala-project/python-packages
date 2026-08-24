c26ff64283a3cb57ff63ee8cdbf61cdeb327b8cb
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:32 -0700

    python3Packages.ufoprocessor: init at 1.14.1

---
+++ python/pkgs/ufoprocessor/default.nix
@@ -0,0 +1,36 @@
+{ buildPythonPackage
+, fetchPypi
+, defcon
+, fonttools
+, lxml
+, mutatormath
+, fontmath
+, fontparts
+, setuptools-scm
+,
+}:
+
+buildPythonPackage rec {
+  pname = "ufoprocessor";
+  version = "1.14.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-/TjTzDWblBcbqNP9weTe/eIgas70+X11tIUDu4rAOwE=";
+  };
+
+  build-system = [ setuptools-scm ];
+
+  dependencies = [
+    defcon
+    fontmath
+    fontparts
+    fonttools
+    mutatormath
+  ]
+  ++ defcon.optional-dependencies.lxml
+  ++ fonttools.optional-dependencies.lxml
+  ++ fonttools.optional-dependencies.ufo;
+
+}
