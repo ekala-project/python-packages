cd5d5aaeac967e384a396386ee01c21068ffac15
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:25 -0700

    python3Packages.requests-unixsocket2: init at 1.0.1

---
+++ python/pkgs/requests-unixsocket2/default.nix
@@ -0,0 +1,37 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitLab
+, hatchling
+, requests
+, urllib3
+,
+}:
+
+buildPythonPackage rec {
+  pname = "requests-unixsocket2";
+  version = "1.0.1";
+  pyproject = true;
+
+  src = fetchFromGitLab {
+    owner = "thelabnyc";
+    repo = "requests-unixsocket2";
+    tag = "v${version}";
+    hash = "sha256-KgPIecKQibB5ZH+itw3OM9heSE3uDuodNS1R9dRkaHE=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    requests
+    urllib3
+  ];
+
+  pythonImportsCheck = [ "requests_unixsocket" ];
+
+  meta = {
+    description = "Use requests to talk HTTP via a UNIX domain socket";
+    homepage = "https://gitlab.com/thelabnyc/requests-unixsocket2";
+    license = lib.licenses.bsd0;
+    maintainers = [ ];
+  };
+}
