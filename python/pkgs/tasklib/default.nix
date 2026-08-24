db04e233b3de9a795c623f6af90a25f421710d2d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:30 -0700

    python3Packages.tasklib: init at 2.5.1

---
+++ python/pkgs/tasklib/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, six
+, pytz
+, tzlocal
+, fetchPypi
+,
+}:
+
+buildPythonPackage rec {
+  pname = "tasklib";
+  version = "2.5.1";
+  format = "setuptools";
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-XM1zG1JjbdEEV6i42FjLDQJv+qsePnUbr3kb+APjfXs=";
+  };
+
+  propagatedBuildInputs = [
+    six
+    pytz
+    tzlocal
+  ];
+
+  meta = {
+    homepage = "https://github.com/robgolding/tasklib";
+    description = "Library for interacting with taskwarrior databases";
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+    license = lib.licenses.bsd3;
+  };
+}
