6c86b551fe11c8451841c3ed059a7d77c9ff3eae
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:08 -0700

    python3Packages.cantools: init at 41.3.1

---
+++ python/pkgs/cantools/default.nix
@@ -0,0 +1,51 @@
+{ lib
+, argparse-addons
+, bitstruct
+, buildPythonPackage
+, python-can
+, crccheck
+, diskcache
+, fetchPypi
+, matplotlib
+, setuptools
+, setuptools-scm
+, textparser
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "cantools";
+  version = "41.3.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-Y5ZbAorAKrG0yGeqIH7Zn5D1WziuEHq+KH19ZtVDXZ8=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    argparse-addons
+    bitstruct
+    python-can
+    crccheck
+    diskcache
+    textparser
+  ];
+
+  optional-dependencies.plot = [ matplotlib ];
+
+  pythonImportsCheck = [ "cantools" ];
+
+  meta = {
+    description = "Tools to work with CAN bus";
+    homepage = "https://github.com/cantools/cantools";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "cantools";
+  };
+})
