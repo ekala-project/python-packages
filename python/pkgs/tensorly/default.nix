0acd456ddc95353574dc67cb0dff34f571914afb
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:30 -0700

    python3Packages.tensorly: init at 0.9.0

---
+++ python/pkgs/tensorly/default.nix
@@ -0,0 +1,54 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, numpy
+, scipy
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "tensorly";
+  version = "0.9.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "tensorly";
+    repo = "tensorly";
+    tag = version;
+    hash = "sha256-A6Zlp8fa7XFgf4qpg7SEtNLlYSNtDGLuRUEfzD+crQc=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    numpy
+    scipy
+  ];
+
+  pythonImportsCheck = [
+    "tensorly"
+    "tensorly.base"
+    "tensorly.cp_tensor"
+    "tensorly.tucker_tensor"
+    "tensorly.tt_tensor"
+    "tensorly.tt_matrix"
+    "tensorly.parafac2_tensor"
+    "tensorly.tenalg"
+    "tensorly.decomposition"
+    "tensorly.regression"
+    "tensorly.solvers"
+    "tensorly.metrics"
+    "tensorly.random"
+    "tensorly.datasets"
+    "tensorly.plugins"
+    "tensorly.contrib"
+  ];
+
+  meta = {
+    description = "Tensor learning in Python";
+    homepage = "https://tensorly.org/";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
