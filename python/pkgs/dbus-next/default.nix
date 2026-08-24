2e8445dc404e10979e951b3f0411e6546165023d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:09 -0700

    python3Packages.dbus-next: init at 0.2.3

---
+++ python/pkgs/dbus-next/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, python
+, setuptools
+, dbus
+,
+}:
+
+buildPythonPackage rec {
+  pname = "dbus-next";
+  version = "0.2.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "altdesktop";
+    repo = "python-dbus-next";
+    tag = "v${version}";
+    hash = "sha256-EKEQZFRUe+E65Z6DNCJFL5uCI5kbXrN7Tzd4O0X5Cqo=";
+  };
+
+  build-system = [ setuptools ];
+
+  # test_peer_interface hits a timeout
+  # test_tcp_connection_with_forwarding fails due to dbus
+  # creating unix socket anyway on v1.14.4
+  meta = {
+    description = "Zero-dependency DBus library for Python with asyncio support";
+    homepage = "https://github.com/altdesktop/python-dbus-next";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
