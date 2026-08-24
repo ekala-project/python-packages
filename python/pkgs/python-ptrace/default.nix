66a796875f43dd6312d03ab8595115841f3dbcdd
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:23 -0700

    python3Packages.python-ptrace: init at 0.9.9

---
+++ python/pkgs/python-ptrace/default.nix
@@ -0,0 +1,28 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "python-ptrace";
+  version = "0.9.9";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-Vrv+9E6vOne+SBOMyldnzfRx6CeP4Umfm3LxUZB/Jc8=";
+  };
+
+  nativeBuildInputs = [ setuptools ];
+
+  # requires distorm, which is optionally
+
+  meta = {
+    description = "Python binding of ptrace library";
+    homepage = "https://github.com/vstinner/python-ptrace";
+    license = lib.licenses.gpl2;
+    maintainers = [ ];
+  };
+}
