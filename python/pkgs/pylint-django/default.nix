5f34d50f1c520eba1d8c031d371bf2923f8d56c5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:19 -0700

    python3Packages.pylint-django: init at 2.8.0

---
+++ python/pkgs/pylint-django/default.nix
@@ -0,0 +1,38 @@
+{ lib
+, buildPythonPackage
+, django
+, fetchFromGitHub
+, poetry-core
+, pylint-plugin-utils
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pylint-django";
+  version = "2.8.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "PyCQA";
+    repo = "pylint-django";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-W3BPCK6fj4poZ1EaBUGyVyfRo/0sZa+2ktk96Ic6+q0=";
+  };
+
+  build-system = [ poetry-core ];
+
+  dependencies = [ pylint-plugin-utils ];
+
+  optional-dependencies = {
+    with_django = [ django ];
+  };
+
+  pythonImportsCheck = [ "pylint_django" ];
+
+  meta = {
+    description = "Pylint plugin to analyze Django applications";
+    homepage = "https://github.com/PyCQA/pylint-django";
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ ];
+  };
+})
