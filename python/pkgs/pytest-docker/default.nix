64ff02c25e18528f4aa4eb70532d597b3165545e
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:22 -0700

    python3Packages.pytest-docker: init at 3.2.5

---
+++ python/pkgs/pytest-docker/default.nix
@@ -0,0 +1,36 @@
+{ lib
+, attrs
+, buildPythonPackage
+, fetchFromGitHub
+, pytest
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pytest-docker";
+  version = "3.2.5";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "avast";
+    repo = "pytest-docker";
+    tag = "v${version}";
+    hash = "sha256-AkVLfCt2aQZrvSfa/5oXr95XUIR5mRqcMRz67kmuKKw=";
+  };
+
+  build-system = [ setuptools ];
+
+  buildInputs = [ pytest ];
+
+  dependencies = [ attrs ];
+
+  pythonImportsCheck = [ "pytest_docker" ];
+
+  meta = {
+    description = "Docker-based integration tests";
+    homepage = "https://github.com/avast/pytest-docker";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
