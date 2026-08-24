3bfbc9a87c9e4322ec391947746019294a09ac1a
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:08 -0700

    python3Packages.colormath2: init at 3.0.3

---
+++ python/pkgs/colormath2/default.nix
@@ -0,0 +1,37 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, networkx
+, numpy
+,
+}:
+
+buildPythonPackage rec {
+  pname = "colormath2";
+  version = "3.0.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "bkmgit";
+    repo = "python-colormath2";
+    tag = version;
+    hash = "sha256-G8b0L8A2RzbVQFPNg2fuBklqTNjo3yqvek/+GnqtsHc=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    networkx
+    numpy
+  ];
+
+  pythonImportsCheck = [ "colormath2" ];
+
+  meta = {
+    description = "Color math and conversion library (fork)";
+    homepage = "https://github.com/bkmgit/python-colormath2";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
