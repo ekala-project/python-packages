5f02f749602d1555f94e01894f975b322a7fe4f5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:23 -0700

    python3Packages.python-string-utils: init at 1.0.0

---
+++ python/pkgs/python-string-utils/default.nix
@@ -0,0 +1,25 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+,
+}:
+
+buildPythonPackage rec {
+  pname = "python-string-utils";
+  version = "1.0.0";
+  format = "setuptools";
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-3PkGCwPwdkfApgNAjciwP4B/O1SgXG4Z6xRGAlb6wMs=";
+  };
+
+  pythonImportsCheck = [ "string_utils" ];
+
+  meta = {
+    description = "Handy Python library to validate, manipulate and generate strings";
+    homepage = "https://github.com/daveoncode/python-string-utils";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
