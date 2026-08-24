c3f35acffb47b23d5b4f2491c57408e6400da3d7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:10 -0700

    python3Packages.exif: init at 1.6.0

---
+++ python/pkgs/exif/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitLab
+, plum-py
+,
+}:
+
+buildPythonPackage rec {
+  pname = "exif";
+  version = "1.6.0";
+  format = "setuptools";
+
+  src = fetchFromGitLab {
+    owner = "TNThieding";
+    repo = "exif";
+    tag = "v${version}";
+    hash = "sha256-uiHL3m0C6+YnAHRLwzMCSzffrQsSyVcuem6FBtTLxek=";
+  };
+
+  propagatedBuildInputs = [ plum-py ];
+
+  pythonImportsCheck = [ "exif" ];
+
+  meta = {
+    description = "Read and modify image EXIF metadata using Python";
+    homepage = "https://gitlab.com/TNThieding/exif";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
