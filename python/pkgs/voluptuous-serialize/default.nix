17fb1d12ebd68bb868002b6a7c854502b81bc160
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:33 -0700

    python3Packages.voluptuous-serialize: init at 2.7.0

---
+++ python/pkgs/voluptuous-serialize/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, voluptuous
+,
+}:
+
+buildPythonPackage rec {
+  pname = "voluptuous-serialize";
+  version = "2.7.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "home-assistant-libs";
+    repo = "voluptuous-serialize";
+    tag = version;
+    hash = "sha256-vmBK4FJr15wOYHtH14OqeyY/vgVOSrpo0Sd9wqu4zgo=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ voluptuous ];
+
+  pythonImportsCheck = [ "voluptuous_serialize" ];
+
+  meta = {
+    description = "Convert Voluptuous schemas to dictionaries so they can be serialized";
+    homepage = "https://github.com/home-assistant-libs/voluptuous-serialize";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
