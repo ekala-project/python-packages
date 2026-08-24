56aa68ba5ef79248592621f0c452174053619542
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:24 -0700

    python3Packages.qdarkstyle: init at 3.2.3

---
+++ python/pkgs/qdarkstyle/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, fetchPypi
+, buildPythonPackage
+, helpdev
+, qtpy
+,
+}:
+
+buildPythonPackage rec {
+  pname = "qdarkstyle";
+  version = "3.2.3";
+  format = "setuptools";
+
+  src = fetchPypi {
+    inherit version;
+    pname = "QDarkStyle";
+    hash = "sha256-DAt/dKbpISEAiZKzabq2BGgVfbHALNMNZKXpo7QC8a4=";
+  };
+
+  # No tests available
+
+  propagatedBuildInputs = [
+    helpdev
+    qtpy
+  ];
+
+  meta = {
+    description = "Dark stylesheet for Python and Qt applications";
+    homepage = "https://github.com/ColinDuquesnoy/QDarkStyleSheet";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
