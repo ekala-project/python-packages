4c41a79190ef4ad2c40022308b229e764d557587
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:15 -0700

    python3Packages.pandas-flavor: init at 0.8.1

---
+++ python/pkgs/pandas-flavor/default.nix
@@ -0,0 +1,44 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  setuptools-scm
+, # dependencies
+  pandas
+, xarray
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pandas-flavor";
+  version = "0.8.1";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "pyjanitor-devs";
+    repo = "pandas_flavor";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-c1pHH8vQOl1qicJJCVGuQoPbJp9uK03KDVr+rJWByhY=";
+  };
+
+  build-system = [
+    setuptools-scm
+  ];
+
+  dependencies = [
+    pandas
+    xarray
+  ];
+
+  pythonImportsCheck = [
+    "pandas_flavor"
+  ];
+
+  meta = {
+    description = "The easy way to write your own flavor of Pandas";
+    homepage = "https://github.com/pyjanitor-devs/pandas_flavor";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
