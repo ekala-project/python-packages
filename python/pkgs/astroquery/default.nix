2a5245a52888bd49c87c83e6e3fd3b0933255854
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:07 -0700

    python3Packages.astroquery: init at 0.4.11

---
+++ python/pkgs/astroquery/default.nix
@@ -0,0 +1,52 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, astropy
+, requests
+, keyring
+, beautifulsoup4
+, html5lib
+, pyvo
+, astropy-helpers
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "astroquery";
+  version = "0.4.11";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "astropy";
+    repo = "astroquery";
+    tag = "v${version}";
+    hash = "sha256-BcdRBPnJfuW17p31xUhjBmP7Lv98CnmOTCO4aU0xpMM=";
+  };
+
+  build-system = [
+    astropy-helpers
+    setuptools
+  ];
+
+  dependencies = [
+    astropy
+    requests
+    keyring
+    beautifulsoup4
+    html5lib
+    pyvo
+  ];
+
+  # Disable automatic update of the astropy-helper module
+  postPatch = ''
+    substituteInPlace setup.cfg --replace "auto_use = True" "auto_use = False"
+  '';
+
+  meta = {
+    description = "Functions and classes to access online data resources";
+    homepage = "https://astroquery.readthedocs.io/";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
