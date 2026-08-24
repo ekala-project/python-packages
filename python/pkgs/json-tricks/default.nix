e5ac69ac6fa8eb2e182f1b72efef498129f522f8
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:12 -0700

    python3Packages.json-tricks: init at 3.17.3

---
+++ python/pkgs/json-tricks/default.nix
@@ -0,0 +1,27 @@
+{ lib
+, fetchFromGitHub
+, buildPythonPackage
+,
+}:
+
+buildPythonPackage rec {
+  pname = "json-tricks";
+  version = "3.17.3";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "mverleg";
+    repo = "pyjson_tricks";
+    tag = "v${version}";
+    hash = "sha256-xddMc4PvVI+mqB3eeVqECZmdeSKAURsdbOnUAXahqM0=";
+  };
+
+  pythonImportsCheck = [ "json_tricks" ];
+
+  meta = {
+    description = "Extra features for Python JSON handling";
+    homepage = "https://github.com/mverleg/pyjson_tricks";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
