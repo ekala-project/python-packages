3188dbf383608c5b7af0ca7e48f4ee5ec40928f7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:27 -0700

    python3Packages.smbus2: init at 0.6.1

---
+++ python/pkgs/smbus2/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "smbus2";
+  version = "0.6.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "kplindegaard";
+    repo = "smbus2";
+    tag = finalAttrs.version;
+    hash = "sha256-CWcRlbZTLiB45DaV6rbhvlk8cTaEJgPAq/JDmbxD7H4=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "smbus2" ];
+
+  meta = {
+    description = "Drop-in replacement for smbus-cffi/smbus-python";
+    homepage = "https://smbus2.readthedocs.io/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
