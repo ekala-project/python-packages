eab92ab674b5312394e0baea1c3f7276591d8fd9
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:12 -0700

    python3Packages.jxlpy: init at 0.9.5

---
+++ python/pkgs/jxlpy/default.nix
@@ -0,0 +1,38 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, cython
+, libjxl
+,
+}:
+
+buildPythonPackage rec {
+  pname = "jxlpy";
+  version = "0.9.5";
+
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-Kqdm8b3hgO0Q3zE68rSIM4Jd7upjG+SQywSseGwCFUI=";
+  };
+
+  build-system = [
+    setuptools
+    cython
+  ];
+
+  buildInputs = [ libjxl ];
+
+  # no tests
+
+  pythonImportsCheck = [ "jxlpy" ];
+
+  meta = {
+    description = "Cython bindings and Pillow plugin for JPEG XL";
+    homepage = "https://github.com/olokelo/jxlpy";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
