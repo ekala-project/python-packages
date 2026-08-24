f6660c42f705aa7c8b5ab0d85883bd61c252ef3c
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:32 -0700

    python3Packages.vector: init at 1.8.1

---
+++ python/pkgs/vector/default.nix
@@ -0,0 +1,46 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  hatch-vcs
+, hatchling
+, # dependencies
+  numpy
+, packaging
+, # tests
+  awkward
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "vector";
+  version = "1.8.1";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "scikit-hep";
+    repo = "vector";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-qQesB6xYzAHMEmscdzoBLJqj36NLd5K1EaPLJlkSFOU=";
+  };
+
+  build-system = [
+    hatch-vcs
+    hatchling
+  ];
+
+  dependencies = [
+    numpy
+    packaging
+  ];
+
+  pythonImportsCheck = [ "vector" ];
+
+  meta = {
+    description = "Library for 2D, 3D, and Lorentz vectors, especially arrays of vectors, to solve common physics problems in a NumPy-like way";
+    homepage = "https://github.com/scikit-hep/vector";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+})
