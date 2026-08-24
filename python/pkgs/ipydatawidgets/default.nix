ad01a29ed5b096aa2ecb82a3963b71536c17dc2c
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:12 -0700

    python3Packages.ipydatawidgets: init at 4.3.5

---
+++ python/pkgs/ipydatawidgets/default.nix
@@ -0,0 +1,41 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, jupyter-packaging
+, ipywidgets
+, numpy
+, six
+, traittypes
+,
+}:
+
+buildPythonPackage rec {
+  __structuredAttrs = true;
+
+  pname = "ipydatawidgets";
+  version = "4.3.5";
+  format = "setuptools";
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-OU8kiVdlh8/XVTd6CaBn9GytIggZZQkgIf0avL54Uqg=";
+  };
+
+  nativeBuildInputs = [ jupyter-packaging ];
+
+  setupPyBuildFlags = [ "--skip-npm" ];
+
+  propagatedBuildInputs = [
+    ipywidgets
+    numpy
+    six
+    traittypes
+  ];
+
+  meta = {
+    description = "Widgets to help facilitate reuse of large datasets across different widgets";
+    homepage = "https://github.com/vidartf/ipydatawidgets";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
