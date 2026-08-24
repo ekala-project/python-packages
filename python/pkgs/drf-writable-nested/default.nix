3a2e659e7ef59b8bb12e5703fa1f1c67fe398aec
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:10 -0700

    python3Packages.drf-writable-nested: init at 0.7.2

---
+++ python/pkgs/drf-writable-nested/default.nix
@@ -0,0 +1,32 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, django
+, djangorestframework
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "drf-writable-nested";
+  version = "0.7.2";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "beda-software";
+    repo = "drf-writable-nested";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-VkQ3Di3vXxQAmvuMP8KpGVVdx7LMYcQFEF4ZsuA9KeA=";
+  };
+
+  propagatedBuildInputs = [
+    django
+    djangorestframework
+  ];
+
+  meta = {
+    description = "Writable nested model serializer for Django REST Framework";
+    homepage = "https://github.com/beda-software/drf-writable-nested";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+})
