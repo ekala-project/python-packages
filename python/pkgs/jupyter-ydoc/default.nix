47e5b1da8953b2969c340cc0a30872b3cde3c926
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:12 -0700

    python3Packages.jupyter-ydoc: init at 3.4.1

---
+++ python/pkgs/jupyter-ydoc/default.nix
@@ -0,0 +1,49 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  hatch-nodejs-version
+, hatchling
+, # dependencies
+  anyio
+, pycrdt
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "jupyter-ydoc";
+  version = "3.4.1";
+  pyproject = true;
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "jupyter-server";
+    repo = "jupyter_ydoc";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-HlYSPlYiHyVwJhsRY10SgotKa9ejlj0hlxbS+chtaBI=";
+  };
+
+  build-system = [
+    hatch-nodejs-version
+    hatchling
+  ];
+
+  pythonRelaxDeps = [
+    "pycrdt"
+  ];
+  dependencies = [
+    anyio
+    pycrdt
+  ];
+
+  pythonImportsCheck = [ "jupyter_ydoc" ];
+
+  # requires a Node.js environment
+
+  meta = {
+    description = "Document structures for collaborative editing using Yjs/pycrdt";
+    homepage = "https://github.com/jupyter-server/jupyter_ydoc";
+    license = lib.licenses.bsd3;
+    teams = [ lib.teams.jupyter ];
+  };
+})
