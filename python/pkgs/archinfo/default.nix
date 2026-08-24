e99c5f1442eff11ecd9fa5c002d77b6af3272316
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:07 -0700

    python3Packages.archinfo: init at 9.2.154

---
+++ python/pkgs/archinfo/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pythonOlder
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "archinfo";
+  version = "9.2.154";
+  pyproject = true;
+
+  disabled = pythonOlder "3.12";
+
+  src = fetchFromGitHub {
+    owner = "angr";
+    repo = "archinfo";
+    tag = "v${version}";
+    hash = "sha256-Vks7Rjd8x2zeHnJPs0laH56S4b8pnR1cK82SpK+XOgE=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "archinfo" ];
+
+  meta = {
+    description = "Classes with architecture-specific information";
+    homepage = "https://github.com/angr/archinfo";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
