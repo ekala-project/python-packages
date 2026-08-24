1eeb6b815cc5ab47489ff84ad15a17192e743ecf
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:22 -0700

    python3Packages.pytest-freezer: init at 0.4.9

---
+++ python/pkgs/pytest-freezer/default.nix
@@ -0,0 +1,36 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, flit-core
+, freezegun
+, pytest
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pytest-freezer";
+  version = "0.4.9";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pytest-dev";
+    repo = "pytest-freezer";
+    tag = version;
+    hash = "sha256-WJGwkON/RAiUiGzNkeqjzch4CEr6mPXij5dqz1ncRXs=";
+  };
+
+  build-system = [ flit-core ];
+
+  buildInputs = [ pytest ];
+
+  dependencies = [ freezegun ];
+
+  pythonImportsCheck = [ "pytest_freezer" ];
+
+  meta = {
+    description = "Pytest plugin providing a fixture interface for spulec/freezegun";
+    homepage = "https://github.com/pytest-dev/pytest-freezer";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
