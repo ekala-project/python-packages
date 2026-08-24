90f232bd9bf7fd77dca7fb35d5075994b8744c7a
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:14 -0700

    python3Packages.oslex: init at 2.0.0

---
+++ python/pkgs/oslex/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, hatchling
+, mslex
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "oslex";
+  version = "2.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "petamas";
+    repo = "oslex";
+    tag = "release/v${finalAttrs.version}";
+    hash = "sha256-BTyLL3tb1P8VMGvTgoHGmwvFqf3gOyXOI+YmHuEjrKc=";
+  };
+
+  build-system = [
+    hatchling
+  ];
+
+  dependencies = [
+    mslex
+  ];
+
+  pythonImportsCheck = [
+    "oslex"
+  ];
+
+  meta = {
+    description = "OS-independent wrapper for shlex and mslex";
+    homepage = "https://github.com/petamas/oslex";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
