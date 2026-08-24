0e140a5c529057de578ac35972a261d9848086df
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:17 -0700

    python3Packages.pycups: init at 2.0.4

---
+++ python/pkgs/pycups/default.nix
@@ -0,0 +1,32 @@
+{ stdenv
+, lib
+, buildPythonPackage
+, fetchPypi
+, cups
+, libiconv
+, setuptools
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pycups";
+  version = "2.0.4";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-hD44XB2/aUmWyoTvAqfzDCg3YDVYj1++rNa64AXPfI0=";
+  };
+
+  build-system = [ setuptools ];
+
+  buildInputs = [ cups ] ++ lib.optional stdenv.hostPlatform.isDarwin libiconv;
+
+  # Wants to connect to CUPS
+
+  meta = {
+    description = "Python bindings for libcups";
+    homepage = "http://cyberelk.net/tim/software/pycups/";
+    license = lib.licenses.gpl2Plus;
+  };
+})
