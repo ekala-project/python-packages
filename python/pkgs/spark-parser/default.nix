bddd3e2579b65f624e95217bedd129ec295b4008
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:27 -0700

    python3Packages.spark-parser: init at 1.9.0

---
+++ python/pkgs/spark-parser/default.nix
@@ -0,0 +1,32 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, click
+,
+}:
+
+buildPythonPackage rec {
+  pname = "spark-parser";
+  version = "1.9.0";
+  format = "setuptools";
+
+  src = fetchPypi {
+    pname = "spark_parser";
+    inherit version;
+    sha256 = "sha256-3GbUjEJlxBM9tBqcX+nBxQKzsgFn3xWKDyNM0xcSz2Q=";
+  };
+
+  propagatedBuildInputs = [ click ];
+
+  unittestFlagsArray = [
+    "-s"
+  ];
+
+  meta = {
+    description = "Early-Algorithm Context-free grammar Parser";
+    mainProgram = "spark-parser-coverage";
+    homepage = "https://github.com/rocky/python-spark";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
