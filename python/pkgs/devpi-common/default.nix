2cca872ad15917f839bce384679958ac17b5f9ab
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:09 -0700

    python3Packages.devpi-common: init at 4.1.1

---
+++ python/pkgs/devpi-common/default.nix
@@ -0,0 +1,50 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  setuptools
+, # dependencies
+  lazy
+, requests
+, tomli
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "devpi-common";
+  version = "4.1.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "devpi";
+    repo = "devpi";
+    tag = "common-${finalAttrs.version}";
+    hash = "sha256-YFY2iLnORzFxnfGYU2kCpJL8CZi+lALIkL1bRpfd4NE=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail '"setuptools_changelog_shortener",' ""
+  '';
+
+  sourceRoot = "${finalAttrs.src.name}/common";
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
+    lazy
+    requests
+    tomli
+  ];
+
+  pythonImportsCheck = [ "devpi_common" ];
+
+  meta = {
+    homepage = "https://github.com/devpi/devpi";
+    description = "Utilities jointly used by devpi-server and devpi-client";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
