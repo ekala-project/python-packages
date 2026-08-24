5551da18afb306da67fb0e5e7d514bba6cdbcbdf
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:10 -0700

    python3Packages.esprima: init at 4.0.1

---
+++ python/pkgs/esprima/default.nix
@@ -0,0 +1,28 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+,
+}:
+
+buildPythonPackage rec {
+  pname = "esprima";
+  version = "4.0.1";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "Kronuz";
+    repo = "esprima-python";
+    rev = "v${version}";
+    sha256 = "WtkPCReXhxyr6pOzE9gsdIeBlLk+nSnbxkS3OowEaHo=";
+  };
+
+  pythonImportsCheck = [ "esprima" ];
+
+  meta = {
+    description = "Python parser for standard-compliant ECMAScript";
+    mainProgram = "esprima";
+    homepage = "https://github.com/Kronuz/esprima-python";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
