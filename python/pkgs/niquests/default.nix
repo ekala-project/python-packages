2f116c45ed3a9256d590fd21833e7c416c046079
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:13 -0700

    python3Packages.niquests: init at 3.21.0

---
+++ python/pkgs/niquests/default.nix
@@ -0,0 +1,57 @@
+{ buildPythonPackage
+, charset-normalizer
+, fetchFromGitHub
+, hatchling
+, lib
+, orjson
+, urllib3-future
+, wassima
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "niquests";
+  version = "3.21.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jawah";
+    repo = "niquests";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-oKxs1ivKzoCIqFnh81MwCbLfjH5JTKj/orTZNe7uiC4=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    charset-normalizer
+    urllib3-future
+    wassima
+  ];
+
+  optional-dependencies = {
+    inherit (urllib3-future.optional-dependencies)
+      brotli
+      socks
+      ws
+      zstd
+      ;
+    full = [
+      orjson
+    ];
+    http3 = urllib3-future.optional-dependencies.qh3;
+    ocsp = urllib3-future.optional-dependencies.qh3;
+    speedups = [
+      orjson
+    ];
+  };
+
+  pythonImportsCheck = [ "niquests" ];
+
+  meta = {
+    description = "Simple HTTP library that is a drop-in replacement for Requests";
+    homepage = "https://github.com/jawah/niquests";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+})
