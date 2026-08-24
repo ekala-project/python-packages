9fffb73cb575f2aad45f406710e29bdca54e708d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:10 -0700

    python3Packages.dom-toml: init at 2.3.0

---
+++ python/pkgs/dom-toml/default.nix
@@ -0,0 +1,48 @@
+{ lib
+, attrs
+, buildPythonPackage
+, domdf-python-tools
+, fetchFromGitHub
+, flit-core
+, tomli-w
+,
+}:
+
+buildPythonPackage rec {
+  pname = "dom-toml";
+  version = "2.3.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "domdfcoding";
+    repo = "dom_toml";
+    tag = "v${version}";
+    hash = "sha256-ukRnQecbgZBdTHhyEBIoHUwGTwQVJxo+u7Dqg4Kjvsw=";
+  };
+
+  build-system = [ flit-core ];
+
+  dependencies = [ domdf-python-tools ];
+
+  optional-dependencies = {
+    all = [
+      attrs
+      tomli-w
+    ];
+    config = [
+      attrs
+      tomli-w
+    ];
+  };
+
+  # Circular dependency whey -> domdf-python-tools -> coincidence
+
+  pythonImportsCheck = [ "dom_toml" ];
+
+  meta = {
+    description = "Dom's tools for Tom's Obvious, Minimal Language";
+    homepage = "https://github.com/domdfcoding/dom_toml";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
