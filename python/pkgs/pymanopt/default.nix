9d52e95b00418e95e3b695d17847c5bebcdadf58
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:19 -0700

    python3Packages.pymanopt: init at 2.2.1

---
+++ python/pkgs/pymanopt/default.nix
@@ -0,0 +1,46 @@
+{ lib
+, fetchFromGitHub
+, buildPythonPackage
+, # build-system
+  setuptools-scm
+, # dependencies
+  numpy
+, scipy
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pymanopt";
+  version = "2.2.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pymanopt";
+    repo = "pymanopt";
+    tag = finalAttrs.version;
+    hash = "sha256-LOEulticgCWZBCf3qj5KFBHt0lMd4H85368IhG3DQ4g=";
+  };
+
+  preConfigure = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail '"pip==22.3.1",' ""
+  '';
+
+  build-system = [
+    setuptools-scm
+  ];
+
+  dependencies = [
+    numpy
+    scipy
+  ];
+
+  pythonImportsCheck = [ "pymanopt" ];
+
+  meta = {
+    description = "Python toolbox for optimization on Riemannian manifolds with support for automatic differentiation";
+    homepage = "https://www.pymanopt.org/";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+})
