5c749a8e3bb48db160cf06a6ae672a4860fedb7a
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:34 -0700

    python3Packages.xkbcommon: init at 1.5.1

---
+++ python/pkgs/xkbcommon/default.nix
@@ -0,0 +1,42 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, python
+, setuptools
+, cffi
+, pkg-config
+, libxkbcommon
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "xkbcommon";
+  version = "1.5.1";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchPypi {
+    inherit (finalAttrs) pname version;
+    hash = "sha256-rBdICNv2HTXZ2oBL8zuqx0vG8r4MEIWUrpPHnNFd3DY=";
+  };
+
+  build-system = [ setuptools ];
+
+  nativeBuildInputs = [ pkg-config ];
+  propagatedNativeBuildInputs = [ cffi ];
+  buildInputs = [ libxkbcommon ];
+  dependencies = [ cffi ];
+  postBuild = ''
+    ${python.pythonOnBuildForHost.interpreter} xkbcommon/ffi_build.py
+  '';
+
+  pythonImportsCheck = [ "xkbcommon" ];
+
+  meta = {
+    homepage = "https://github.com/sde1000/python-xkbcommon";
+    description = "Python bindings for libxkbcommon using cffi";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
