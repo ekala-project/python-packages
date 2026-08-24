ab37b9444694a4e208829071dc206b7ae229e72d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:28 -0700

    python3Packages.sphinx-inline-tabs: init at 2025.12.21.14

---
+++ python/pkgs/sphinx-inline-tabs/default.nix
@@ -0,0 +1,35 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, flit-core
+, sphinx
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "sphinx-inline-tabs";
+  version = "2025.12.21.14";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pradyunsg";
+    repo = "sphinx-inline-tabs";
+    tag = finalAttrs.version;
+    hash = "sha256-aHsTdCVu/e9uaM4ayOfY3IBjjivZwDiHoWA0W2vyvNA=";
+  };
+
+  nativeBuildInputs = [ flit-core ];
+
+  propagatedBuildInputs = [ sphinx ];
+
+  # no tests, see https://github.com/pradyunsg/sphinx-inline-tabs/issues/6
+
+  pythonImportsCheck = [ "sphinx_inline_tabs" ];
+
+  meta = {
+    description = "Add inline tabbed content to your Sphinx documentation";
+    homepage = "https://github.com/pradyunsg/sphinx-inline-tabs";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
