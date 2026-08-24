ba3b579316203189c57eb9b604c0344a4b78a125
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:18 -0700

    python3Packages.pydiffx: init at 1.1

---
+++ python/pkgs/pydiffx/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, six
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pydiffx";
+  version = "1.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "beanbaginc";
+    repo = "diffx";
+    tag = "pydiffx/release-${version}";
+    hash = "sha256-oJjHrg1X02SmNJKbWbTPc0kycI+jLj0C4eUFFXwb+TA=";
+  };
+
+  sourceRoot = "${src.name}/python";
+
+  postPatch = ''
+    substituteInPlace pydiffx/tests/testcases.py \
+      --replace-fail "assertRaisesRegexp" "assertRaisesRegex"
+  '';
+
+  build-system = [ setuptools ];
+
+  dependencies = [ six ];
+
+  pythonImportsCheck = [ "pydiffx" ];
+
+  meta = {
+    description = "DiffX file format and utilities";
+    homepage = "https://github.com/beanbaginc/diffx";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
