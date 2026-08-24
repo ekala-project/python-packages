62005e47e120afb450291d23bf8a859e4f2165e4
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:34 -0700

    python3Packages.wsme: init at 0.12.1

---
+++ python/pkgs/wsme/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pbr
+, setuptools
+, importlib-metadata
+, simplegeneric
+, netaddr
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "wsme";
+  version = "0.12.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "WSME";
+    inherit (finalAttrs) version;
+    hash = "sha256-m36yJErzxwSskUte0iGVS7aK3QqLKy84okSwZ7M3mS0=";
+  };
+
+  build-system = [ setuptools ];
+
+  nativeBuildInputs = [ pbr ];
+
+  dependencies = [
+    importlib-metadata
+    simplegeneric
+    netaddr
+  ];
+
+  meta = {
+    description = "Simplify the writing of REST APIs, and extend them with additional protocols";
+    homepage = "https://pythonhosted.org/WSME/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
