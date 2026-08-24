281d471f26d0ae1433883cd360c44da9260cb2a7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:32 -0700

    python3Packages.unicrypto: init at 0.0.12

---
+++ python/pkgs/unicrypto/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pycryptodome
+, pycryptodomex
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "unicrypto";
+  version = "0.0.12";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "skelsec";
+    repo = "unicrypto";
+    tag = version;
+    hash = "sha256-RYwovFMalBNDPDEVjQ/8/N7DkOMiyeEQ5ESdgCK8RW8=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    pycryptodome
+    pycryptodomex
+  ];
+
+  # Module has no tests
+
+  pythonImportsCheck = [ "unicrypto" ];
+
+  meta = {
+    description = "Unified interface for cryptographic libraries";
+    homepage = "https://github.com/skelsec/unicrypto";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
