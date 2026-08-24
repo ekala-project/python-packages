e120778d435e8d9ba61a6f1f534f033f5de0594f
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:18 -0700

    python3Packages.pygobject-stubs: init at 2.16.0

---
+++ python/pkgs/pygobject-stubs/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, typing-extensions
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pygobject-stubs";
+  version = "2.16.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pygobject";
+    repo = "pygobject-stubs";
+    tag = "v${version}";
+    hash = "sha256-3gWz/mWAMoXAHMfCFv3ri680iuMm0WLtGyMODujq31M=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ typing-extensions ];
+
+  # This package does not include any tests.
+
+  meta = {
+    description = "PEP 561 Typing Stubs for PyGObject";
+    homepage = "https://github.com/pygobject/pygobject-stubs";
+    license = lib.licenses.lgpl21Plus;
+    maintainers = [ ];
+  };
+}
