028d770590b3c50f2ae59599d680d6027fb89691
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:22 -0700

    python3Packages.pytest-mpi: init at 0.6

---
+++ python/pkgs/pytest-mpi/default.nix
@@ -0,0 +1,44 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, sybil
+, pytest
+, mpi
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pytest-mpi";
+  version = "0.6";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "aragilar";
+    repo = "pytest-mpi";
+    rev = "v${version}";
+    hash = "sha256-m3HTGLoPnYeg0oeIA1nzTzch7FtkuXTYpox4rRgo5MU=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+  buildInputs = [
+    # Don't propagate it to let a different pytest version be used if needed
+    pytest
+  ];
+  dependencies = [
+    sybil
+  ];
+
+  # to debug. (TODO: discuss this with upstream)
+
+  pythonImportsCheck = [ "pytest_mpi" ];
+
+  meta = {
+    description = "Pytest plugin for working with MPI";
+    homepage = "https://github.com/aragilar/pytest-mpi";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
