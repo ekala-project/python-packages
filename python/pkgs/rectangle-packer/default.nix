58577d39d7e02099e536b35ea4ff884caa01028c
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:25 -0700

    python3Packages.rectangle-packer: init at 2.0.5

---
+++ python/pkgs/rectangle-packer/default.nix
@@ -0,0 +1,27 @@
+{ buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, cython
+,
+}:
+
+buildPythonPackage rec {
+  pname = "rectangle-packer";
+  version = "2.0.5";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Penlect";
+    repo = "rectangle-packer";
+    rev = version;
+    hash = "sha256-BHFy88yrcfDRalvrzwUHseSKmQXIM70ginnd+W6LVLY=";
+  };
+
+  build-system = [
+    cython
+    setuptools
+  ];
+
+  pythonImportsCheck = [ "rpack" ];
+
+}
