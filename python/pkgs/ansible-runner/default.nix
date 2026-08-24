e519bb2058315ef3d7d472897ce4f2b3db918f6e
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:07 -0700

    python3Packages.ansible-runner: init at 2.4.3

---
+++ python/pkgs/ansible-runner/default.nix
@@ -0,0 +1,54 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  setuptools
+, setuptools-scm
+, # dependencies
+  packaging
+, pexpect
+, python-daemon
+, pyyaml
+,
+}:
+
+buildPythonPackage rec {
+  pname = "ansible-runner";
+  version = "2.4.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ansible";
+    repo = "ansible-runner";
+    tag = version;
+    hash = "sha256-kKG6Jn0RRa6rglUrE8fJmXj+nbeKNUbL/GVWqT20Hfk=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "setuptools>=45, <=70.0.0" setuptools \
+      --replace-fail "setuptools-scm[toml]>=6.2, <=8.1.0" setuptools-scm
+  '';
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    packaging
+    pexpect
+    python-daemon
+    pyyaml
+  ];
+
+  pythonImportsCheck = [ "ansible_runner" ];
+
+  meta = {
+    description = "Helps when interfacing with Ansible";
+    homepage = "https://github.com/ansible/ansible-runner";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+    mainProgram = "ansible-runner";
+  };
+}
