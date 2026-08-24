493e6d9095144808c152cb5cea408dae047ed92f
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:25 -0700

    python3Packages.rbtools: init at 5.2.1

---
+++ python/pkgs/rbtools/default.nix
@@ -0,0 +1,58 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, isPy3k
+, setuptools
+, colorama
+, texttable
+, tqdm
+, certifi
+, housekeeping
+, puremagic
+, pydiffx
+, typing-extensions
+, importlib-metadata
+, importlib-resources
+, packaging
+,
+}:
+
+buildPythonPackage rec {
+  pname = "rbtools";
+  version = "5.2.1";
+  pyproject = true;
+  disabled = !isPy3k;
+
+  src = fetchFromGitHub {
+    owner = "reviewboard";
+    repo = "rbtools";
+    tag = "release-${version}";
+    hash = "sha256-Ci9lHlP2X95y7ldHBbqb5qWozPj3TJ0AxeVhqzVsdFA=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    texttable
+    tqdm
+    colorama
+    certifi
+    housekeeping
+    puremagic
+    pydiffx
+    typing-extensions
+    importlib-metadata
+    importlib-resources
+    packaging
+  ];
+
+  pythonRelaxDeps = [ "pydiffx" ];
+
+  meta = {
+    homepage = "https://www.reviewboard.org/docs/rbtools/dev/";
+    description = "RBTools is a set of command line tools for working with Review Board and RBCommons";
+    mainProgram = "rbt";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
