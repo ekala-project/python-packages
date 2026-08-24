ecfd9b9763a4b28a5fa536c8462c50c4b4782139
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:09 -0700

    python3Packages.dbus-signature-pyparsing: init at 0.4.1

---
+++ python/pkgs/dbus-signature-pyparsing/default.nix
@@ -0,0 +1,29 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pyparsing
+,
+}:
+
+buildPythonPackage rec {
+  pname = "dbus-signature-pyparsing";
+  version = "0.4.1";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "stratis-storage";
+    repo = "dbus-signature-pyparsing";
+    rev = "v${version}";
+    hash = "sha256-+jY8kg3jBDpZr5doih3DiyUEcSskq7TgubmW3qdBoZM=";
+  };
+
+  propagatedBuildInputs = [ pyparsing ];
+  pythonImportsCheck = [ "dbus_signature_pyparsing" ];
+
+  meta = {
+    description = "Parser for a D-Bus Signature";
+    homepage = "https://github.com/stratis-storage/dbus-signature-pyparsing";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
