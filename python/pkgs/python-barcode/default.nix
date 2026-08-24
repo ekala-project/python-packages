17e4ff9e20303573bcc798192f119ec8c2ebf10d
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:23 -0700

    python3Packages.python-barcode: init at 0.16.1

---
+++ python/pkgs/python-barcode/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, setuptools-scm
+, pillow
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "python-barcode";
+  version = "0.16.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "WhyNotHugo";
+    repo = "python-barcode";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-a/w2JxFBm/jqIRnqIB7ZtkdiLnBNjbR0V5SNuau/YxY=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  optional-dependencies = {
+    images = [ pillow ];
+  };
+
+  pythonImportsCheck = [ "barcode" ];
+
+  meta = {
+    description = "Create standard barcodes with Python";
+    mainProgram = "python-barcode";
+    homepage = "https://github.com/WhyNotHugo/python-barcode";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
