20f49df146f15ebf26bec9cedaa096aa119d2135
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:22 -0700

    python3Packages.pytest-jupyter: init at 0.11.0

---
+++ python/pkgs/pytest-jupyter/default.nix
@@ -0,0 +1,58 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build
+  hatchling
+, pytest
+, # runtime
+  jupyter-core
+, # optionals
+  jupyter-client
+, ipykernel
+, jupyter-server
+, nbformat
+,
+}:
+
+let
+  self = buildPythonPackage rec {
+    pname = "pytest-jupyter";
+    version = "0.11.0";
+    pyproject = true;
+
+    src = fetchFromGitHub {
+      owner = "jupyter-server";
+      repo = "pytest-jupyter";
+      tag = "v${version}";
+      hash = "sha256-x3Q9Ei4WIMDjjrYfWees30eooWep60EljGYyUyypxqQ=";
+    };
+
+    nativeBuildInputs = [ hatchling ];
+
+    buildInputs = [ pytest ];
+
+    propagatedBuildInputs = [ jupyter-core ];
+
+    optional-dependencies = {
+      client = [
+        jupyter-client
+        nbformat
+        ipykernel
+      ];
+      server = [
+        jupyter-server
+        jupyter-client
+        nbformat
+        ipykernel
+      ];
+    };
+
+    meta = {
+      description = "Pytest plugin for testing Jupyter core libraries and extensions";
+      homepage = "https://github.com/jupyter-server/pytest-jupyter";
+      license = lib.licenses.bsd3;
+      maintainers = [ ];
+    };
+  };
+in
+self
