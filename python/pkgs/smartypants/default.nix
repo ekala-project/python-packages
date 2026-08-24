c9e361618b12f51c23df4671b61689a107aa1be3
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:27 -0700

    python3Packages.smartypants: init at 2.0.2

---
+++ python/pkgs/smartypants/default.nix
@@ -0,0 +1,24 @@
+{ buildPythonPackage
+, fetchFromGitHub
+, isPyPy
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "smartypants";
+  version = "2.0.2";
+  pyproject = true;
+
+  disabled = isPyPy;
+
+  src = fetchFromGitHub {
+    owner = "leohemsted";
+    repo = "smartypants.py";
+    tag = "v${version}";
+    hash = "sha256-jSGiT36Rr0P6eEWZIHtMj4go3KGDRaF2spLxLNruDec=";
+  };
+
+  build-system = [ setuptools ];
+
+}
