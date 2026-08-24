16d1293d6a7560327586e6b0843ff3ab945e6cf3
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:35 -0700

    python3Packages.zope-hookable: init at 8.2

---
+++ python/pkgs/zope-hookable/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "zope-hookable";
+  version = "8.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "zopefoundation";
+    repo = "zope.hookable";
+    tag = version;
+    hash = "sha256-pryx55dzvg+6jSUj4avskTnGKe6w1HkEh6v6OOlHIXY=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "zope.hookable" ];
+
+  unittestFlagsArray = [ "src/zope/hookable/tests" ];
+
+  pythonNamespaces = [ "zope" ];
+
+  meta = {
+    description = "Supports the efficient creation of “hookable” objects";
+    homepage = "https://github.com/zopefoundation/zope.hookable";
+    license = lib.licenses.zpl21;
+  };
+}
