b1ca5a7af698b0160f77ede7aa13890175e41ffc
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:17 -0700

    python3Packages.pydantic-compat: init at 0.1.2

---
+++ python/pkgs/pydantic-compat/default.nix
@@ -0,0 +1,47 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, hatch-vcs
+, hatchling
+, gitMinimal
+, importlib-metadata
+, pydantic
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pydantic-compat";
+  version = "0.1.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pyapp-kit";
+    repo = "pydantic-compat";
+    tag = "v${version}";
+    leaveDotGit = true;
+    hash = "sha256-YJUfWu+nyGlwpJpxYghCKzj3CasdAaqYoNVCcfo/7YE=";
+  };
+
+  build-system = [
+    hatch-vcs
+    hatchling
+  ];
+
+  nativeBuildInputs = [
+    gitMinimal
+  ];
+
+  dependencies = [
+    importlib-metadata
+    pydantic
+  ];
+
+  pythonImportsCheck = [ "pydantic_compat" ];
+
+  meta = {
+    description = "Compatibility layer for pydantic v1/v2";
+    homepage = "https://github.com/pyapp-kit/pydantic-compat";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
