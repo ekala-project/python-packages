34bc1cb6b1cef8e6a5843f6ecd1a9beffc4e6906
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:20 -0700

    python3Packages.pypugjs: init at 7.0.0

---
+++ python/pkgs/pypugjs/default.nix
@@ -0,0 +1,46 @@
+{ lib
+, buildPythonPackage
+, charset-normalizer
+, fetchFromGitHub
+, poetry-core
+, six
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pypugjs";
+  version = "7.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "kakulukia";
+    repo = "pypugjs";
+    tag = "v${version}";
+    hash = "sha256-L4vE0INrSsjtMQ2Psx2YUDdfMoetGd1rFgmeP5bSUVo=";
+  };
+
+  build-system = [
+    poetry-core
+  ];
+
+  dependencies = [
+    six
+    charset-normalizer
+  ];
+
+  pythonRelaxDeps = [
+    "charset-normalizer"
+  ];
+
+  pythonImportsCheck = [
+    "pypugjs"
+  ];
+
+  meta = {
+    description = "PugJS syntax template adapter for Django, Jinja2, Mako and Tornado templates";
+    mainProgram = "pypugjs";
+    homepage = "https://github.com/kakulukia/pypugjs";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
