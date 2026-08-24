ccc15ceb71cd3ff2b3e76b41e307468b1f51db50
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:33 -0700

    python3Packages.wn: init at 0.14.0

---
+++ python/pkgs/wn/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, hatchling
+, httpx
+, tomli
+, starlette
+,
+}:
+
+buildPythonPackage rec {
+  pname = "wn";
+  version = "0.14.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-z2mDEFx7Qn5LKyji4CgFhxvCUblZeXLf2hjy4i6lMjQ=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    httpx
+    tomli
+  ];
+
+  optional-dependencies.web = [
+    starlette
+  ];
+
+  pythonImportsCheck = [ "wn" ];
+
+  meta = {
+    description = "Modern, interlingual wordnet interface for Python";
+    homepage = "https://github.com/goodmami/wn";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
