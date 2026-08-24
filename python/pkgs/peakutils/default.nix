20823930e0a783923ccad58e6ad1c8abe46f07d5
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:15 -0700

    python3Packages.peakutils: init at 1.3.5

---
+++ python/pkgs/peakutils/default.nix
@@ -0,0 +1,35 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, numpy
+, scipy
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "peakutils";
+  version = "1.3.5";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-T/Ln8zMLkwJP6Noe4E4AOJ4mvLLveb0vnPhszUli4RQ=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    numpy
+    scipy
+  ];
+
+  pythonImportsCheck = [ "peakutils" ];
+
+  meta = {
+    description = "Peak detection utilities for 1D data";
+    homepage = "https://pypi.org/project/PeakUtils/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
