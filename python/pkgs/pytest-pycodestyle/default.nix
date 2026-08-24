bcfd9dcde015693f1c0ca878e26743657e015bad
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:22 -0700

    python3Packages.pytest-pycodestyle: init at 2.5.0

---
+++ python/pkgs/pytest-pycodestyle/default.nix
@@ -0,0 +1,36 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pycodestyle
+, pytest
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pytest-pycodestyle";
+  version = "2.5.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "henry0312";
+    repo = "pytest-pycodestyle";
+    tag = "v${version}";
+    hash = "sha256-X/vacxI0RFHIqlZ2omzvvFDePS/SZYSFQHEmfcbvf/4=";
+  };
+
+  build-system = [ setuptools ];
+
+  buildInputs = [ pytest ];
+
+  dependencies = [ pycodestyle ];
+
+  pythonImportsCheck = [ "pytest_pycodestyle" ];
+
+  meta = {
+    description = "Pytest plugin to run pycodestyle";
+    homepage = "https://github.com/henry0312/pytest-pycodestyle";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
