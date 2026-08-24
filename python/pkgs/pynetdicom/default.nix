75dbf65e0a40e1d65a78854e28541029c7305e99
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:20 -0700

    python3Packages.pynetdicom: init at 3.0.4

---
+++ python/pkgs/pynetdicom/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, flit-core
+, pydicom
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pynetdicom";
+  version = "3.0.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pydicom";
+    repo = "pynetdicom";
+    tag = "v${version}";
+    hash = "sha256-4LISckHH+fVBmPcBr8rM62E6r3IkKAgdUneVHyc5Vm8=";
+  };
+
+  build-system = [ flit-core ];
+
+  dependencies = [ pydicom ];
+
+  pythonImportsCheck = [ "pynetdicom" ];
+
+  meta = {
+    description = "Python implementation of the DICOM networking protocol";
+    homepage = "https://github.com/pydicom/pynetdicom";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
