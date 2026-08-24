3c4c7acb3cc501b9cd1dcf4acc464bbb363488b2
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:14 -0700

    python3Packages.oslo-i18n: init at 6.9.0

---
+++ python/pkgs/oslo-i18n/default.nix
@@ -0,0 +1,37 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pbr
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "oslo-i18n";
+  version = "6.9.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "oslo_i18n";
+    inherit version;
+    hash = "sha256-V0vPIYc7GFBovOyVHeHsCTFY/9/wWoBV/Rjdy2n2nmU=";
+  };
+
+  postPatch = ''
+    # only a small portion of the listed packages are actually needed for running the tests
+    # so instead of removing them one by one remove everything
+    rm test-requirements.txt
+  '';
+
+  build-system = [
+    pbr
+    setuptools
+  ];
+
+  meta = {
+    description = "Oslo i18n library";
+    homepage = "https://github.com/openstack/oslo.i18n";
+    license = lib.licenses.asl20;
+    teams = [ lib.teams.openstack ];
+  };
+}
