eb78a6a0ffa22954f61e6cec98995734809280bc
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:31 -0700

    python3Packages.transmission-rpc: init at 7.0.11

---
+++ python/pkgs/transmission-rpc/default.nix
@@ -0,0 +1,37 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, requests
+, typing-extensions
+,
+}:
+
+buildPythonPackage rec {
+  pname = "transmission-rpc";
+  version = "7.0.11";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Trim21";
+    repo = "transmission-rpc";
+    tag = "v${version}";
+    hash = "sha256-t07TuLLHfbxvWh+7854OMigfGC8jHzvpd4QO3v0M15I=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    requests
+    typing-extensions
+  ];
+
+  pythonImportsCheck = [ "transmission_rpc" ];
+
+  meta = {
+    description = "Python module that implements the Transmission bittorent client RPC protocol";
+    homepage = "https://github.com/Trim21/transmission-rpc";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
