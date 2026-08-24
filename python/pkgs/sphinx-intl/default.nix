fb9bb4d80bf55fdd5acd788f9580cc5f225892f8
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:28 -0700

    python3Packages.sphinx-intl: init at 2.3.2

---
+++ python/pkgs/sphinx-intl/default.nix
@@ -0,0 +1,44 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, babel
+, click
+, setuptools
+, setuptools-scm
+, sphinx
+,
+}:
+
+buildPythonPackage rec {
+  pname = "sphinx-intl";
+  version = "2.3.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "sphinx-doc";
+    repo = "sphinx-intl";
+    tag = version;
+    hash = "sha256-5Ro+UG9pwwp656fYyCsna6P4s9Gb86Tu3Qm2WUI7tsE=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    babel
+    click
+    setuptools
+    sphinx
+  ];
+
+  pythonImportsCheck = [ "sphinx_intl" ];
+
+  meta = {
+    description = "Sphinx utility that make it easy to translate and to apply translation";
+    homepage = "https://github.com/sphinx-doc/sphinx-intl";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
