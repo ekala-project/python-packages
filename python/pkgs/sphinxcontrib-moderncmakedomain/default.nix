e4d67dd11cbb24fed49ffa3e7201f699e92cf833
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:28 -0700

    python3Packages.sphinxcontrib-moderncmakedomain: init at 3.29.0

---
+++ python/pkgs/sphinxcontrib-moderncmakedomain/default.nix
@@ -0,0 +1,32 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, hatchling
+, sphinx
+,
+}:
+
+buildPythonPackage rec {
+  pname = "sphinxcontrib-moderncmakedomain";
+  version = "3.29.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit version;
+    pname = "sphinxcontrib_moderncmakedomain";
+    hash = "sha256-NYfe8kH/JXfQu+8RgQoILp3sG3ij1LSgZiQLXz3BtbI=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [ sphinx ];
+
+  pythonNamespaces = [ "sphinxcontrib" ];
+
+  meta = {
+    description = "Sphinx extension which renders CMake documentation";
+    homepage = "https://github.com/scikit-build/moderncmakedomain";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
