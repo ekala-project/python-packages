f5d947d9ff284c03cc582bd37a37e8fd95421c79
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:17 -0700

    python3Packages.pyclibrary: init at 0.3.0

---
+++ python/pkgs/pyclibrary/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  setuptools
+, setuptools-scm
+, pyparsing
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "pyclibrary";
+  version = "0.3.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "MatthieuDartiailh";
+    repo = "pyclibrary";
+    tag = finalAttrs.version;
+    hash = "sha256-RyIbRySRWSZwKP5G6yXYCOnfKOV0165aPyjMf3nSbOM=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    pyparsing
+  ];
+
+  pythonImportsCheck = [ "pyclibrary" ];
+
+  meta = {
+    description = "C parser and ctypes automation for python";
+    homepage = "https://github.com/MatthieuDartiailh/pyclibrary";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
