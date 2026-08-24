b4394228effd89165a117f0baacd3fe3374b28f3
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:25 -0700

    python3Packages.rfc6555: init at 0.1.0

---
+++ python/pkgs/rfc6555/default.nix
@@ -0,0 +1,27 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+,
+}:
+
+buildPythonPackage rec {
+  pname = "rfc6555";
+  version = "0.1.0";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "sethmlarson";
+    repo = "rfc6555";
+    rev = "v${version}";
+    hash = "sha256-Lmwgusc4EQlF0GHmMTUxWzUCjBk19cvurNwbOnT+1jM=";
+  };
+
+  pythonImportsCheck = [ "rfc6555" ];
+
+  meta = {
+    description = "Python implementation of the Happy Eyeballs Algorithm";
+    homepage = "https://github.com/sethmlarson/rfc6555";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
