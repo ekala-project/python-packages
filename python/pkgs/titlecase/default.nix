c007c5c526ecb8683fcab37f428a144733717c3f
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:31 -0700

    python3Packages.titlecase: init at 2.4.1

---
+++ python/pkgs/titlecase/default.nix
@@ -0,0 +1,34 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, regex
+, setuptools-scm
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "titlecase";
+  version = "2.4.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ppannuto";
+    repo = "python-titlecase";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-s+C0UOKLEpMksfePIB6VzTv0dFLeamurdxjf5u1ek3g=";
+  };
+
+  build-system = [ setuptools-scm ];
+
+  dependencies = [ regex ];
+
+  pythonImportsCheck = [ "titlecase" ];
+
+  meta = {
+    description = "Python library to capitalize strings as specified by the New York Times";
+    mainProgram = "titlecase";
+    homepage = "https://github.com/ppannuto/python-titlecase";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
