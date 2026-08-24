b394e1b822663c2b870d4c667e389535670f1bb0
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:33 -0700

    python3Packages.verboselogs: init at 1.7

---
+++ python/pkgs/verboselogs/default.nix
@@ -0,0 +1,32 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "verboselogs";
+  version = "1.7";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "xolox";
+    repo = "python-verboselogs";
+    tag = finalAttrs.version;
+    hash = "sha256-hcIdbn0gdkdJ33KcOx6uv0iMXW0x+i880SoROi+qX4I=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "verboselogs" ];
+
+  meta = {
+    description = "Verbose logging for Python's logging module";
+    homepage = "https://github.com/xolox/python-verboselogs";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
