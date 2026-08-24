e39d2f8209df5db60f94ba49819923ad582748db
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:16 -0700

    python3Packages.podcastparser: init at 0.6.11

---
+++ python/pkgs/podcastparser/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "podcastparser";
+  version = "0.6.11";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "gpodder";
+    repo = "podcastparser";
+    tag = version;
+    hash = "sha256-eF/YHKSCMZnavkoX3LcAFHPSPABijn+aPVzaeRYY3WI=";
+  };
+
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "podcastparser" ];
+
+  meta = {
+    description = "Module to parse podcasts";
+    homepage = "http://gpodder.org/podcastparser/";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
