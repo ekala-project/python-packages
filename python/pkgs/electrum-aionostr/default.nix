ba7faeb961f3af9fc6d3dd244ccbe7e48350a60b
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:10 -0700

    python3Packages.electrum-aionostr: init at 0.1.0

---
+++ python/pkgs/electrum-aionostr/default.nix
@@ -0,0 +1,41 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, aiohttp
+, aiohttp-socks
+, aiorpcx
+, cryptography
+, electrum-ecc
+,
+}:
+
+buildPythonPackage rec {
+  pname = "electrum-aionostr";
+  version = "0.1.0";
+  pyproject = true;
+  build-system = [ setuptools ];
+
+  src = fetchPypi {
+    pname = "electrum_aionostr";
+    inherit version;
+    hash = "sha256-N3T46DEjiCcuEIUahpyfTT1KVNjVZIUcNuLcQCl77IQ=";
+  };
+
+  dependencies = [
+    aiohttp
+    aiohttp-socks
+    aiorpcx
+    cryptography
+    electrum-ecc
+  ];
+
+  pythonImportsCheck = [ "electrum_aionostr" ];
+
+  meta = {
+    description = "Asyncio nostr client";
+    homepage = "https://github.com/spesmilo/electrum-aionostr";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
