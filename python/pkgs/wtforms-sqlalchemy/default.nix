dafc3bfe0af0252ee171aaf89c864be38bbf0d9c
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:34 -0700

    python3Packages.wtforms-sqlalchemy: init at 0.4.2

---
+++ python/pkgs/wtforms-sqlalchemy/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, hatchling
+, sqlalchemy
+, wtforms
+,
+}:
+
+buildPythonPackage rec {
+  pname = "wtforms-sqlalchemy";
+  version = "0.4.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "wtforms";
+    repo = "wtforms-sqlalchemy";
+    tag = version;
+    hash = "sha256-E2F8lOcgne2yGEyn6g8j3mHr045eOyKu77DFGwWTPkc=";
+  };
+
+  build-system = [
+    hatchling
+  ];
+
+  dependencies = [
+    sqlalchemy
+    wtforms
+  ];
+
+  pythonImportsCheck = [ "wtforms_sqlalchemy" ];
+
+  meta = {
+    description = "WTForms integration for SQLAlchemy";
+    homepage = "https://github.com/wtforms/wtforms-sqlalchemy";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
