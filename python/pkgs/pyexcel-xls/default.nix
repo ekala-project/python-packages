0659a4078dfdb563c0c5acb75d1f93579b904224
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:18 -0700

    python3Packages.pyexcel-xls: init at 0.7.1

---
+++ python/pkgs/pyexcel-xls/default.nix
@@ -0,0 +1,42 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pyexcel-io
+, xlrd
+, xlwt
+, pyexcel
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pyexcel-xls";
+  version = "0.7.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pyexcel";
+    repo = "pyexcel-xls";
+    tag = "v${version}";
+    hash = "sha256-+iwdMSGUsUbWFO4s4+3Zf+47J9bzFffWthZoeThT8f0=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    pyexcel-io
+    xlrd
+    xlwt
+  ];
+
+  postPatch = ''
+    substituteInPlace setup.py --replace "xlrd<2" "xlrd<3"
+  '';
+
+  meta = {
+    description = "Wrapper library to read, manipulate and write data in xls using xlrd and xlwt";
+    homepage = "http://docs.pyexcel.org/";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
