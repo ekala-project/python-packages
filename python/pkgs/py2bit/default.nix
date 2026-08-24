1d0ff09d8dcc4f8c65eb379611c7fb0b3cd7023f
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:16 -0700

    python3Packages.py2bit: init at 0.3.3

---
+++ python/pkgs/py2bit/default.nix
@@ -0,0 +1,29 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, setuptools-scm
+,
+}:
+
+buildPythonPackage rec {
+  pname = "py2bit";
+  version = "0.3.3";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-Jk9b/DnXKfGsrVTHYKwE+oog1BhPS1BdnDM9LgMlN3A=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  meta = {
+    homepage = "https://github.com/deeptools/py2bit";
+    description = "File access to 2bit files";
+    license = lib.licenses.mit;
+  };
+}
