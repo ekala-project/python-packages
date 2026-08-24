45cf92917e6e125ba6515367f60d7eae794207ce
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:14 -0700

    python3Packages.oslo-serialization: init at 5.11.0

---
+++ python/pkgs/oslo-serialization/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, msgpack
+, oslo-utils
+, pbr
+, setuptools
+,
+}:
+
+buildPythonPackage rec {
+  pname = "oslo-serialization";
+  version = "5.11.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "oslo_serialization";
+    inherit version;
+    hash = "sha256-gyboWoCFbBBoAHQj/Pb+Kd0v5Xoy9dh//wEgtVW0tnw=";
+  };
+
+  build-system = [
+    pbr
+    setuptools
+  ];
+
+  dependencies = [
+    msgpack
+    oslo-utils
+  ];
+
+  pythonImportsCheck = [ "oslo_serialization" ];
+
+  meta = {
+    description = "Oslo Serialization library";
+    homepage = "https://github.com/openstack/oslo.serialization";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
