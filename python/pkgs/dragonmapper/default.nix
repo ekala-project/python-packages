a0bebc7633df08dda27e3f411c9b4e049b7e6198
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:10 -0700

    python3Packages.dragonmapper: init at 0.3.0

---
+++ python/pkgs/dragonmapper/default.nix
@@ -0,0 +1,37 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, hatchling
+, hanzidentifier
+, zhon
+,
+}:
+
+buildPythonPackage rec {
+  pname = "dragonmapper";
+  version = "0.3.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "tsroten";
+    repo = "dragonmapper";
+    tag = "v${version}";
+    hash = "sha256-3SRSu/9cpg2YcEuPFxBXg6KHgRSX5SiMAFbyE40m6ks=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    hanzidentifier
+    zhon
+  ];
+
+  pythonImportsCheck = [ "dragonmapper" ];
+
+  meta = {
+    description = "Identification and conversion functions for Chinese text processing";
+    homepage = "https://github.com/tsroten/dragonmapper";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
