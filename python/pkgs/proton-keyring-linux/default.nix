58cf8370c039f3ae5f22067136f5a42695cd9325
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:16 -0700

    python3Packages.proton-keyring-linux: init at 0.2.3

---
+++ python/pkgs/proton-keyring-linux/default.nix
@@ -0,0 +1,41 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, keyring
+, proton-core
+,
+}:
+
+buildPythonPackage rec {
+  pname = "proton-keyring-linux";
+  version = "0.2.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ProtonVPN";
+    repo = "python-proton-keyring-linux";
+    tag = "v${version}";
+    hash = "sha256-Oj0jcoJJGTh/eIE5byS4XohnvfnzlYWkozu9VJJH2vk=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    keyring
+    proton-core
+  ];
+
+  pythonImportsCheck = [
+    "proton.keyring_linux.core"
+    "proton.keyring_linux"
+  ];
+
+  meta = {
+    description = "ProtonVPN core component to access Linux's keyring";
+    homepage = "https://github.com/ProtonVPN/python-proton-keyring-linux";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
+}
