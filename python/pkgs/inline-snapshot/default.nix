959deefb06a7216d2c70c410e061354324e4a8b6
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:12 -0700

    python3Packages.inline-snapshot: init at 0.34.2

---
+++ python/pkgs/inline-snapshot/default.nix
@@ -0,0 +1,53 @@
+{ lib
+, asttokens
+, black
+, buildPythonPackage
+, dirty-equals
+, executing
+, fetchFromGitHub
+, hatchling
+, pytest
+, rich
+, typing-extensions
+,
+}:
+
+buildPythonPackage rec {
+  pname = "inline-snapshot";
+  version = "0.34.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "15r10nk";
+    repo = "inline-snapshot";
+    tag = version;
+    hash = "sha256-4Uvc925/6RxJRHjP3SZaB7T+gqky5KlL9agHy/14Jd0=";
+  };
+
+  build-system = [ hatchling ];
+
+  buildInputs = [
+    pytest
+  ];
+
+  dependencies = [
+    asttokens
+    executing
+    rich
+    typing-extensions
+  ];
+
+  optional-dependencies = {
+    black = [ black ];
+    dirty-equals = [ dirty-equals ];
+  };
+
+  pythonImportsCheck = [ "inline_snapshot" ];
+
+  meta = {
+    description = "Create and update inline snapshots in Python tests";
+    homepage = "https://github.com/15r10nk/inline-snapshot/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
