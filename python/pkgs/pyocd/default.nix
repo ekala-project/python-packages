482747285c30881325871f28303a264569f8dfb7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:20 -0700

    python3Packages.pyocd: init at 0.42.0

---
+++ python/pkgs/pyocd/default.nix
@@ -0,0 +1,68 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  setuptools-scm
+, # dependencies
+  capstone
+, cmsis-pack-manager
+, colorama
+, importlib-metadata
+, importlib-resources
+, intelhex
+, intervaltree
+, lark
+, natsort
+, prettytable
+, pyelftools
+, pylink-square
+, pyusb
+, pyyaml
+, typing-extensions
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pyocd";
+  version = "0.42.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pyocd";
+    repo = "pyOCD";
+    tag = "v${version}";
+    hash = "sha256-VSEItt+mXiV3u3SAKQc8uGiJdT6b4nER/u6BwfaX2CM=";
+  };
+
+  pythonRelaxDeps = [ "capstone" ];
+  pythonRemoveDeps = [ "libusb-package" ];
+
+  build-system = [ setuptools-scm ];
+
+  dependencies = [
+    capstone
+    cmsis-pack-manager
+    colorama
+    importlib-metadata
+    importlib-resources
+    intelhex
+    intervaltree
+    lark
+    natsort
+    prettytable
+    pyelftools
+    pylink-square
+    pyusb
+    pyyaml
+    typing-extensions
+  ];
+  pythonImportsCheck = [ "pyocd" ];
+
+  meta = {
+    description = "Python library for programming and debugging Arm Cortex-M microcontrollers";
+    downloadPage = "https://github.com/pyocd/pyOCD";
+    homepage = "https://pyocd.io";
+    license = lib.licenses.asl20;
+    maintainers = [ ];
+  };
+}
