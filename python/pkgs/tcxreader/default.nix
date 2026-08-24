6da936106d1d0ad5edc2c51b99c49894cf5a9bfa
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:30 -0700

    python3Packages.tcxreader: init at 0.4.11

---
+++ python/pkgs/tcxreader/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, poetry-core
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "tcxreader";
+  version = "0.4.11";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "alenrajsp";
+    repo = "tcxreader";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Iz0VQSukF5CI8lKaxKU4HEmU+n0EbQkuKmduOfsZ/GM=";
+  };
+
+  nativeBuildInputs = [ poetry-core ];
+
+  pythonImportsCheck = [ "tcxreader" ];
+
+  meta = {
+    description = "Reader for Garmin’s TCX file format";
+    homepage = "https://github.com/alenrajsp/tcxreader";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
