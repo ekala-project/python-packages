7bbcfc3c80e86a19e74aa382615332df2539c530
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:23 -0700

    python3Packages.python-ly: init at 0.9.10

---
+++ python/pkgs/python-ly/default.nix
@@ -0,0 +1,34 @@
+{ buildPythonPackage
+, fetchFromGitHub
+, hatchling
+, lib
+, versionCheckHook
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "python-ly";
+  version = "0.9.10";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "frescobaldi";
+    repo = "python-ly";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-diLg1rU+SmCutW1WJQtMJvpipU+k8GluvAqFfcv1GS4=";
+  };
+
+  pythonImportsCheck = [ "ly" ];
+
+  build-system = [ hatchling ];
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  meta = {
+    description = "Tool and library for manipulating LilyPond files";
+    homepage = "https://pypi.org/project/python-ly";
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ ];
+    mainProgram = "ly";
+  };
+})
