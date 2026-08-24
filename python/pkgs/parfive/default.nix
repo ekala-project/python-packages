25aea7398e361b739096441374f51cf0170a277f
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:15 -0700

    python3Packages.parfive: init at 2.3.1

---
+++ python/pkgs/parfive/default.nix
@@ -0,0 +1,47 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  setuptools-scm
+, # dependencies
+  aiohttp
+, # optional dependencies
+  aioftp
+, # tests
+  tqdm
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "parfive";
+  version = "2.3.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Cadair";
+    repo = "parfive";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-i9B860A27KDUJKlE/eQNiGVPEPvnmvmNqMjjdOeBcyY=";
+  };
+
+  build-system = [ setuptools-scm ];
+
+  dependencies = [
+    aiohttp
+    tqdm
+  ];
+
+  optional-dependencies = {
+    ftp = [ aioftp ];
+  };
+
+  pythonImportsCheck = [ "parfive" ];
+
+  meta = {
+    description = "HTTP and FTP parallel file downloader";
+    mainProgram = "parfive";
+    homepage = "https://parfive.readthedocs.io/";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+})
