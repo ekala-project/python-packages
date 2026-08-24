d2d6b26f9d33950796bdec7db83b36839666d1f3
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:23 -0700

    python3Packages.python-gvm: init at 26.11.1

---
+++ python/pkgs/python-gvm/default.nix
@@ -0,0 +1,43 @@
+{ lib
+, buildPythonPackage
+, defusedxml
+, fetchFromGitHub
+, lxml
+, paramiko
+, poetry-core
+, typing-extensions
+,
+}:
+
+buildPythonPackage (finalAttrs: {
+  pname = "python-gvm";
+  version = "26.11.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "greenbone";
+    repo = "python-gvm";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-NTUDFZnDavHhl5AELMNj8AkwwVtY+96cMB9uhm4veQg=";
+  };
+
+  build-system = [ poetry-core ];
+
+  pythonRelaxDeps = [ "defusedxml" ];
+
+  dependencies = [
+    defusedxml
+    lxml
+    paramiko
+    typing-extensions
+  ];
+
+  pythonImportsCheck = [ "gvm" ];
+
+  meta = {
+    description = "Collection of APIs that help with remote controlling a Greenbone Security Manager";
+    homepage = "https://github.com/greenbone/python-gvm";
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ ];
+  };
+})
