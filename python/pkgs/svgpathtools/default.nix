de060db07214fc2773baf5bd415deec95aede408
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:30 -0700

    python3Packages.svgpathtools: init at 1.7.2

---
+++ python/pkgs/svgpathtools/default.nix
@@ -0,0 +1,45 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, # build-system
+  setuptools
+, # dependencies
+  numpy
+, scipy
+, svgwrite
+,
+}:
+
+buildPythonPackage rec {
+  pname = "svgpathtools";
+  version = "1.7.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "mathandy";
+    repo = "svgpathtools";
+    tag = "v${version}";
+    hash = "sha256-OGengjPIEuxDYHqzFUBbYcVs9RjBSKSd1NNjx/KqnSk=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
+    numpy
+    scipy
+    svgwrite
+  ];
+
+  pythonImportsCheck = [
+    "svgpathtools"
+  ];
+
+  meta = {
+    description = "Collection of tools for manipulating and analyzing SVG Path objects and Bezier curves";
+    homepage = "https://github.com/mathandy/svgpathtools";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
