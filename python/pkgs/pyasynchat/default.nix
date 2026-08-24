d43f793f4a931e85b31674c88378febeba5da936
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:17 -0700

    python3Packages.pyasynchat: init at 1.0.5

---
+++ python/pkgs/pyasynchat/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, pyasyncore
+,
+}:
+
+buildPythonPackage rec {
+  pname = "pyasynchat";
+  version = "1.0.5";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "simonrob";
+    repo = "pyasynchat";
+    rev = "v${version}";
+    hash = "sha256-KJmUou1llxUhDrMCOpJxqYNnPpJ0OoQv5VwYs/PJXbs=";
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
+    pyasyncore
+  ];
+
+  pythonImportsCheck = [
+    "asynchat"
+  ];
+
+  meta = {
+    description = "Make asynchat available for Python 3.12 onwards";
+    homepage = "https://github.com/simonrob/pyasynchat";
+    license = lib.licenses.psfl;
+    maintainers = [ ];
+  };
+}
