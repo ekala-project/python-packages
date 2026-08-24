ed5286746f74b1fcd6b98023f7e8c2f9ef20db38
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:25 -0700

    python3Packages.rpi-gpio: init at 0.7.1

---
+++ python/pkgs/rpi-gpio/default.nix
@@ -0,0 +1,27 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+,
+}:
+
+buildPythonPackage rec {
+  pname = "rpi-gpio";
+  version = "0.7.1";
+  format = "setuptools";
+
+  src = fetchPypi {
+    pname = "RPi.GPIO";
+    inherit version;
+    hash = "sha256-zWHEsDw3tiu6SlrP6phidJwzxhjgKV5+kKpHE/s3O3A=";
+  };
+
+  # Raspberry Pi
+
+  meta = {
+    homepage = "https://sourceforge.net/p/raspberry-gpio-python";
+    description = "Python module to control the GPIO on a Raspberry Pi";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    maintainers = [ ];
+  };
+}
