4154d78c68a3d52c15afb2e487aaa4ad08cbb338
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:10 -0700

    python3Packages.fingerprints: init at 1.3.1

---
+++ python/pkgs/fingerprints/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, hatchling
+, normality
+,
+}:
+
+buildPythonPackage rec {
+  pname = "fingerprints";
+  version = "1.3.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "alephdata";
+    repo = "fingerprints";
+    tag = version;
+    hash = "sha256-Q+XCsuGMHPtOqB0SauVuYInR5FGMuG6aNhqiAwTJvSI=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [ normality ];
+
+  pythonImportsCheck = [ "fingerprints" ];
+
+  meta = {
+    description = "Library to generate entity fingerprints";
+    homepage = "https://github.com/alephdata/fingerprints";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
