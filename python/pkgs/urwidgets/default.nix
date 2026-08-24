7c66bbd63e1169c612c8a1a6cb1649794862059f
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:32 -0700

    python3Packages.urwidgets: init at 0.2.1

---
+++ python/pkgs/urwidgets/default.nix
@@ -0,0 +1,35 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, urwid
+,
+}:
+
+buildPythonPackage rec {
+  pname = "urwidgets";
+  version = "0.2.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "AnonymouX47";
+    repo = "urwidgets";
+    tag = "v${version}";
+    hash = "sha256-RgY7m0smcdUspGkCdzepxruEMDq/mAsVFNjHMLoWAyc=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ urwid ];
+
+  pythonRelaxDeps = [ "urwid" ];
+
+  pythonImportsCheck = [ "urwidgets" ];
+
+  meta = {
+    description = "Collection of widgets for urwid";
+    homepage = "https://github.com/AnonymouX47/urwidgets";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
