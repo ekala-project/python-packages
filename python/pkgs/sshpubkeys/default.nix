9b9c7100bf4b9d2e02c5e8fd8c15d67945aa3a01
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:29 -0700

    python3Packages.sshpubkeys: init at 3.3.1

---
+++ python/pkgs/sshpubkeys/default.nix
@@ -0,0 +1,36 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, cryptography
+,
+}:
+
+buildPythonPackage rec {
+  version = "3.3.1";
+  pname = "sshpubkeys";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ojarva";
+    repo = "python-sshpubkeys";
+    # https://github.com/ojarva/python-sshpubkeys/issues/94
+    tag = "v3.2.0";
+    hash = "sha256-2OJatnQuCt9XQ797F5nEmgEZl5/tu9lrAry5yBGW61g=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    cryptography
+  ];
+
+  pythonImportsCheck = [ "sshpubkeys" ];
+
+  meta = {
+    description = "OpenSSH Public Key Parser for Python";
+    homepage = "https://github.com/ojarva/python-sshpubkeys";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+  };
+}
