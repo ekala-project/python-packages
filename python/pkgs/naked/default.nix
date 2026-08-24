3be9f31f44463e6eb6cadbb220be251d23ed0eae
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:13 -0700

    python3Packages.naked: init at 0.1.32

---
+++ python/pkgs/naked/default.nix
@@ -0,0 +1,51 @@
+{ buildPythonPackage
+, python
+, fetchFromGitHub
+, lib
+, requests
+, pyyaml
+, setuptools
+, wheel
+,
+}:
+
+buildPythonPackage rec {
+  pname = "naked";
+  version = "0.1.32";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "chrissimpkins";
+    repo = "naked";
+    rev = "v${version}";
+    hash = "sha256-KhygnURFggvUTR9wwWtORtfQES8ANd5sIaCONvIhfRM=";
+  };
+
+  postPatch = ''
+    # fix hardcoded absolute paths
+    substituteInPlace tests/test_SYSTEM*.py \
+      --replace-fail /Users/ces/Desktop/code/naked/tests/ "$PWD"/tests/
+    substituteInPlace lib/Naked/toolshed/c/*.c \
+      --replace-fail /Users/ces/Desktop/code/naked/lib/ $out/${python.sitePackages}/
+  '';
+
+  nativeBuildInputs = [
+    wheel
+    setuptools
+  ];
+
+  propagatedBuildInputs = [
+    requests
+    pyyaml
+  ];
+
+  pythonImportsCheck = [ "Naked" ];
+
+  meta = {
+    description = "Python command line application framework";
+    homepage = "https://github.com/chrissimpkins/naked";
+    downloadPage = "https://github.com/chrissimpkins/naked/tags";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
