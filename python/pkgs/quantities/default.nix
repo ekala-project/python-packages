b2575f94d29f5599f489524d29ffc7a8e1fb92f7
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:25 -0700

    python3Packages.quantities: init at 0.16.4

---
+++ python/pkgs/quantities/default.nix
@@ -0,0 +1,37 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, numpy
+, setuptools
+, setuptools-scm
+,
+}:
+
+buildPythonPackage rec {
+  pname = "quantities";
+  version = "0.16.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "python-quantities";
+    repo = "python-quantities";
+    tag = "v${version}";
+    hash = "sha256-a+UtNvcnQr4z87tpidx99u46M2H+EKtQ1EzIG5zQnmI=";
+  };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [ numpy ];
+
+  pythonImportsCheck = [ "quantities" ];
+
+  meta = {
+    description = "Quantities is designed to handle arithmetic and conversions of physical quantities";
+    homepage = "https://python-quantities.readthedocs.io/";
+    license = lib.licenses.bsd2;
+    maintainers = [ ];
+  };
+}
