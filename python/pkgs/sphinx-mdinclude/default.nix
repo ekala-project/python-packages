66779e25c4c7bb2b85ed14411b9aa94885160a6f
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:29 -0700

    python3Packages.sphinx-mdinclude: init at 0.6.2

---
+++ python/pkgs/sphinx-mdinclude/default.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, # build-system
+  flit-core
+, # dependencies
+  docutils
+, mistune
+, pygments
+, sphinx
+,
+}:
+
+buildPythonPackage rec {
+  pname = "sphinx-mdinclude";
+  version = "0.6.2";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "sphinx_mdinclude";
+    inherit version;
+    hash = "sha256-RHRi6Cy4vmFASiIEIn+SB2nrkj0vV2COMyXzu4goa0w=";
+  };
+
+  nativeBuildInputs = [ flit-core ];
+
+  propagatedBuildInputs = [
+    docutils
+    mistune
+    pygments
+    sphinx
+  ];
+
+  meta = {
+    homepage = "https://github.com/omnilib/sphinx-mdinclude";
+    description = "Sphinx extension for including or writing pages in Markdown format";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+  };
+}
