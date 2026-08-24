01ba441bc3cb93371395d4ae079cbd985e93c3b9
Jonathan Ringer <jonringer117@gmail.com>
2026-08-24 13:20:31 -0700

    python3Packages.toml-adapt: init at 0.3.4

---
+++ python/pkgs/toml-adapt/default.nix
@@ -0,0 +1,38 @@
+{ lib
+, buildPythonPackage
+, click
+, fetchFromGitHub
+, poetry-core
+, toml
+,
+}:
+
+buildPythonPackage rec {
+  pname = "toml-adapt";
+  version = "0.3.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "firefly-cpp";
+    repo = "toml-adapt";
+    tag = version;
+    hash = "sha256-GtwE8P4uP3F6wOrzv/vZ4CJR4tzF7CxpWV/8X/hBZhc=";
+  };
+
+  nativeBuildInputs = [ poetry-core ];
+
+  propagatedBuildInputs = [
+    click
+    toml
+  ];
+
+  pythonImportsCheck = [ "toml_adapt" ];
+
+  meta = {
+    description = "Simple Command-line interface for manipulating toml files";
+    homepage = "https://github.com/firefly-cpp/toml-adapt";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "toml-adapt";
+  };
+}
