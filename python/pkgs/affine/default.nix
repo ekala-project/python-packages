1dc40addead049ea9df5d99d290bfda6b0d57a39
Jonathan Ringer <jonringer117@gmail.com>
2026-09-05 06:03:19 -0700

    affine: 2.4.0 -> 3.0.1

--- python/pkgs/affine/default.nix
+++ python/pkgs/affine/default.nix
@@ -3,19 +3,22 @@
   buildPythonPackage,
   fetchPypi,
   flit-core,
+  attrs,
 }:
 
 buildPythonPackage rec {
   pname = "affine";
-  version = "2.4.0";
+  version = "3.0.1";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ok2BjWqDbBMZdtIvjCe408oy0K9kwdjSnet7r6TaHuo=";
+    hash = "sha256-4bPDjF1NPvUCShgqbRvx4MUasiGCV4HHQa600MB5p+I=";
   };
 
   build-system = [ flit-core ];
+
+  dependencies = [ attrs ];
 
   pythonImportsCheck = [ "affine" ];
 
