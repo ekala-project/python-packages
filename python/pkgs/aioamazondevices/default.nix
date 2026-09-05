d1d66e06e7ba918c2e472ae5677c58307c08ed3b
Jonathan Ringer <jonringer117@gmail.com>
2026-09-05 06:15:18 -0700

    aioamazondevices: 14.2.0 -> 15.1.0

--- python/pkgs/aioamazondevices/default.nix
+++ python/pkgs/aioamazondevices/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "aioamazondevices";
-  version = "14.2.0";
+  version = "15.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "chemelli74";
     repo = "aioamazondevices";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-KP1Rc+8c5RNTtCmuNjbjVhNEElwMKOGvumdWF+BIn0I=";
+    hash = "sha256-sx10iI7k5nMt6OmgLrNcEmQDOoPEuCOiSYAL5ctc+Xs=";
   };
 
   build-system = [ poetry-core ];
