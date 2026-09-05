b46410e6f70966161f02388224fa5bb32b126a00
Jonathan Ringer <jonringer117@gmail.com>
2026-09-05 08:59:22 -0700

    asyncmy: 0.2.11 -> 0.2.14

--- python/pkgs/asyncmy/default.nix
+++ python/pkgs/asyncmy/default.nix
@@ -9,15 +9,17 @@
 
 buildPythonPackage rec {
   pname = "asyncmy";
-  version = "0.2.11";
+  version = "0.2.14";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "long2ice";
     repo = "asyncmy";
     tag = "v${version}";
-    hash = "sha256-+6sXXlSAZr09nPuJtZH8S2UfijFthDpRA/AdGOyIfxU=";
+    hash = "sha256-bUpuFHfHW5mDtcbLk7Ro2gGq48sw3v3adY8tQ23e460=";
   };
+
+  requiredSystemFeatures = [ "big-parallel" ];
 
   nativeBuildInputs = [
     cython
