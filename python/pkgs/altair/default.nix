55974f1bcc8e4d44ad71ab4f066982b61713e3e8
Jonathan Ringer <jonringer117@gmail.com>
2026-09-05 07:04:04 -0700

    altair: 6.1.0 -> 6.2.2

--- python/pkgs/altair/default.nix
+++ python/pkgs/altair/default.nix
@@ -6,6 +6,7 @@
 
   # build-system
   hatchling,
+  versioningit,
 
   # dependencies
   jinja2,
@@ -20,17 +21,20 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "altair";
-  version = "6.1.0";
+  version = "6.2.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "altair-viz";
     repo = "altair";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-6507I+4ItjZezbprKn4r/oeaS1U7CF8+JKkHRmDC8js=";
+    hash = "sha256-rq9xs2Otqx38vtAMO6PRjvmIW5HKRuuAX15X96XDBOU=";
   };
 
-  build-system = [ hatchling ];
+  build-system = [
+    hatchling
+    versioningit
+  ];
 
   dependencies = [
     jinja2
