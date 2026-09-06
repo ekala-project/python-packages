a0e5688eb75be5039aa28fef786b475d9ce5249b
Jonathan Ringer <jonringer117@gmail.com>
2026-09-06 12:24:58 -0700

    awkward: 2.12.0 -> 2.13.0

--- python/pkgs/awkward/default.nix
+++ python/pkgs/awkward/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "awkward";
-  version = "2.12.0";
+  version = "2.13.0";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -22,7 +22,7 @@ buildPythonPackage (finalAttrs: {
     owner = "scikit-hep";
     repo = "awkward";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-X5fwX4HCMPRaYEIEf5cgGWUodtb6eEmU7ue6mjCzVSA=";
+    hash = "sha256-z6/OoWLB5VqNe7iciiemXFp+RnvCLHn4cOA6zS3t6BE=";
   };
 
   build-system = [
