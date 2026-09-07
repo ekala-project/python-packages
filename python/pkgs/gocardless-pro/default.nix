a5354f204be30b449ee48e50df1134891b1e7b22
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 08:08:27 -0700

    gocardless-pro: 3.3.0 -> 3.7.2

--- python/pkgs/gocardless-pro/default.nix
+++ python/pkgs/gocardless-pro/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "gocardless-pro";
-  version = "3.3.0";
+  version = "3.7.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "gocardless";
     repo = "gocardless-pro-python";
     tag = "v${version}";
-    hash = "sha256-XD5GUiSHTq/DLrKo6FY4moNnbFpXkVJWM13Yu6c+tZw=";
+    hash = "sha256-SSmyrFHDwmxmgt1D5XurFY1mvw3V2Q7AOr4JnYKJHC8=";
   };
 
   build-system = [ setuptools ];
