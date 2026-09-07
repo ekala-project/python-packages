8607afc0e5769e8e690fe8b70f4fecdd015c4ef9
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 12:43:20 -0700

    imagesize: 2.0.0 -> 2.0.1

--- python/pkgs/imagesize/default.nix
+++ python/pkgs/imagesize/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "imagesize";
-  version = "2.0.0";
-  format = "setuptools";
+  version = "2.0.1";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-joNYxKBcME8fzPf/lvA25yQ6GJ6eQukIUZk8VYz+nuM=";
+    hash = "sha256-srpqTepIen681TJI00dqykSdMNsSot3l4MXKliT9d+U=";
   };
+
+  build-system = [ setuptools ];
 
   meta = {
     description = "Getting image size from png/jpeg/jpeg2000/gif file";
