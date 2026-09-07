ca3b4d413bae259fad75a62869afc4b641bdd1e7
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 12:42:01 -0700

    imageio: 2.37.2 -> 2.37.4

--- python/pkgs/imageio/default.nix
+++ python/pkgs/imageio/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "imageio";
-  version = "2.37.2";
+  version = "2.37.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "imageio";
     repo = "imageio";
     tag = "v${version}";
-    hash = "sha256-8wKTcmnep67zBMYgd6Gpr3wRCIrzYaqfytL1o7iBNAk=";
+    hash = "sha256-7UiKj/PH3eVa8Qa8zz5FJ2RmFHcL19THwSLY51tdy2I=";
   };
 
   build-system = [ setuptools ];
