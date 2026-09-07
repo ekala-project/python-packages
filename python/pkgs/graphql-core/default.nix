c8c80cf141ddead80482a1bebea83906be6411ee
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 08:46:21 -0700

    graphql-core: 3.2.7 -> 3.2.12

--- python/pkgs/graphql-core/default.nix
+++ python/pkgs/graphql-core/default.nix
@@ -7,19 +7,19 @@
 
 buildPythonPackage rec {
   pname = "graphql-core";
-  version = "3.2.7";
+  version = "3.2.12";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "graphql-python";
     repo = "graphql-core";
     tag = "v${version}";
-    hash = "sha256-ag8yFf6254dX2xNZMKtVBW5QtI5JOZjzgcZveuoeAss=";
+    hash = "sha256-KzPjD/5HGPI2lrsCfW8aocXMhXc7YB7N+WUukc9Ru3s=";
   };
 
   postPatch = ''
     substituteInPlace pyproject.toml \
-      --replace-fail ', "setuptools>=59,<81"' ""
+      --replace-fail ', "setuptools>=59,<85"' ""
 
     # avoid big pytest-benchmark dependency
     substituteInPlace setup.cfg \
