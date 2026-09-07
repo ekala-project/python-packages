b6a6df0bc9f2f26d290b677bc2ea3ed242fa09a9
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 16:48:20 -0700

    llama-cloud: 2.11.0 -> 2.15.0

--- python/pkgs/llama-cloud/default.nix
+++ python/pkgs/llama-cloud/default.nix
@@ -24,13 +24,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "llama-cloud";
-  version = "2.11.0";
+  version = "2.15.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "llama_cloud";
     inherit (finalAttrs) version;
-    hash = "sha256-vvyYS5vxN2cMEIEAy82qd1PCxh/TYcDOL6YivkjN9c0=";
+    hash = "sha256-FBuw/s1ATz2CbZmYp0yWeMEBQzt3np3GjwuWV9hi9MQ=";
   };
 
   postPatch = ''
