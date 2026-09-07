cb30d58dd294c2ecae48d9d50ba21b3581c3f23c
Jonathan Ringer <jonringer117@gmail.com>
2026-09-06 23:51:01 -0700

    biopython: 1.86 -> 1.88

--- python/pkgs/biopython/default.nix
+++ python/pkgs/biopython/default.nix
@@ -9,21 +9,13 @@
 
 buildPythonPackage rec {
   pname = "biopython";
-  version = "1.86";
+  version = "1.88";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-k6ULWGpNLOxoqy+Z0D71g8V2HY+6VTXLjoHaeB0Nkv8=";
+    hash = "sha256-mqoxwL2k0Fn3su4Av9tcu3Ot4wV6pLc3p8wBhwkdBxo=";
   };
-
-  patches = [
-    # Numpy 2.4 compatibility
-    (fetchpatch {
-      url = "https://github.com/biopython/biopython/pull/5161.patch";
-      hash = "sha256-oN0nNlhvshIgNrmm+tIeCAJx1U/OqhdL4tj51DV2CHU=";
-    })
-  ];
 
   build-system = [ setuptools ];
 
