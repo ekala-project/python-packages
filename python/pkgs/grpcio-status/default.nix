4685f83222f32053945669392c0e25a8b2fef6a6
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 09:02:07 -0700

    grpcio-status: 1.83.0 -> 1.83.1

--- python/pkgs/grpcio-status/default.nix
+++ python/pkgs/grpcio-status/default.nix
@@ -9,13 +9,13 @@
 
 buildPythonPackage rec {
   pname = "grpcio-status";
-  version = "1.83.0";
+  version = "1.83.1";
   format = "setuptools";
 
   src = fetchPypi {
     pname = "grpcio_status";
     inherit version;
-    hash = "sha256-g3IZxt6a/cy29vcrNLxx4VGiAR7wQEDj+qynRqV+VK4=";
+    hash = "sha256-wIyNVT1quW7/rh2SODneIpJvWjFqlC9IpIBA4EfFF68=";
   };
 
   postPatch = ''
