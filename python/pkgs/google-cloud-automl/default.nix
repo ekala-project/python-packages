eb200a591645207bc590878b2ae1b89ac4bec6fb
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 08:20:02 -0700

    google-cloud-automl: 2.20.0 -> 2.20.1

--- python/pkgs/google-cloud-automl/default.nix
+++ python/pkgs/google-cloud-automl/default.nix
@@ -13,13 +13,13 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-automl";
-  version = "2.20.0";
+  version = "2.20.1";
   pyproject = true;
 
   src = fetchPypi {
     pname = "google_cloud_automl";
     inherit version;
-    hash = "sha256-3BaZODxm3KaDecL6iM0JQz9mOwaOvOTnb1HJ8sCOzEA=";
+    hash = "sha256-PIZ21kxcBgQXcJL77jNzy04qfaNsfBYy6WNAhIN+Bk0=";
   };
 
   build-system = [ setuptools ];
