8bd8361813fa69df38d469d9eadb40911f8595a7
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 02:24:11 -0700

    cli-helpers: 2.15.0 -> 2.15.1

--- python/pkgs/cli-helpers/default.nix
+++ python/pkgs/cli-helpers/default.nix
@@ -9,13 +9,13 @@
 
 buildPythonPackage rec {
   pname = "cli-helpers";
-  version = "2.15.0";
+  version = "2.15.1";
   pyproject = true;
 
   src = fetchPypi {
     pname = "cli_helpers";
     inherit version;
-    hash = "sha256-wsWhpf1qitUbG5noBoXNBMAGsOKcrPTCSxuopEDqjU0=";
+    hash = "sha256-6cCCbdooVXRetjs/2OM7asiIEYjyupHlGlFu2DP8DLg=";
   };
 
   build-system = [ setuptools ];
