cdc7f4c406f06509112048b6af1e073c81ccafb6
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 09:11:58 -0700

    guntamatic: 1.9.2 -> 1.12.0

--- python/pkgs/guntamatic/default.nix
+++ python/pkgs/guntamatic/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "guntamatic";
-  version = "1.9.2";
+  version = "1.12.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "JensTimmerman";
     repo = "guntamatic";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-cm3aFIRnWFKgkaEYDQCGSREZRmGhv0ltKMpkWHu+ugI=";
+    hash = "sha256-fpWnlu6t09gfKWw+uYahyKJt6wbXNDN5ILaB2oWum5o=";
   };
 
   build-system = [ setuptools ];
