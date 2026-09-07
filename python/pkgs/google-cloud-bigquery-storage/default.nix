e3dd4bcb1125a6fd641ff86315c671636c2e4b6f
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 08:22:26 -0700

    google-cloud-bigquery-storage: 2.39.0 -> 2.41.0

--- python/pkgs/google-cloud-bigquery-storage/default.nix
+++ python/pkgs/google-cloud-bigquery-storage/default.nix
@@ -12,13 +12,13 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-bigquery-storage";
-  version = "2.39.0";
+  version = "2.41.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "google_cloud_bigquery_storage";
     inherit version;
-    hash = "sha256-1a/ZCtBs8k2RZzFsynCrWzROiA/BMDHXOSqnjudri7Y=";
+    hash = "sha256-uZkpXwjOfi1qJg0kW1uYQ7oWA/YWRRXJ8Mtdge5xpdI=";
   };
 
   build-system = [ setuptools ];
