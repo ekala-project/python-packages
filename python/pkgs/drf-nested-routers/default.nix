2a680c92ff595934a1d21d445753b85a7a3256a2
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 05:21:00 -0700

    drf-nested-routers: 0.95.0 -> 0.95.3

--- python/pkgs/drf-nested-routers/default.nix
+++ python/pkgs/drf-nested-routers/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
   django,
   djangorestframework,
   pytest-django,
@@ -10,19 +11,19 @@
 
 buildPythonPackage rec {
   pname = "drf-nested-routers";
-  version = "0.95.0";
-  format = "setuptools";
+  version = "0.95.3";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "alanjds";
     repo = "drf-nested-routers";
     tag = "v${version}";
-    hash = "sha256-9oB6pmhZJVvVJeueY44q9ST1JgjmK1FF8QMx7mX5ZFI=";
+    hash = "sha256-09yMZW2dvXHQ8oak5skWQKCzqn9pcsgXpkQAkfR1lyA=";
   };
 
-  buildInputs = [ django ];
+  build-system = [ setuptools ];
 
-  propagatedBuildInputs = [ djangorestframework ];
+  dependencies = [ djangorestframework ];
   meta = {
     homepage = "https://github.com/alanjds/drf-nested-routers";
     description = "Provides routers and fields to create nested resources in the Django Rest Framework";
