9b5cf9a6ff9e2223450aaa52224cbce1eca24388
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 03:48:55 -0700

    dbutils: 3.1.2 -> 3.2.0

--- python/pkgs/dbutils/default.nix
+++ python/pkgs/dbutils/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "dbutils";
-  version = "3.1.2";
+  version = "3.2.0";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-FgtXiBVPGt7dxhCA2v8VMLTfK6DUWvHDv7rHbbJBhrM=";
+    hash = "sha256-3+P16244MELWitB+Tpd4squ8xGJ6KD+F78ghAxnAddI=";
   };
 
   build-system = [ setuptools ];
