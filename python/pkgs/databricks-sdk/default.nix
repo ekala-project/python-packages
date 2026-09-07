2f50b29bce452fff0138492a1a6711a2bfcaaa09
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 03:26:09 -0700

    databricks-sdk: 0.121.0 -> 0.136.0

--- python/pkgs/databricks-sdk/default.nix
+++ python/pkgs/databricks-sdk/default.nix
@@ -21,7 +21,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "databricks-sdk";
-  version = "0.121.0";
+  version = "0.136.0";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -29,7 +29,7 @@ buildPythonPackage (finalAttrs: {
     owner = "databricks";
     repo = "databricks-sdk-py";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-EvTh2zxQE7xSzxaipI8qeG0tK/Bnk8bDKbWKdMDO0F4=";
+    hash = "sha256-oxWgm0tjD0bWFMqpRf/xfVqNPf7iOe1uVTS4mUHArlM=";
   };
 
   build-system = [
