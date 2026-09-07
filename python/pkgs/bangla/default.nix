d148d4fc9d5715f38b275ad4f08e2869dd088abe
Jonathan Ringer <jonringer117@gmail.com>
2026-09-06 23:40:56 -0700

    bangla: 0.0.5 -> 0.0.6

--- python/pkgs/bangla/default.nix
+++ python/pkgs/bangla/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "bangla";
-  version = "0.0.5";
-  format = "setuptools";
+  version = "0.0.6";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-rX2/rUUf9g4otYMNX0LDPXSIDRbIE8xRl95NamHzRwQ=";
+    hash = "sha256-r/5AaVajaWNRwTRkY3O59+aWewrJ2Rnclnc29n3GCxs=";
   };
+
+  build-system = [ setuptools ];
 
   pythonImportsCheck = [ "bangla" ];
 
