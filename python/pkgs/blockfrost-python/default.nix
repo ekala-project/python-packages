4785a0cd26713a58efdc314c2acf9ea687c6019d
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 00:08:27 -0700

    blockfrost-python: 0.6.0 -> 0.7.0

--- python/pkgs/blockfrost-python/default.nix
+++ python/pkgs/blockfrost-python/default.nix
@@ -2,7 +2,6 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  fetchpatch2,
   # Python deps
   requests,
   setuptools,
@@ -10,23 +9,15 @@
 
 buildPythonPackage rec {
   pname = "blockfrost-python";
-  version = "0.6.0";
+  version = "0.7.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "blockfrost";
     repo = "blockfrost-python";
     tag = version;
-    hash = "sha256-mN26QXxizDR+o2V5C2MlqVEbRns1BTmwZdUnnHNcFzw=";
+    hash = "sha256-2NuWUK1DthGnMTbZQIaDvF9VF2dVLgYrCUKJIxuq1V0=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      name = "blockfrost-python-fix-version";
-      url = "https://github.com/blockfrost/blockfrost-python/commit/02fdc67ff6d1333c0855e740114585852bbfa0bc.patch?full_index=1";
-      hash = "sha256-070tnWxOnVNsCYXmBFo39JUgQDqphdpqx3A9OIuC94U=";
-    })
-  ];
 
   build-system = [
     setuptools
