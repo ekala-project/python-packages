1bb4b4f397b0b94fe24f531d71d1f309006d5e78
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 06:25:41 -0700

    fastcore: 2.1.5 -> 2.2.22

--- python/pkgs/fastcore/default.nix
+++ python/pkgs/fastcore/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "fastcore";
-  version = "2.1.5";
+  version = "2.2.22";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "fastai";
     repo = "fastcore";
     tag = finalAttrs.version;
-    hash = "sha256-cjH+0gcCYj3kFq+Un82uLJhtVTDwwJnB9G8q0nofV3U=";
+    hash = "sha256-kNoDY9GscIvcq5owzULkILzbm02Xs81sva9Tdo3HFtQ=";
   };
 
   build-system = [ setuptools ];
