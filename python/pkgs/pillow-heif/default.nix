eb162ede27a480a7fbc85dca2f61771a08c1916d
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 22:54:04 -0700

    pillow-heif: 1.4.0 -> 1.7.0

--- python/pkgs/pillow-heif/default.nix
+++ python/pkgs/pillow-heif/default.nix
@@ -19,16 +19,26 @@
   pillow,
 }:
 
+let
+  # Disable gdk-pixbuf plugin in libheif to avoid broken gdk-pixbuf build;
+  # pillow-heif only needs the core library, not the gdk-pixbuf loader.
+  libheif' = libheif.overrideAttrs (old: {
+    buildInputs = lib.filter (i: i.pname or "" != "gdk-pixbuf") old.buildInputs;
+    cmakeFlags = (old.cmakeFlags or [ ]) ++ [ "-DWITH_GDK_PIXBUF=OFF" ];
+    env = builtins.removeAttrs (old.env or { }) [ "PKG_CONFIG_GDK_PIXBUF_2_0_GDK_PIXBUF_MODULEDIR" ];
+  });
+in
+
 buildPythonPackage rec {
   pname = "pillow-heif";
-  version = "1.4.0";
+  version = "1.7.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bigcat88";
     repo = "pillow_heif";
     tag = "v${version}";
-    hash = "sha256-EaislmA4v2qKCDQ87I85Pn8IlS4VJWyNXkITipKSBC8=";
+    hash = "sha256-OATq+z6lQLEYh3tFCcW5WRTdAFrjjnZ4yNig8NPiUTM=";
   };
 
   postPatch = ''
@@ -50,7 +60,7 @@ buildPythonPackage rec {
   buildInputs = [
     libaom
     libde265
-    libheif
+    libheif'
     x265
   ];
 
