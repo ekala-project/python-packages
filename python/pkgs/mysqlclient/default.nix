917ff61eda6944dbd0a173424a536333e847ccf0
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 19:54:42 -0700

    mysqlclient: 2.2.7 -> 2.2.8

--- python/pkgs/mysqlclient/default.nix
+++ python/pkgs/mysqlclient/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "mysqlclient";
-  version = "2.2.7";
+  version = "2.2.8";
   format = "setuptools";
 
   nativeBuildInputs = [ pkg-config ];
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   # Tests need a MySQL database
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-JK4itZQW1fzOfpnJ03VINQtFZbqsgvleFJysbOQWOEU=";
+    hash = "sha256-jtIMVhWpFdpFG7MIx9AwZkik/ZooCbqVyZJpAAYwYZk=";
   };
 
   meta = {
