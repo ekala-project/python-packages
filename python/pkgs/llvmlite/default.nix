8008924d21e809916655eb856822b6bc650cd3a6
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 17:51:41 -0700

    llvmlite: 0.48.0 -> 0.49.0

--- python/pkgs/llvmlite/default.nix
+++ python/pkgs/llvmlite/default.nix
@@ -21,7 +21,7 @@ in
 
 buildPythonPackage rec {
   pname = "llvmlite";
-  version = "0.48.0";
+  version = "0.49.0";
   pyproject = true;
 
   disabled = isPyPy;
@@ -30,8 +30,15 @@ buildPythonPackage rec {
     owner = "numba";
     repo = "llvmlite";
     tag = "v${version}";
-    hash = "sha256-qFly3Thx0jkCHy6r8+VWuGROUj910oHBEQFMZlAX1aw=";
+    hash = "sha256-AUte9llrcPl2z4ipkZ3PGeryDveZ9vj5oaBQtzGaT+w=";
   };
+
+  postPatch = ''
+    substituteInPlace ffi/CMakeLists.txt \
+      --replace-fail \
+        "set(LLVMLITE_SUPPORTED_LLVM_VERSION_DEFAULT 22)" \
+        "set(LLVMLITE_SUPPORTED_LLVM_VERSION_DEFAULT 21)"
+  '';
 
   build-system = [ setuptools ];
 
