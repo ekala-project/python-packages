c23062d65c6d5430447f2eec2e7e4d44b6b59d60
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 18:41:54 -0700

    metar: 1.11.0 -> 2.0.1

--- python/pkgs/metar/default.nix
+++ python/pkgs/metar/default.nix
@@ -3,19 +3,28 @@
   buildPythonPackage,
   fetchFromGitHub,
   pytestCheckHook,
+  setuptools,
+  setuptools-scm,
 }:
 
 buildPythonPackage rec {
   pname = "metar";
-  version = "1.11.0";
-  format = "setuptools";
+  version = "2.0.1";
+  pyproject = true;
+
+  env.SETUPTOOLS_SCM_PRETEND_VERSION = version;
 
   src = fetchFromGitHub {
     owner = "python-metar";
     repo = "python-metar";
     tag = "v${version}";
-    hash = "sha256-ZDjlXcSTUcSP7oRdhzLpXf/fLUA7Nkc6nj2I6vovbHg=";
+    hash = "sha256-MRcGUFcp3oL9rrGl3xGGttle5oXkJqxxZvkWVe0uWv8=";
   };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
