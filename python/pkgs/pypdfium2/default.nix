feefdc671b5a39f979418899dbbc1a6957002c64
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 02:51:51 -0700

    pypdfium2: 5.7.0 -> 5.13.0

--- python/pkgs/pypdfium2/default.nix
+++ python/pkgs/pypdfium2/default.nix
@@ -33,14 +33,14 @@ let
 in
 buildPythonPackage rec {
   pname = "pypdfium2";
-  version = "5.7.0";
+  version = "5.13.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pypdfium2-team";
     repo = "pypdfium2";
     tag = version;
-    hash = "sha256-zc/83Ypmxul8fB3q0lUSgC9yfcdg7tJuZff+0LE0w30=";
+    hash = "sha256-DbCbJUvYzjWIlGJyrUbJfzebHrPpRFEx60DYqs1UrkQ=";
   };
 
   build-system = [
