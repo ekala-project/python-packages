9b65dc7d917f38a9043783344a4fa5dfbf51dcf8
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 17:56:31 -0700

    logfire: 4.37.0 -> 5.0.0

--- python/pkgs/logfire/default.nix
+++ python/pkgs/logfire/default.nix
@@ -3,7 +3,6 @@
   callPackage,
   fetchFromGitHub,
   lib,
-  fetchpatch2,
 
   # build-system
   hatchling,
@@ -64,7 +63,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "logfire";
-  version = "4.37.0";
+  version = "5.0.0";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -72,19 +71,8 @@ buildPythonPackage (finalAttrs: {
     owner = "pydantic";
     repo = "logfire";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-o6qtMiwXcfUuYkUrM+Iy6WhtwBOLK3q/nqPDC5QMmg0=";
+    hash = "sha256-XqdTF+A/baZ2ftx/ao9N4NbygiHTjyyt5x15/V4MmIA=";
   };
-
-  patches = [
-    # https://github.com/pydantic/logfire/pull/2042
-    (fetchpatch2 {
-      url = "https://github.com/pydantic/logfire/commit/c7ff8beec3a674a2b096fcd9395159723de4cc67.patch";
-      hash = "sha256-HdPT1cYOUiyjJBUYC5yoVRjC+gWyCMvo1MpOSL32lPM=";
-      excludes = [
-        "uv.lock"
-      ];
-    })
-  ];
 
   build-system = [ hatchling ];
 
