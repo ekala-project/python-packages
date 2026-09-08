22ef924034610eb4d99a4c66858d17fa55ac609a
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 01:50:42 -0700

    pyinstaller: 6.18.0 -> 6.22.2

--- python/pkgs/pyinstaller/default.nix
+++ python/pkgs/pyinstaller/default.nix
@@ -15,6 +15,7 @@
   macholib,
   packaging,
   pyinstaller-hooks-contrib,
+  setuptools,
 
   # tests
   binutils,
@@ -25,12 +26,12 @@
 
 buildPythonPackage rec {
   pname = "pyinstaller";
-  version = "6.18.0";
+  version = "6.22.2";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-zcUHVCeDURytSFb85YL9w36fKWZcpZaInGY8g+yMbsk=";
+    hash = "sha256-ibZaOtB9ndWDIlPje8RfMYctENf51cn9D91giKg4Kd0=";
   };
 
   build-system = [ hatchling ];
@@ -42,6 +43,7 @@ buildPythonPackage rec {
     packaging
     macholib
     pyinstaller-hooks-contrib
+    setuptools
   ];
 
   makeWrapperArgs = lib.optionals stdenv.hostPlatform.isLinux [
