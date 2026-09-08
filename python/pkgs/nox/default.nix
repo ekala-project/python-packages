b807ff5376471fbbbcc39d66147e76172456169b
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 20:35:08 -0700

    nox: 2024.10.9 -> 2026.8.17

--- python/pkgs/nox/default.nix
+++ python/pkgs/nox/default.nix
@@ -4,27 +4,35 @@
   buildPythonPackage,
   hatchling,
   argcomplete,
+  attrs,
   colorlog,
+  dependency-groups,
+  humanize,
   packaging,
+  python-discovery,
   virtualenv,
 }:
 
 buildPythonPackage rec {
   pname = "nox";
-  version = "2024.10.9";
+  version = "2026.8.17";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-eqncjRwn6fRasEb/0cOyxPfJF1UwR2nfIxMIhJ697ZU=";
+    hash = "sha256-jZxpybmWpZ2x6yxpaN6uvC7bxVMX0gWYG7xKNzUdPy4=";
   };
 
   build-system = [ hatchling ];
 
   dependencies = [
     argcomplete
+    attrs
     colorlog
+    dependency-groups
+    humanize
     packaging
+    python-discovery
     virtualenv
   ];
 
