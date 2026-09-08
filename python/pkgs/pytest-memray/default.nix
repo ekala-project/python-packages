9d02bfae55a7b0d0e26ca4e89d5686df94191582
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 03:16:02 -0700

    pytest-memray: 1.8.0 -> 1.10.0

--- python/pkgs/pytest-memray/default.nix
+++ python/pkgs/pytest-memray/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pytest-memray";
-  version = "1.8.0";
+  version = "1.10.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bloomberg";
     repo = "pytest-memray";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-73Lyy14t2Hcqo0aTlWbGMzaxJ73bKjzc4BFE/jPG99I=";
+    hash = "sha256-jtbtgN3x99vpJ+CWew+pkK1VXdviw4JWNQIB/oKlPWs=";
   };
 
   build-system = [
