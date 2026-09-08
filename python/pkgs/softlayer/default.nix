173be822853f058f085e7b392688002e4b538512
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 07:15:28 -0700

    softlayer: 6.2.7 -> 6.2.9

--- python/pkgs/softlayer/default.nix
+++ python/pkgs/softlayer/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "softlayer";
-  version = "6.2.7";
+  version = "6.2.9";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "softlayer";
     repo = "softlayer-python";
     tag = "v${version}";
-    hash = "sha256-mlC4o39Ol1ALguc9KGpxB0M0vhWz4LG2uwhW8CBrVgg=";
+    hash = "sha256-kGgCW9N2NZi8PHcfpN+8L2bg7v1edP8ZXYaoSt9545M=";
   };
 
   build-system = [ setuptools ];
