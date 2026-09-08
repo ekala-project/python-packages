5196cedc4cabf213b316d44042e7cf36af2c60cc
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 07:32:28 -0700

    sparse: 0.18.0 -> 0.19.2

--- python/pkgs/sparse/default.nix
+++ python/pkgs/sparse/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "sparse";
-  version = "0.18.0";
+  version = "0.19.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pydata";
     repo = "sparse";
     tag = finalAttrs.version;
-    hash = "sha256-HHZ47TAgNbEEZj1sEe85yQRleW4Un2wfwQyFp4BPCbI=";
+    hash = "sha256-9RXiM5t0U64kKjMnz8Ram0qQm8QKNrOnO3OnFR/shdg=";
   };
 
   build-system = [
