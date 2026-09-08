9afc14a4297b9ff4f24f3d156a17151f92449127
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 04:35:27 -0700

    reportlab: 5.0.0 -> 5.0.1

--- python/pkgs/reportlab/default.nix
+++ python/pkgs/reportlab/default.nix
@@ -16,7 +16,7 @@ let
 in
 buildPythonPackage rec {
   pname = "reportlab";
-  version = "5.0.0";
+  version = "5.0.1";
   pyproject = true;
 
   # See https://bitbucket.org/pypy/compatibility/wiki/reportlab%20toolkit
@@ -24,7 +24,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-5ElKDGYjriE7uFb7pSMXGytUp79in9oC1eUlp7iZp4Q=";
+    hash = "sha256-69ExVL4chRXmZd5wvS0wOundw+9H5Er9URZEHKAoOiY=";
   };
 
   postPatch = ''
