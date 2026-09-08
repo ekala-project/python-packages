b07d965b295ef53cebb72bd2000faecc85981267
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 07:28:44 -0700

    sparklines: 0.7.0 -> 1.0.0

--- python/pkgs/sparklines/default.nix
+++ python/pkgs/sparklines/default.nix
@@ -8,28 +8,21 @@
 
 buildPythonPackage rec {
   pname = "sparklines";
-  version = "0.7.0";
+  version = "1.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "deeplook";
     repo = "sparklines";
     tag = "v${version}";
-    sha256 = "sha256-jiMrxZMWN+moap0bDH+uy66gF4XdGst9HJpnboJrQm4=";
+    sha256 = "sha256-HXOt3xqXfd9GwHQZifHcveE1v48nKRaCKGAJI8xeLdM=";
   };
 
-  propagatedBuildInputs = [
-    hatchling
-    termcolor
-  ];
-  pythonImportsCheck = [ "sparklines" ];
+  build-system = [ hatchling ];
 
-  postPatch = ''
-    export TMPDIR=$PWD/tmp
-    mkdir -p $TMPDIR
-    substituteInPlace tests/test_sparkline.py \
-      --replace-fail "/tmp/" "$TMPDIR/"
-  '';
+  dependencies = [ termcolor ];
+
+  pythonImportsCheck = [ "sparklines" ];
 
   meta = {
     description = "This Python package implements Edward Tufte's concept of sparklines, but limited to text only";
