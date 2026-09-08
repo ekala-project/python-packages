2515ab2611792b5f16ac1007ef97fa17fb12ca99
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 19:41:12 -0700

    msmart-ng: 2026.7.0 -> 2026.8.1

--- python/pkgs/msmart-ng/default.nix
+++ python/pkgs/msmart-ng/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage rec {
   pname = "msmart-ng";
-  version = "2026.7.0";
+  version = "2026.8.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "mill1000";
     repo = "midea-msmart";
     tag = version;
-    hash = "sha256-OW5++yd+o2KqaFWTo/RiLjK1HO2l9WSDxkiX3lYtaUs=";
+    hash = "sha256-WiHUQgiRyrro6uFCfZCiozlF2jFdtdGZP+2ud8LzHGw=";
   };
 
   build-system = [
