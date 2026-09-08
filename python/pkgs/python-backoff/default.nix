c7f6ad838d2b6e5e1029055f17bbd8c4b347a351
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 03:20:18 -0700

    python-backoff: 2.3.1 -> 3.0.0

--- python/pkgs/python-backoff/default.nix
+++ python/pkgs/python-backoff/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "python-backoff";
-  version = "2.3.1";
+  version = "3.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "python-backoff";
     repo = "backoff";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Os20Gz+uWaEdUPPF9/tT7LNxbmN0W/tuzVZa3H+ZG2A=";
+    hash = "sha256-y/L27SVOktR1FemVrCgrY1s28t8OxZ7rKqOB7zz8TC8=";
   };
 
   build-system = [ hatchling ];
