976249c1ed0ee48b411b872b81b73844e52f365d
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 04:28:26 -0700

    redis: 8.0.1 -> 8.1.0

--- python/pkgs/redis/default.nix
+++ python/pkgs/redis/default.nix
@@ -19,14 +19,14 @@
 
 buildPythonPackage rec {
   pname = "redis";
-  version = "8.0.1";
+  version = "8.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "redis";
     repo = "redis-py";
     tag = "v${version}";
-    hash = "sha256-41i4oZmbWi87KBSaAAaZe2gPlCpgw6kEPne1IA3PHQM=";
+    hash = "sha256-qKrJaI4IaqTzdeaxx7Q7vEvE6qmmtkrRKefkrhCy4Q4=";
   };
 
   build-system = [ hatchling ];
