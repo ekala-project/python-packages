8a7ec699b787aa4a06eabe75c150248a25cfb960
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 06:51:26 -0700

    simple-parsing: 0.1.8 -> 0.1.9

--- python/pkgs/simple-parsing/default.nix
+++ python/pkgs/simple-parsing/default.nix
@@ -26,14 +26,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "simple-parsing";
-  version = "0.1.8";
+  version = "0.1.9";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "lebrice";
     repo = "SimpleParsing";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Nsr+I+BoVxockRGQAjG+ushRQ4CtWgkHrg5aVorSrvw=";
+    hash = "sha256-tSQgyxJ0INGQkmqRmzVgKwBd/JXr3OTecdNSfc9Nf94=";
   };
 
   build-system = [
