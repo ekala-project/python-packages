18d42537ed5a10818437fbcd884f0c8770685631
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 22:40:21 -0700

    petl: 1.7.19 -> 1.7.24

--- python/pkgs/petl/default.nix
+++ python/pkgs/petl/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "petl";
-  version = "1.7.19";
+  version = "1.7.24";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "petl-developers";
     repo = "petl";
     tag = "v${version}";
-    hash = "sha256-xRNQ4QwTw96kVYzfBiMZcsrPugGFiiRblV1nZ8pAFLY=";
+    hash = "sha256-0YIaA/0VaQ1dgKC0NrLTkAsn8kkEHIJ4W9sdZHYQeMM=";
   };
 
   build-system = [
