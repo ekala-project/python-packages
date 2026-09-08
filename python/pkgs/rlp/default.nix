70c83a7e02bb15afaf72c26f339973578cfd39aa
Jonathan Ringer <jonringer117@gmail.com>
2026-09-08 04:45:32 -0700

    rlp: 4.1.0 -> 5.0.0

--- python/pkgs/rlp/default.nix
+++ python/pkgs/rlp/default.nix
@@ -2,23 +2,24 @@
   lib,
   fetchFromGitHub,
   setuptools,
+  setuptools-scm,
   buildPythonPackage,
   eth-utils,
 }:
 
 buildPythonPackage rec {
   pname = "rlp";
-  version = "4.1.0";
+  version = "5.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "pyrlp";
     rev = "v${version}";
-    hash = "sha256-moerdcAJXqhlzDnTlvxL3Nzz485tOzJVCPlGrof80eQ=";
+    hash = "sha256-a13KBXZyId83EukBTc3zu1GgllsqMRS0gpYfZ29+bjQ=";
   };
 
-  build-system = [ setuptools ];
+  build-system = [ setuptools setuptools-scm ];
 
   propagatedBuildInputs = [ eth-utils ];
   pythonImportsCheck = [ "rlp" ];
