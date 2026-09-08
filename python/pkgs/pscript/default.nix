215c644b1dc66e4e74e18fbaadabc0373b01c648
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 23:55:26 -0700

    pscript: 0.8.0 -> 0.8.1

--- python/pkgs/pscript/default.nix
+++ python/pkgs/pscript/default.nix
@@ -7,21 +7,15 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pscript";
-  version = "0.8.0";
+  version = "0.8.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "flexxui";
     repo = "pscript";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-pqjig3dFJ4zfpor6TT6fiBMS7lAtJE/bAYbzl46W/YY=";
+    hash = "sha256-eb2hODFiVAsYmx3XnPToVQFzgug/IkfE+MiTUeSWY7M=";
   };
-
-  postPatch = ''
-    # https://github.com/flexxui/pscript/pull/77
-    substituteInPlace pscript/commonast.py \
-      --replace-fail "ast.Ellipsis" "ast.Constant"
-  '';
 
   build-system = [ flit-core ];
 
