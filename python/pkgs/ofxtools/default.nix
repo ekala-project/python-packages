f0c5309135ffdb6fde9f0b11447669c3f112fcd8
Jonathan Ringer <jonringer117@gmail.com>
2026-09-07 21:01:20 -0700

    ofxtools: 0.9.5 -> 1.1.1

--- python/pkgs/ofxtools/default.nix
+++ python/pkgs/ofxtools/default.nix
@@ -2,21 +2,24 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
   pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "ofxtools";
-  version = "0.9.5";
-  format = "setuptools";
+  version = "1.1.1";
+  pyproject = true;
 
   # PyPI distribution does not include tests
   src = fetchFromGitHub {
     owner = "csingley";
     repo = "ofxtools";
     rev = version;
-    hash = "sha256-NsImnD+erhpakQnl1neuHfSKiV6ipNBMPGKMDM0gwWc=";
+    hash = "sha256-874xcs5M5K6JxYoz9xhbZM/VZICZshHe9nb2C3HAQLA=";
   };
+
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytestCheckHook ];
   # override $HOME directory:
@@ -29,6 +32,6 @@ buildPythonPackage rec {
     homepage = "https://github.com/csingley/ofxtools";
     description = "Library for working with Open Financial Exchange (OFX) formatted data used by financial institutions";
     mainProgram = "ofxget";
-    license = lib.licenses.mit;
+    license = lib.licenses.gpl3Only;
   };
 }
