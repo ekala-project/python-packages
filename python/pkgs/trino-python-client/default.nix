3728be04d546ef64464c88ea4849e0167b75598d
Jonathan Ringer <jonringer117@gmail.com>
2026-09-09 15:49:07 -0700

    trino-python-client: 0.338.0 -> 0.339.0

--- python/pkgs/trino-python-client/default.nix
+++ python/pkgs/trino-python-client/default.nix
@@ -21,14 +21,14 @@
 
 buildPythonPackage rec {
   pname = "trino-python-client";
-  version = "0.338.0";
+  version = "0.339.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     repo = "trino-python-client";
     owner = "trinodb";
     tag = version;
-    hash = "sha256-kWbqzdeOkzjhcaQOS4bCUnXFILpurtVE3N3KLoqSeds=";
+    hash = "sha256-gdxSxCbFBuOGJuS5j8bJHf4IA0B/umFRiDGNU4ezZkU=";
   };
 
   build-system = [ setuptools ];
