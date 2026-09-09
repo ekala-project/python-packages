77a5c2ba4160c9c1f64098b1160e4b9a3f813964
Jonathan Ringer <jonringer117@gmail.com>
2026-09-09 16:27:30 -0700

    uncalled-for: 0.3.2 -> 0.4.0

--- python/pkgs/uncalled-for/default.nix
+++ python/pkgs/uncalled-for/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "uncalled-for";
-  version = "0.3.2";
+  version = "0.4.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "chrisguidry";
     repo = "uncalled-for";
     tag = finalAttrs.version;
-    hash = "sha256-5jFMlynYaxd83SQiZ1uPs1whWFgKP4y6s473TplH4iI=";
+    hash = "sha256-owpWPOV70z1NCpfVOGlyczfqrPR8Yyd7SUzY9wWZiXU=";
   };
 
   build-system = [
