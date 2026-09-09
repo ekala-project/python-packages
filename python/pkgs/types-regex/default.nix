21ef9971cbd8a2e7d9b684ae53d8badfb382944e
Jonathan Ringer <jonringer117@gmail.com>
2026-09-09 16:12:31 -0700

    types-regex: 2026.7.10.20260711 -> 2026.9.3.20260903

--- python/pkgs/types-regex/default.nix
+++ python/pkgs/types-regex/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "types-regex";
-  version = "2026.7.10.20260711";
+  version = "2026.9.3.20260903";
   pyproject = true;
 
   src = fetchPypi {
     pname = "types_regex";
     inherit (finalAttrs) version;
-    hash = "sha256-mqGxQgYK3V1KT0IUbry3DgHnsniefORzJVMj88Xu3e0=";
+    hash = "sha256-7vgqCuCIBvMmCfxIKf7oRfmGct1jeINMNN7DO/jGDPA=";
   };
 
   build-system = [ setuptools ];
