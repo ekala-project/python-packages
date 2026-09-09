8d20559b6421ed2f9c824aeb8f5d13e769c1bf3b
Jonathan Ringer <jonringer117@gmail.com>
2026-09-09 16:09:31 -0700

    types-psutil: 7.2.1.20260116 -> 7.2.2.20260906

--- python/pkgs/types-psutil/default.nix
+++ python/pkgs/types-psutil/default.nix
@@ -2,18 +2,21 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "types-psutil";
-  version = "7.2.1.20260116";
-  format = "setuptools";
+  version = "7.2.2.20260906";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "types_psutil";
     inherit version;
-    hash = "sha256-RmG+XV16zV2K+wKpLQUWCmy7LOdHIyRbUfe6ff25+YE=";
+    hash = "sha256-k6vyLPmmK5Ffck5DO95wKZWsJ0hlQl/Up20dm1go2ho=";
   };
+
+  build-system = [ setuptools ];
 
   # Module doesn't have tests
   doCheck = false;
