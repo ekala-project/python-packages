b307a1429937ae50cb4c54440fbb0b723ffda0be
Jonathan Ringer <jonringer117@gmail.com>
2026-09-09 13:29:45 -0700

    supabase-auth: 2.29.0 -> 2.31.0

--- python/pkgs/supabase-auth/default.nix
+++ python/pkgs/supabase-auth/default.nix
@@ -2,7 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  uv-build,
+  hatchling,
   httpx,
   pydantic,
   pyjwt,
@@ -13,19 +13,19 @@
 }:
 buildPythonPackage (finalAttrs: {
   pname = "supabase-auth";
-  version = "2.29.0";
+  version = "2.31.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "supabase";
     repo = "supabase-py";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-LaSlAYFvx/HHdfmc9J+KScVQ9JFGS98Yfihzn8F7t3g=";
+    hash = "sha256-cdsxB42X9nJeDElOI20jaWRKOrpNYwY2sB4vty8yYUM=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/src/auth";
 
-  build-system = [ uv-build ];
+  build-system = [ hatchling ];
 
   dependencies = [
     httpx
@@ -37,7 +37,8 @@ buildPythonPackage (finalAttrs: {
 
   postPatch = ''
     substituteInPlace pyproject.toml \
-      --replace-fail 'uv_build>=0.8.3,<0.9.0' 'uv_build>=0.8.3'
+      --replace-fail 'requires = ["uv_build>=0.8.3,<0.9.0"]' 'requires = ["hatchling"]' \
+      --replace-fail 'build-backend = "uv_build"' 'build-backend = "hatchling.build"'
   '';
   pythonImportsCheck = [ "supabase_auth" ];
 
