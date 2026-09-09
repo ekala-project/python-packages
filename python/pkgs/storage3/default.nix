1e0406061802e28535fd502a7c73ed4d3db18f1e
Jonathan Ringer <jonringer117@gmail.com>
2026-09-09 13:20:46 -0700

    storage3: 2.29.0 -> 2.31.0

--- python/pkgs/storage3/default.nix
+++ python/pkgs/storage3/default.nix
@@ -2,42 +2,41 @@
   buildPythonPackage,
   fetchFromGitHub,
   lib,
-  uv-build,
+  hatchling,
   httpx,
   pydantic,
   yarl,
-  pyiceberg,
   deprecation,
 }:
 
 buildPythonPackage (finalAttrs: {
   pname = "storage3";
-  version = "2.29.0";
+  version = "2.31.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "supabase";
     repo = "supabase-py";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-LaSlAYFvx/HHdfmc9J+KScVQ9JFGS98Yfihzn8F7t3g=";
+    hash = "sha256-cdsxB42X9nJeDElOI20jaWRKOrpNYwY2sB4vty8yYUM=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/src/storage";
 
-  build-system = [ uv-build ];
+  build-system = [ hatchling ];
 
   dependencies = [
     httpx
     pydantic
     yarl
-    pyiceberg
     deprecation
   ]
   ++ httpx.optional-dependencies.http2;
 
   postPatch = ''
     substituteInPlace pyproject.toml \
-      --replace-fail 'uv_build>=0.8.3,<0.9.0' 'uv_build>=0.8.3'
+      --replace-fail 'requires = ["uv_build>=0.8.3,<0.9.0"]' 'requires = ["hatchling"]' \
+      --replace-fail 'build-backend = "uv_build"' 'build-backend = "hatchling.build"'
   '';
   pythonImportsCheck = [ "storage3" ];
   meta = {
