02bd3a3fe3364361f579b1e64084d16cf996ec47
Jonathan Ringer <jonringer117@gmail.com>
2026-09-09 13:22:31 -0700

    stripe: 15.3.1 -> 15.6.1

--- python/pkgs/stripe/default.nix
+++ python/pkgs/stripe/default.nix
@@ -9,12 +9,12 @@
 
 buildPythonPackage rec {
   pname = "stripe";
-  version = "15.3.1";
+  version = "15.6.1";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-L5qNlh5cLsGRSftOC0fZcJ0BSmhZLNsfTv0ESryNI1k=";
+    hash = "sha256-nzmGqpYEGQQaupx2GRdGvfLDQslYkqz79u21AKsAbTE=";
   };
 
   build-system = [ flit-core ];
