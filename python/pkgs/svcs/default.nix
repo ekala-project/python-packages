330677c7614542dfcc67148514e3c40844f9b692
Jonathan Ringer <jonringer117@gmail.com>
2026-09-09 13:32:19 -0700

    svcs: 25.1.0 -> 26.2.0

--- python/pkgs/svcs/default.nix
+++ python/pkgs/svcs/default.nix
@@ -5,6 +5,7 @@
   fetchFromGitHub,
 
   attrs,
+  typing-extensions,
   hatch-fancy-pypi-readme,
   hatch-vcs,
   hatchling,
@@ -21,14 +22,14 @@
 
 buildPythonPackage rec {
   pname = "svcs";
-  version = "25.1.0";
+  version = "26.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "hynek";
     repo = "svcs";
     tag = version;
-    hash = "sha256-dDPmOKGifAGmAH3TD0NzJvR8lUB5qDWbxIwzHtNeF+4=";
+    hash = "sha256-GgfuCZ1LvcPD3p9/YSrUUXZHS86GTMjLwmXivYmwgCQ=";
   };
 
   build-system = [
@@ -39,6 +40,7 @@ buildPythonPackage rec {
 
   dependencies = [
     attrs
+    typing-extensions
   ];
   disabledTests = lib.optionals stdenv.hostPlatform.isDarwin [
     "test_aclose_registry_ok"
