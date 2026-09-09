b95cc22533fde58f4ca95daed252b5c0d22920ae
Jonathan Ringer <jonringer117@gmail.com>
2026-09-09 13:46:11 -0700

    tensorboardx: 2.6.4 -> 2.6.5

--- python/pkgs/tensorboardx/default.nix
+++ python/pkgs/tensorboardx/default.nix
@@ -13,21 +13,15 @@
 
 buildPythonPackage rec {
   pname = "tensorboardx";
-  version = "2.6.4";
+  version = "2.6.5";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "lanpa";
     repo = "tensorboardX";
     tag = "v${version}";
-    hash = "sha256-GZQUJCiCKVthO95jHMIzNFcBM3R85BkyxO74CKCzizc=";
+    hash = "sha256-VTekZSfD2oZUQe7B2NwePlljGdrZNQMdjJ4K66hS4Ps=";
   };
-
-  postPatch = ''
-    # https://github.com/lanpa/tensorboardX/pull/761
-    substituteInPlace tensorboardX/utils.py tests/test_utils.py \
-      --replace-fail "newshape=" "shape="
-  '';
 
   build-system = [
     setuptools
